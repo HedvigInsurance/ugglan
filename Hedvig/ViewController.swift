83e25429d4bfdcb112c9eba6f55af156eedce892
Sam Pettersson <sam@sampettersson.com>
2018-11-07 12:57:12 +0100

    Initial Commit

---
+++ Hedvig/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hedvig
+//
+//  Created by Sam Pettersson on 2018-11-07.
+//  Copyright © 2018 Sam Pettersson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
