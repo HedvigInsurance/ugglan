0fe3dc1814c5f6133001efeb1ba1653b5bc9ea34
Sam Pettersson <sam@sampettersson.com>
2019-01-13 19:13:34 +0100

    Delete prefix once again

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,10 +1,11 @@
+npm config delete prefix
+
 echo "Installing NVM..."
 brew install nvm
 source $(brew --prefix nvm)/nvm.sh
 
 echo "Installing v8.5..."
 nvm install v8.5.0
-nvm use --delete-prefix v6.15.1
 nvm use --delete-prefix v8.5.0
 nvm alias default v8.5.0
 
