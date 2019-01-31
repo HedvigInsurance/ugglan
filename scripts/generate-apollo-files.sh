e9ec26a5e8138d2f9629363e7699b521067157cc
Sam Pettersson <sam@sampettersson.com>
2019-01-31 23:49:01 +0100

    Update scripts

--- scripts/generate-apollo-files.sh
+++ scripts/generate-apollo-files.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-CONFIGURATION="DEBUG" sh Carthage/Build/iOS/Apollo.framework/check-and-run-apollo-cli.sh codegen:generate --queries="$(find . -name '*.graphql')"  --schema=Src/Data/schema.json Src/Data/API.swift
+CONFIGURATION="DEBUG" sh Carthage/Build/iOS/Apollo.framework/check-and-run-apollo-cli.sh codegen:generate --queries="$(find Src -name '*.graphql')"  --schema=Src/Data/schema.json Src/Data/API.swift
