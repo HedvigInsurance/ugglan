951abfe3e362a0de4a6f8f1afd99c5961eb989a8
Sam Pettersson <sam@sampettersson.com>
2019-02-17 12:23:39 +0100

    Set shadow image to 1pt

--- Src/Generic/Extensions/UIColor+Image.swift
+++ Src/Generic/Extensions/UIColor+Image.swift
@@ -10,10 +10,10 @@ import UIKit
 
 extension UIColor {
     func as1ptImage() -> UIImage {
-        UIGraphicsBeginImageContext(CGSize(width: 1 / UIScreen.main.scale, height: 1 / UIScreen.main.scale))
+        UIGraphicsBeginImageContext(CGSize(width: 1, height: 1))
         let ctx = UIGraphicsGetCurrentContext()
         self.setFill()
-        ctx?.fill(CGRect(x: 0, y: 0, width: 1 / UIScreen.main.scale, height: 1 / UIScreen.main.scale))
+        ctx?.fill(CGRect(x: 0, y: 0, width: 1, height: 1))
         let image = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         return image!
