6e4b279f156c02433f39add42b0bdbf5a8d61fc0
Gustaf Gunér <gustaf@guner.se>
2019-02-20 16:01:14 +0100

    App version and Member ID is now included in email

--- Src/Screens/ProfileScreen/Screens/FeedbackScreen/Viewables/ReportBugRow.swift
+++ Src/Screens/ProfileScreen/Screens/FeedbackScreen/Viewables/ReportBugRow.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2019 Hedvig AB. All rights reserved.
 //
 
+import Apollo
 import Flow
 import Form
 import Foundation
@@ -14,7 +15,13 @@ import MessageUI
 import DeviceKit
 
 struct ReportBugRow {
+    let client: ApolloClient
     let presentingViewController: UIViewController
+    
+    init(client: ApolloClient = HedvigApolloClient.shared.client!, presentingViewController: UIViewController) {
+        self.client = client
+        self.presentingViewController = presentingViewController
+    }
 }
 
 extension ReportBugRow: Viewable {
@@ -31,29 +38,34 @@ extension ReportBugRow: Viewable {
         row.valueStyleSignal.value = .rowTitlePurple
     
         let device = Device()
-    
-        let deviceInfo = "Device: \(device)\nSystem: \(device.systemName) \(device.systemVersion)"
-        
-        var attachments: [MFMailComposeViewControllerAttachment] = []
-        
-        if let data = deviceInfo.data(using: .utf8) {
-            attachments.append(MFMailComposeViewControllerAttachment(data, mimeType: "text/txt", fileName: "device-info.txt"))
-        }
+        let appVersion = Bundle.main.infoDictionary!["CFBundleShortVersionString"] as? String
         
         bag += events.onSelect.onValue { _ in
             if MFMailComposeViewController.canSendMail() {
-                let mailView = MailView(
-                    recipients: [emailAddress],
-                    attachments: attachments
-                )
-                
-                let activityViewPresentation = Presentation(
-                    mailView,
-                    style: .activityView,
-                    options: .defaults
-                )
-                
-                self.presentingViewController.present(activityViewPresentation)
+                self.client.fetch(query: MemberIdQuery()) { (result, error) in
+                    if let memberId = result?.data?.member.id {
+                        let deviceInfo = "Device: \(device)\nSystem: \(device.systemName) \(device.systemVersion)\nApp Version: \(appVersion ?? "")\nMember ID: \(memberId)"
+                        
+                        var attachments: [MFMailComposeViewControllerAttachment] = []
+                        
+                        if let data = deviceInfo.data(using: .utf8) {
+                            attachments.append(MFMailComposeViewControllerAttachment(data, mimeType: "text/txt", fileName: "device-info.txt"))
+                        }
+                        
+                        let mailView = MailView(
+                            recipients: [emailAddress],
+                            attachments: attachments
+                        )
+                        
+                        let activityViewPresentation = Presentation(
+                            mailView,
+                            style: .activityView,
+                            options: .defaults
+                        )
+                        
+                        self.presentingViewController.present(activityViewPresentation)
+                    }
+                }
             }
         }
        
