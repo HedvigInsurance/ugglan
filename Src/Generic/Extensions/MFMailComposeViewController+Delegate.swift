034c8c6dd8600a0c7d29a84a3b0d5f40d418950d
Gustaf Gunér <gustaf@guner.se>
2019-02-20 13:18:05 +0100

    Mail extension is now supporting attachments

--- Src/Generic/Extensions/MFMailComposeViewController+Delegate.swift
+++ Src/Generic/Extensions/MFMailComposeViewController+Delegate.swift
@@ -9,18 +9,39 @@
 import Foundation
 import MessageUI
 
+
+public struct MFMailComposeViewControllerAttachment {
+    let data: Data
+    let mimeType: String
+    let fileName: String
+    
+    init(_ data: Data, mimeType: String, fileName: String) {
+        self.data = data
+        self.mimeType = mimeType
+        self.fileName = fileName
+    }
+}
+
 public extension MFMailComposeViewController {
-    public static func create(to: [String]) -> MFMailComposeViewController {
+    public static func create(to: [String], subject: String = "", attachments: [MFMailComposeViewControllerAttachment] = []) -> MFMailComposeViewController {
         let viewController = MFMailComposeViewController()
+        
         viewController.mailComposeDelegate = viewController
+        
         viewController.setToRecipients(to)
+        viewController.setSubject(subject)
+        
+        for attachment in attachments {
+            print(attachment)
+            viewController.addAttachmentData(attachment.data, mimeType: attachment.mimeType, fileName: attachment.fileName)
+        }
         
         return viewController
     }
 }
 
 extension MFMailComposeViewController: MFMailComposeViewControllerDelegate {
-    public func mailComposeController(controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+    public func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
         dismiss(animated: true, completion: nil)
     }
 }
--- Src/Generic/MailView.swift
+++ Src/Generic/MailView.swift
@@ -18,7 +18,17 @@ struct MailView {
 
 extension MailView: Presentable {
     func materialize() -> (MFMailComposeViewController, Disposable) {
-        let mailComposeViewController = MFMailComposeViewController.create(to: self.recipients)
+        var attachments: [MFMailComposeViewControllerAttachment] = []
+        
+        let deviceInfo = "Device name: \(UIDevice.current.name)\nSystem: \(UIDevice.current.systemName) \(UIDevice.current.systemVersion)\nModel \(UIDevice.current.model)"
+        
+        print(deviceInfo)
+        
+        if let data = deviceInfo.data(using: .utf8) {
+            attachments.append(MFMailComposeViewControllerAttachment(data, mimeType: "text/txt", fileName: "device-info.txt"))
+        }
+        
+        let mailComposeViewController = MFMailComposeViewController.create(to: self.recipients, attachments: attachments)
         
         return (mailComposeViewController, NilDisposer())
     }    
