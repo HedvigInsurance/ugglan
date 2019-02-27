1429067c858a96c7689130d537c0e5b839b7c7e4
Sam Pettersson <sam@sampettersson.com>
2019-02-27 19:00:08 +0100

    Open BankId when requested from trustly (#21)

---
+++ Src/Generic/Extensions/WKWebView+OpenBankId.swift
@@ -0,0 +1,57 @@
+//
+//  WKWebView+OpenBankId.swift
+//  ugglan
+//
+//  Created by Sam Pettersson on 2019-02-27.
+//
+
+import Foundation
+import WebKit
+import Presentation
+
+class OpenBankIdHandler: NSObject, WKURLSchemeHandler {
+    let presentingViewController: UIViewController
+    
+    @available(iOS 11.0, *)
+    func webView(_ webView: WKWebView, start urlSchemeTask: WKURLSchemeTask) {
+        guard let url = urlSchemeTask.request.url else { return }
+        
+        webView.stopLoading()
+        
+        if UIApplication.shared.canOpenURL(url) {
+            UIApplication.shared.openURL(url)
+        } else {
+            let alert = Alert<Void>(
+                title: String(.TRUSTLY_MISSING_BANK_ID_APP_ALERT_TITLE),
+                message: String(.TRUSTLY_MISSING_BANK_ID_APP_ALERT_MESSAGE),
+                actions: [
+                    Alert.Action(
+                        title: String(.TRUSTLY_MISSING_BANK_ID_APP_ALERT_ACTION)
+                    ) { () }
+                ]
+            )
+            
+            presentingViewController.present(alert)
+        }
+    }
+    
+    @available(iOS 11.0, *)
+    func webView(_ webView: WKWebView, stop urlSchemeTask: WKURLSchemeTask) {
+        // do nothing
+    }
+    
+    init(_ presentingViewController: UIViewController) {
+        self.presentingViewController = presentingViewController
+    }
+}
+
+extension WKWebViewConfiguration {
+    func addOpenBankIDBehaviour(_ presentingViewController: UIViewController) {
+        if #available(iOS 11.0, *) {
+            self.setURLSchemeHandler(
+                OpenBankIdHandler(presentingViewController),
+                forURLScheme: "bankid"
+            )
+        }
+    }
+}
