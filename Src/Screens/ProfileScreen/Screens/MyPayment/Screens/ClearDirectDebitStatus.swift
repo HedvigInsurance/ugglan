b5583df062c81c98a38fca951c92066efea2e91b
Sam Pettersson <sam@sampettersson.com>
2019-03-13 14:48:57 +0100

    Add ClearDirectDebitStatus struct

---
+++ Src/Screens/ProfileScreen/Screens/MyPayment/Screens/ClearDirectDebitStatus.swift
@@ -0,0 +1,13 @@
+//
+//  ClearDirectDebitStatus.swift
+//  ugglan
+//
+//  Created by Sam Pettersson on 2019-03-13.
+//
+
+import Foundation
+
+struct ClearDirectDebitStatus {
+    // React Native hack, remove later
+    static func clear() {}
+}
