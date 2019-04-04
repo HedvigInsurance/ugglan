f49332e86a179b4c2ab970b29525c9f4e36d8190
Gustaf Gunér <gustaf@guner.se>
2019-04-04 11:08:30 +0200

    Markdown support in charity information

--- Src/Screens/ProfileScreen/Screens/CharityScreen/Viewables/CharityInformation.swift
+++ Src/Screens/ProfileScreen/Screens/CharityScreen/Viewables/CharityInformation.swift
@@ -10,6 +10,7 @@ import Flow
 import Form
 import Presentation
 import UIKit
+import MarkdownKit
 
 struct CharityInformation {}
 
@@ -24,6 +25,31 @@ extension CharityInformation: Presentable {
         
         let header = DraggableViewHeader(title: String(.PROFILE_MY_CHARITY_INFO_TITLE))
         bag += containerView.add(header)
+        
+        let bodyView = UIView()
+        containerView.addSubview(bodyView)
+        
+        bodyView.snp.remakeConstraints { make in
+            make.top.equalTo(60)
+            make.width.equalToSuperview()
+        }
+        
+        let markdownParser = MarkdownParser()
+        
+        let body = UILabel()
+        body.numberOfLines = 0
+        body.lineBreakMode = .byWordWrapping
+        body.attributedText = markdownParser.parse(String(.PROFILE_MY_CHARITY_INFO_BODY))
+        
+        bag += body.didLayoutSignal.onValue { _ in
+            body.snp.remakeConstraints { make in
+                make.width.equalToSuperview().inset(24)
+                make.height.equalToSuperview().inset(24)
+                make.center.equalToSuperview()
+            }
+        }
+        
+        bodyView.addSubview(body)
         
         viewController.view = containerView;
         
