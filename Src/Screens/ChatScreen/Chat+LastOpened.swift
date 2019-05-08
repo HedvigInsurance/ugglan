1dbce23ca7cc95ecb23381a034bab2699cd14d91
Sam Pettersson <sam@sampettersson.com>
2019-05-08 15:39:34 +0200

    Formatting

--- Src/Screens/ChatScreen/Chat+LastOpened.swift
+++ Src/Screens/ChatScreen/Chat+LastOpened.swift
@@ -5,36 +5,36 @@
 //  Created by Sam Pettersson on 2019-05-08.
 //
 
-import Foundation
 import Flow
+import Foundation
 
 extension Chat {
     private static var lastOpenedChatKey = "lastOpenedChat"
-    
+
     static var lastOpenedChatSignal: ReadSignal<Int64?> {
         func getSignalValue() -> Int64? {
             if let lastOpenedChatDescription = UserDefaults.standard.string(forKey: lastOpenedChatKey) {
                 return Int64(lastOpenedChatDescription)
             }
-            
+
             return nil
         }
-        
+
         let originalValue = getSignalValue()
-        
+
         return Signal { callback in
             let bag = DisposeBag()
-            
-            bag += NotificationCenter.default.signal(forName: UserDefaults.didChangeNotification).onValue { notification in
+
+            bag += NotificationCenter.default.signal(forName: UserDefaults.didChangeNotification).onValue { _ in
                 if getSignalValue() != originalValue {
                     callback(getSignalValue())
                 }
             }
-            
+
             return bag
-            }.readable(capturing: getSignalValue())
+        }.readable(capturing: getSignalValue())
     }
-    
+
     static func didOpen() {
         let newValue = Date().currentTimeMillis()
         UserDefaults.standard.set(newValue, forKey: lastOpenedChatKey)
