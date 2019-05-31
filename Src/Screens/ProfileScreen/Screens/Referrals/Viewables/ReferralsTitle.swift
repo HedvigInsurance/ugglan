11b078daf40606247950c516d4ea908a0064dd06
Sam Pettersson <sam@sampettersson.com>
2019-05-31 11:02:11 +0200

    Add description

--- Src/Screens/ProfileScreen/Screens/Referrals/Viewables/ReferralsTitle.swift
+++ Src/Screens/ProfileScreen/Screens/Referrals/Viewables/ReferralsTitle.swift
@@ -18,16 +18,30 @@ extension ReferralsTitle: Viewable {
         let view = UIStackView()
         view.axis = .vertical
         view.alignment = .center
+        view.spacing = 8
 
         let title = MultilineLabel(
             styledText: StyledText(
                 text: String(key: .REFERRALS_TITLE),
-                style: .standaloneLargeTitle
+                style: TextStyle.standaloneLargeTitle.centerAligned
             )
         )
 
         bag += view.addArranged(title) { titleView in
             titleView.snp.makeConstraints { make in
+                make.width.equalToSuperview().inset(20)
+            }
+        }
+        
+        let description = MultilineLabel(
+            styledText: StyledText(
+                text: "Du ger bort 10 kr rabatt och får 10 kr rabatt för varje vän du bjuder in via din unika länk! Kan du nå gratis försäkring?",
+                style: TextStyle.bodyOffBlack.centerAligned
+            )
+        )
+        
+        bag += view.addArranged(description) { descriptionView in
+            descriptionView.snp.makeConstraints { make in
                 make.width.equalToSuperview().inset(20)
             }
         }
