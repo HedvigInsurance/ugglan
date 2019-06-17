2b8c2ba08070f023b5beb5625faa770c4a515190
Sam Pettersson <sam@sampettersson.com>
2019-06-17 16:32:28 +0200

    Loading animations and stuff
    # Conflicts:
    #	Cartfile.resolved
    #	Src/Screens/ProfileScreen/Screens/Referrals/Viewables/ReferralsCode/ReferralsCode.swift

--- Src/Screens/ProfileScreen/Screens/Referrals/Referrals.swift
+++ Src/Screens/ProfileScreen/Screens/Referrals/Referrals.swift
@@ -25,65 +25,11 @@ enum ReferralsFailure: LocalizedError {
 
 struct Referrals {
     let client: ApolloClient
-    let remoteConfigContainer: RemoteConfigContainer
 
     init(
-        client: ApolloClient = ApolloContainer.shared.client,
-        remoteConfigContainer: RemoteConfigContainer = RemoteConfigContainer.shared
+        client: ApolloClient = ApolloContainer.shared.client
     ) {
         self.client = client
-        self.remoteConfigContainer = remoteConfigContainer
-    }
-
-    func createInvitationLink(memberId: String) -> Future<String> {
-        return Future { completion in
-            let incentive = self.remoteConfigContainer.referralsIncentive()
-
-            guard let link = URL(
-                string: String(
-                    key:
-                    .REFERRALS_DYNAMIC_LINK_LANDING(
-                        incentive: String(incentive),
-                        memberId: memberId
-                    )
-                )
-            ) else {
-                return NilDisposer()
-            }
-
-            let domainUriPrefix = self.remoteConfigContainer.dynamicLinkDomainPrefix()
-
-            let linkBuilder = DynamicLinkComponents(
-                link: link,
-                domainURIPrefix: domainUriPrefix
-            )
-
-            linkBuilder?.iOSParameters = DynamicLinkIOSParameters(
-                bundleID: self.remoteConfigContainer.dynamicLinkiOSBundleId()
-            )
-            linkBuilder?.iOSParameters?.appStoreID = self.remoteConfigContainer.dynamicLinkiOSAppStoreId()
-            linkBuilder?.androidParameters = DynamicLinkAndroidParameters(
-                packageName: self.remoteConfigContainer.dynamicLinkAndroidPackageName()
-            )
-
-            linkBuilder?.socialMetaTagParameters = DynamicLinkSocialMetaTagParameters()
-            linkBuilder?.socialMetaTagParameters?.title = String(key: .REFERRAL_SHARE_SOCIAL_TITLE)
-            linkBuilder?.socialMetaTagParameters?.descriptionText = String(key: .REFERRAL_SHARE_SOCIAL_DESCRIPTION)
-
-            if let imageUrl = URL(string: String(key: .REFERRAL_SHARE_SOCIAL_IMAGE_URL)) {
-                linkBuilder?.socialMetaTagParameters?.imageURL = imageUrl
-            }
-
-            linkBuilder?.shorten { url, _, error in
-                if error != nil {
-                    completion(.failure(ReferralsFailure.failedToCreateLink))
-                } else if let url = url {
-                    completion(.success(url.absoluteString))
-                }
-            }
-
-            return NilDisposer()
-        }
     }
 }
 
@@ -133,11 +79,11 @@ extension Referrals: Presentable {
 
         let linkSignal = ReadWriteSignal<String?>(nil)
 
-        bag += client.fetch(query: MemberIdQuery()).valueSignal.compactMap {
-            $0.data?.member.id
-        }.onValue { memberId in
-            bag += self.createInvitationLink(memberId: memberId).bindTo(linkSignal)
-        }
+//        bag += client.fetch(query: MemberIdQuery()).valueSignal.compactMap {
+//            $0.data?.member.id
+//        }.onValue { memberId in
+//            bag += self.createInvitationLink(memberId: memberId).bindTo(linkSignal)
+//        }
 
         let button = LoadableButton(
             button: Button(
@@ -160,11 +106,11 @@ extension Referrals: Presentable {
             bag += button.onTapSignal.withLatestFrom(
                 linkSignal.plain()
             ).compactMap { $1 }.onValue { link in
-                let incentive = String(self.remoteConfigContainer.referralsIncentive())
-                let shareMessage = String(key: .REFERRALS_SHARE_MESSAGE(incentive: incentive, link: link))
+                //let incentive = String(self.remoteConfigContainer.referralsIncentive())
+                //let shareMessage = String(key: .REFERRALS_SHARE_MESSAGE(incentive: incentive, link: link))
 
                 let activityView = ActivityView(
-                    activityItems: [shareMessage],
+                    activityItems: [""],
                     applicationActivities: nil,
                     sourceView: buttonView,
                     sourceRect: buttonView.bounds
