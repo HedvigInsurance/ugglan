eebc5ce717e4d0ef53f9792e42740f8d5df3909a
Gustaf Gunér <gustaf@guner.se>
2019-06-26 09:52:49 +0200

    Remove comment

--- Src/Generic/Viewables/RemoteVectorIcon.swift
+++ Src/Generic/Viewables/RemoteVectorIcon.swift
@@ -94,15 +94,13 @@ extension RemoteVectorIcon: Viewable {
                 self.finishedLoadingCallback.callAll()
             }
         }
-        /*
+        
         bag += imageView.didLayoutSignal.map { return imageView.bounds.size }.filter { $0.width != 0 && $0.height != 0 }.distinct()
             .withLatestFrom(pdfDocumentSignal.atOnce().plain().compactMap { $0 })
             .onValue { _, pdfDocument in
-                print("hej")
-                print(imageView.bounds.size)
                 renderPdfDocument(pdfDocument: pdfDocument)
             }
-        */
+        
         bag += pdfDocumentSignal.compactMap { $0 }.onValue { pdfDocument in
             renderPdfDocument(pdfDocument: pdfDocument)
         }
