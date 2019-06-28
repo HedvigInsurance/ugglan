02e6a77e5cb700565f7c43ed7f36381ca657c2f1
Gustaf Gunér <gustaf@guner.se>
2019-06-28 12:02:07 +0200

    Welcome screen

--- Src/Screens/WelcomeScreen/Viewables/WelcomePager.swift
+++ Src/Screens/WelcomeScreen/Viewables/WelcomePager.swift
@@ -1,8 +1,8 @@
 //
-//  WhatsNewPager.swift
+//  WelcomePager.swift
 //  project
 //
-//  Created by Gustaf Gunér on 2019-06-12.
+//  Created by Gustaf Gunér on 2019-06-28.
 //
 
 import Flow
@@ -11,64 +11,64 @@ import Foundation
 import Presentation
 import UIKit
 
-struct WhatsNewPager {
-    let dataSignal = ReadWriteSignal<WhatsNewQuery.Data?>(nil)
+struct WelcomePager {
+    let dataSignal = ReadWriteSignal<WelcomeQuery.Data?>(nil)
     let scrollToNextCallbacker: Callbacker<Void>
     let scrolledToPageIndexCallbacker: Callbacker<Int>
     let scrolledToEndCallbacker: Callbacker<Void>
     let presentingViewController: UIViewController
 }
 
-extension WhatsNewPager: Viewable {
+extension WelcomePager: Viewable {
     func materialize(events _: ViewableEvents) -> (UIView, Disposable) {
         let bag = DisposeBag()
         let view = UIStackView()
-
+        
         let scrollToNextSignal = scrollToNextCallbacker.signal()
-
+        
         let pager = Pager(
             scrollToNextSignal: scrollToNextSignal,
             scrolledToPageIndexCallbacker: scrolledToPageIndexCallbacker
         )
-
+        
         bag += view.addArranged(pager) { collectionView in
             bag += collectionView.contentOffsetSignal.onValue { point in
                 let slidesScrolledThrough = point.x / collectionView.frame.width + 1
                 let amountOfRealSlides = CGFloat(pager.dataSignal.value.count - 1)
-
+                
                 if slidesScrolledThrough > amountOfRealSlides {
                     let position = slidesScrolledThrough - amountOfRealSlides
                     self.presentingViewController.view.alpha = 1 - position
-
+                    
                     if position == 1 {
                         self.scrolledToEndCallbacker.callAll()
                     }
                 }
             }
         }
-
+        
         bag += dataSignal
             .atOnce()
-            .compactMap { $0?.news }
-            .onValue { news in
-                var newsPagerScreens = news.map { newsPost -> PagerScreen in
-                    let whatsNewPagerScreen = WhatsNewPagerScreen(
-                        title: newsPost.title,
-                        paragraph: newsPost.paragraph,
-                        iconUrl: newsPost.illustration.pdfUrl
+            .compactMap { $0?.welcome }
+            .onValue { welcome in
+                var welcomePagerScreens = welcome.map { welcomePost -> PagerScreen in
+                    let welcomePagerScreen = WelcomePagerScreen(
+                        title: welcomePost.title,
+                        paragraph: welcomePost.paragraph,
+                        iconUrl: welcomePost.illustration.pdfUrl
                     )
-
+                    
                     return PagerScreen(
                         id: UUID(),
-                        content: AnyPresentable(whatsNewPagerScreen)
+                        content: AnyPresentable(welcomePagerScreen)
                     )
                 }
-
-                newsPagerScreens.append(PagerScreen(id: UUID(), content: AnyPresentable(DummyPagerScreen())))
-
-                pager.dataSignal.value = newsPagerScreens
-            }
-
+                
+                welcomePagerScreens.append(PagerScreen(id: UUID(), content: AnyPresentable(DummyPagerScreen())))
+                
+                pager.dataSignal.value = welcomePagerScreens
+        }
+        
         return (view, bag)
     }
 }
---
+++ Src/Screens/WelcomeScreen/Viewables/WelcomePagerProceedButton.swift
@@ -0,0 +1,81 @@
+//
+//  WelcomePagerProceedButton.swift
+//  project
+//
+//  Created by Gustaf Gunér on 2019-06-28.
+//
+
+import Flow
+import Form
+import Foundation
+
+struct WelcomePagerProceedButton {
+    let button: Button
+    let onTapSignal: Signal<Void>
+    private let onTapReadWriteSignal = ReadWriteSignal<Void>(())
+    
+    let pageAmountSignal: ReadWriteSignal<Int> = ReadWriteSignal(0)
+    let dataSignal: ReadWriteSignal<WelcomeQuery.Data?> = ReadWriteSignal(nil)
+    let onScrolledToPageIndexSignal = ReadWriteSignal<Int>(0)
+    
+    init(button: Button) {
+        self.button = button
+        onTapSignal = onTapReadWriteSignal.plain()
+    }
+}
+
+extension WelcomePagerProceedButton: Viewable {
+    func materialize(events: ViewableEvents) -> (UIButton, Disposable) {
+        let bag = DisposeBag()
+        let (buttonView, disposable) = button.materialize(events: events)
+        buttonView.alpha = 0
+        
+        let buttonTitleSignal = ReadWriteSignal<String>("")
+        
+        func setButtonStyle(isMorePages: Bool) {
+            button.type.value = isMorePages ? ButtonType.standard(backgroundColor: .blackPurple, textColor: .white) : ButtonType.standard(backgroundColor: .purple, textColor: .white)
+        }
+        
+        func setButtonTitle(isMorePages: Bool) {
+            buttonTitleSignal.value = isMorePages ? String(key: .REFERRAL_RECIEVER_WELCOME_BTN_SKIP) : String(key: .REFERRAL_RECIEVER_WELCOME_BTN_CTA)
+        }
+        
+        bag += button.onTapSignal.bindTo(onTapReadWriteSignal)
+        
+        bag += buttonTitleSignal
+            .distinct()
+            .delay(by: 0.25)
+            .animated(style: SpringAnimationStyle.lightBounce(duration: 0.15)) { title in
+                buttonView.setTitle(title)
+                
+                buttonView.snp.remakeConstraints { make in
+                    make.width.equalTo(buttonView.intrinsicContentSize.width + self.button.type.value.extraWidthOffset())
+                }
+                
+                buttonView.layoutIfNeeded()
+        }
+        
+        bag += pageAmountSignal
+            .take(first: 1)
+            .onValue { pageAmount in
+                let isMorePages = pageAmount > 1
+                
+                setButtonTitle(isMorePages: isMorePages)
+                setButtonStyle(isMorePages: isMorePages)
+                
+                buttonView.alpha = 1
+        }
+        
+        bag += onScrolledToPageIndexSignal.withLatestFrom(pageAmountSignal).onValue { pageIndex, pageAmount in
+            let isMorePages = pageIndex < (pageAmount - 1)
+            
+            setButtonTitle(isMorePages: isMorePages)
+            setButtonStyle(isMorePages: isMorePages)
+        }
+        
+        return (buttonView, Disposer {
+            disposable.dispose()
+            bag.dispose()
+        })
+    }
+}
