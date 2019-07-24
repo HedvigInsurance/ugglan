28c8bcb0174ccd558197f8b9586cf5138c5b05bf
Sam Pettersson <sam@sampettersson.com>
2019-07-24 15:27:10 +0200

    Toast fixes

--- Src/Screens/ChatScreen/Chat.swift
+++ Src/Screens/ChatScreen/Chat.swift
@@ -29,18 +29,6 @@ extension Chat: Presentable {
         let view = UIView()
         view.backgroundColor = .purple
 
-        let dummyButton = Button(title: "yoo", type: .outline(borderColor: .white, textColor: .white))
-        bag += view.add(dummyButton) { dummyButton in
-            dummyButton.snp.makeConstraints { make in
-                make.width.equalToSuperview()
-                make.height.equalToSuperview()
-            }
-        }
-
-        bag += dummyButton.onTapSignal.onValue { _ in
-            UIApplication.shared.appDelegate.createToast(symbol: .character("🤖"), body: "HELLO BITCH")
-        }
-
         viewController.view = view
 
         return (viewController, Future { _ in
