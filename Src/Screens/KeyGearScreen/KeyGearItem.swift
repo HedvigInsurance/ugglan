dfddd6b6d932e3894a96d9b271b36467b009d48b
Sam Pettersson <sam@sampettersson.com>
2020-01-30 16:43:21 +0100

    Add EditableRow to KeyGearItem screen

--- Src/Screens/KeyGearScreen/KeyGearItem.swift
+++ Src/Screens/KeyGearScreen/KeyGearItem.swift
@@ -16,6 +16,10 @@ struct KeyGearItem {
     let name: String
     
     class KeyGearItemViewController: UIViewController {
+        override var preferredStatusBarStyle: UIStatusBarStyle {
+            return .lightContent
+        }
+        
         override func viewWillAppear(_ animated: Bool) {
             self.navigationController!.navigationBar.setBackgroundImage(UIImage(), for: .default)
             self.navigationController!.navigationBar.setBackgroundImage(UIImage(), for: .compact)
@@ -37,9 +41,11 @@ extension KeyGearItem: Presentable {
         scrollView.backgroundColor = .primaryBackground
         
         let form = FormView()
+        form.spacing = 20
+        
         bag += form.didLayoutSignal.take(first: 1).onValue { _ in
-            form.dynamicStyle = DynamicFormStyle.defaultGrouped.restyled({ (style: inout FormStyle) in
-                style.insets = UIEdgeInsets(top: -(scrollView.safeAreaInsets.top), left: 0, bottom: 0, right: 0)
+            form.dynamicStyle = DynamicFormStyle.default.restyled({ (style: inout FormStyle) in
+                style.insets = UIEdgeInsets(top: -(scrollView.safeAreaInsets.top), left: 20, bottom: 20, right: 20)
             })
         }
         
@@ -50,7 +56,15 @@ extension KeyGearItem: Presentable {
                 let realOffset = offset.y + scrollView.safeAreaInsets.top
                 
                 if realOffset < 0 {
-                    imageCarouselView.transform = CGAffineTransform(translationX: 0, y: realOffset * 0.5).concatenating(CGAffineTransform(scaleX: 1 + abs(realOffset / imageCarouselView.frame.height), y: 1 + abs(realOffset / imageCarouselView.frame.height)))
+                    imageCarouselView.transform = CGAffineTransform(
+                        translationX: 0,
+                        y: realOffset * 0.5
+                    ).concatenating(
+                        CGAffineTransform(
+                            scaleX: 1 + abs(realOffset / imageCarouselView.frame.height),
+                            y: 1 + abs(realOffset / imageCarouselView.frame.height)
+                        )
+                    )
                 } else {
                     imageCarouselView.transform = CGAffineTransform.identity
                 }
@@ -59,9 +73,10 @@ extension KeyGearItem: Presentable {
         }
         
         let section = form.appendSection()
+        section.dynamicStyle = .sectionPlain
         
-        Array(repeating: 1, count: 300).forEach { _ in
-            section.append(RowView(title: "Namn"))
+        bag += section.append(EditableRow(valueSignal: .static("Namn"), placeholderSignal: .static("Namn"))).onValue { _ in
+            print("was saved")
         }
         
         return (viewController, bag)
