0f2aafb4ee7c4ffad4288d28e3154e141f94c664
Sam Pettersson <sam@sampettersson.com>
2020-03-15 00:43:43 +0100

    Revert "Revert "Add important messages box""
    This reverts commit 41fe7fccc51a9da859cdf08a77c1b51bdde8f9a1.

--- Src/Screens/DashboardScreen/Dashboard.swift
+++ Src/Screens/DashboardScreen/Dashboard.swift
@@ -31,9 +31,12 @@ extension Dashboard: Presentable {
         containerStackView.spacing = 15
         containerStackView.isLayoutMarginsRelativeArrangement = true
         containerStackView.edgeInsets = UIEdgeInsets(horizontalInset: 0, verticalInset: 15)
-
+        
         let chatPreview = ChatPreview(presentingViewController: viewController)
         bag += containerStackView.addArranged(chatPreview)
+        
+        let importantMessagesSection = ImportantMessagesSection(presentingViewController: viewController)
+        bag += containerStackView.addArranged(importantMessagesSection)
 
         let renewalsSection = RenewalsSection(presentingViewController: viewController)
         bag += containerStackView.addArranged(renewalsSection)
