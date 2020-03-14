4739114ec7bd6f9dc1afaf9445672f75b0872a2e
Sam Pettersson <sam@sampettersson.com>
2020-03-14 23:43:06 +0100

    Add important messages box

---
+++ Src/Screens/DashboardScreen/Viewables/ImportantMessagesSection.swift
@@ -0,0 +1,126 @@
+//
+//  CoronaSection.swift
+//  Ugglan
+//
+//  Created by Sam Pettersson on 2020-03-14.
+//
+
+import Flow
+import Form
+import Foundation
+import UIKit
+import Space
+import ComponentKit
+import Apollo
+import Common
+import SafariServices
+
+struct ImportantMessagesSection {
+    let presentingViewController: UIViewController
+    @Inject var client: ApolloClient
+}
+
+extension ImportantMessagesSection {
+    struct Message: Viewable {
+        let presentingViewController: UIViewController
+        let title: String
+        let message: String
+        let buttonText: String
+        let link: String
+        
+        func materialize(events: ViewableEvents) -> (UIView, Disposable) {
+            let bag = DisposeBag()
+            
+            let containerView = UIView()
+            containerView.backgroundColor = UIColor.init(dynamic: { trait -> UIColor in
+                trait.userInterfaceStyle == .dark ? UIColor.hedvig(.yellow).darkened(amount: 0.25) : .hedvig(.yellow)
+            })
+            containerView.layer.cornerRadius = 8
+
+            let containerStackView = UIStackView()
+            containerStackView.axis = .vertical
+            containerStackView.spacing = 12
+            containerStackView.alignment = .fill
+            containerStackView.edgeInsets = UIEdgeInsets(top: 24, left: 24, bottom: 24, right: 24)
+
+            containerView.addSubview(containerStackView)
+
+            containerStackView.snp.makeConstraints { make in
+                make.height.width.centerX.centerY.equalToSuperview()
+            }
+
+            let titleLabel = MultilineLabel(value: title, style: TextStyle.rowTitleBold.centerAligned)
+            bag += containerStackView.addArranged(titleLabel)
+
+            let infoLabel = MarkdownText(textSignal: .static(message), style: TextStyle.bodyOffBlack.centerAligned)
+            bag += containerStackView.addArranged(infoLabel)
+
+            let buttonContainer = UIView()
+            let button = Button(
+                title: buttonText,
+                type: .outline(borderColor: .hedvig(.primaryText), textColor: .hedvig(.primaryText))
+            )
+            bag += buttonContainer.add(button) { buttonView in
+                buttonView.snp.makeConstraints { make in
+                    make.height.centerY.centerX.equalToSuperview()
+                }
+            }
+
+            bag += button.onTapSignal.onValue { _ in
+                if let url = URL(string: self.link) {
+                    self.presentingViewController.present(
+                        SFSafariViewController(url: url),
+                        animated: true,
+                        completion: nil
+                    )
+                }
+            }
+
+            containerStackView.addArrangedSubview(buttonContainer)
+
+            bag += containerView.didLayoutSignal.take(first: 1).onValue { _ in
+                containerView.snp.makeConstraints { make in
+                    make.trailing.leading.equalToSuperview().inset(16)
+                    make.top.equalToSuperview()
+                    make.bottom.equalToSuperview()
+                }
+            }
+            
+            return (containerView, bag)
+        }
+    }
+}
+
+extension ImportantMessagesSection: Viewable {
+    func materialize(events _: ViewableEvents) -> (UIView, Disposable) {
+        let bag = DisposeBag()
+
+        let wrapper = UIStackView()
+        wrapper.isHidden = true
+        wrapper.isLayoutMarginsRelativeArrangement = true
+
+        bag += client.fetch(query: ImportantMessagesQuery(languageCode: Localization.Locale.currentLocale.code)).valueSignal.compactMap {
+            $0.data?.importantMessages.compactMap { $0 }
+        }.onValueDisposePrevious { messages in
+            let innerBag = DisposeBag()
+            
+            innerBag += messages.map { message in
+                wrapper.addArranged(Message(
+                    presentingViewController: self.presentingViewController,
+                    title: message.title ?? "",
+                    message: message.message ?? "",
+                    buttonText: message.button ?? "",
+                    link: message.link ?? ""
+                ))
+            }
+            
+            innerBag += Signal(after: 0).animated(style: SpringAnimationStyle.lightBounce()) { _ in
+                wrapper.animationSafeIsHidden = false
+            }
+            
+            return innerBag
+        }
+
+        return (wrapper, bag)
+    }
+}
