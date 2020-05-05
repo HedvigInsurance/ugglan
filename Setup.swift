7a80d0d9b097f3de1b0be22c2300448d54f384d6
Sam Pettersson <sam@sampettersson.com>
2020-05-05 11:19:58 +0200

    Run translations before swiftgen

--- Setup.swift
+++ Setup.swift
@@ -4,6 +4,6 @@ let setup = Setup([
     .homebrew(packages: ["swiftlint", "carthage", "swiftformat"]),
     .custom(name: "Carthage", meet: ["./scripts/carthage.sh"], isMet: ["Carthage"]),
     .custom(name: "Apollo Codegen", meet: ["./scripts/codegen.sh"], isMet: ["exit 1"]),
-    .custom(name: "Swiftgen", meet: ["./scripts/swiftgen.sh"], isMet: ["exit 1"]),
-    .custom(name: "Translations", meet: ["./scripts/translations.sh"], isMet: ["exit 1"])
+    .custom(name: "Translations", meet: ["./scripts/translations.sh"], isMet: ["exit 1"]),
+    .custom(name: "Swiftgen", meet: ["./scripts/swiftgen.sh"], isMet: ["exit 1"])
 ])
