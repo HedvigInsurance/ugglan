5405f131ace40acfde2a03c5235b15d48776fe93
Sam Pettersson <sam@sampettersson.com>
2020-05-06 09:00:48 +0200

    test

--- Setup.swift
+++ Setup.swift
@@ -4,6 +4,6 @@ let setup = Setup([
     .homebrew(packages: ["swiftlint", "carthage", "swiftformat"]),
     .custom(name: "Install Git Hooks", meet: ["./scripts/githooks.sh"], isMet: ["exit 1"]),
     .custom(name: "Carthage", meet: ["./scripts/carthage.sh"], isMet: ["Carthage"]),
-            .custom(name: "Translations", meet: ["./scripts/translations.sh"], isMet: ["exit 1"]),
+    .custom(name: "Translations", meet: ["./scripts/translations.sh"], isMet: ["exit 1"]),
     .custom(name: "Swiftgen", meet: ["./scripts/swiftgen.sh"], isMet: ["exit 1"]),
 ])
