cfd5c30f89fa18b6c8893abdfa2652c0eb18e953
Sam Pettersson <sam@sampettersson.com>
2020-05-14 13:11:36 +0200

    goBackPanGesture

--- Projects/Embark/Sources/Viewables/Passage.swift
+++ Projects/Embark/Sources/Viewables/Passage.swift
@@ -9,6 +9,7 @@ import Foundation
 import Flow
 import UIKit
 import hCore
+import Form
 
 struct Passage {
     let store: EmbarkStore
@@ -18,6 +19,69 @@ struct Passage {
 }
 
 extension Passage: Viewable {
+    func goBackPanGesture(_ view: UIView, actionView: UIView) -> Disposable {
+        let bag = DisposeBag()
+        
+        let panGestureRecognizer = UIPanGestureRecognizer()
+        let hasSentFeedback = ReadWriteSignal(false)
+        
+        let releaseToGoBackLabel = UILabel(value: "Release to go back", style: TextStyle.chatTimeStamp
+            .centerAligned)
+        releaseToGoBackLabel.alpha = 0
+        
+        view.addSubview(releaseToGoBackLabel)
+        
+        releaseToGoBackLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).inset(-20)
+            make.left.right.equalToSuperview()
+        }
+        
+        bag += panGestureRecognizer.signal(forState: .began).onValue({ _ in
+            if panGestureRecognizer.translation(in: view).y < 0 {
+                panGestureRecognizer.state = .ended
+            }
+        })
+        
+        bag += panGestureRecognizer
+            .signal(forState: .changed)
+            .filter(predicate: { _ in self.canGoBackSignal.value })
+            .onValue { _ in
+            let translationY = max(panGestureRecognizer.translation(in: view).y, (panGestureRecognizer.translation(in: view).y / 25))
+            view.transform = CGAffineTransform(translationX: 0, y: min(translationY, 50 + (translationY / 25)))
+
+            releaseToGoBackLabel.alpha = (translationY - 35) / 50
+            releaseToGoBackLabel.transform = CGAffineTransform(translationX: 0, y: -min(0, 50 + (translationY / 100)))
+             actionView.transform = CGAffineTransform(translationX: 0, y: -min(translationY, 50 + (translationY / 100)))
+
+                
+            if translationY > 50, hasSentFeedback.value == false {
+                hasSentFeedback.value = true
+                bag += Signal(after: 0).feedback(type: .impactLight)
+            } else if translationY < 50 {
+                hasSentFeedback.value = false
+            }
+        }
+        
+        bag += panGestureRecognizer
+            .signal(forState: .ended)
+            .filter(predicate: { _ in self.canGoBackSignal.value })
+            .animated(style: SpringAnimationStyle.heavyBounce()) { _ in
+            hasSentFeedback.value = false
+            if panGestureRecognizer.translation(in: view).y > 40 {
+                self.goBackSignal.value = true
+                bag += Signal(after: 0).feedback(type: .selection)
+            }
+             
+            releaseToGoBackLabel.alpha = 0
+            view.transform = CGAffineTransform(translationX: 0, y: 0)
+        }
+        
+        view.addGestureRecognizer(panGestureRecognizer)
+        
+        return bag
+    }
+    
+    
     func materialize(events: ViewableEvents) -> (UIView, Signal<EmbarkLinkFragment>) {
         let view = UIStackView()
         view.axis = .vertical
@@ -63,9 +127,10 @@ extension Passage: Viewable {
             view.layoutIfNeeded()
         })
         
-        
         return (view, Signal { callback in
-            bag += view.addArranged(action).onValue(callback)
+            bag += view.addArranged(action) { actionView in
+                bag += self.goBackPanGesture(view, actionView: actionView)
+            }.onValue(callback)
             return bag
         })
     }
