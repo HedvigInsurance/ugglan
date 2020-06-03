f0a75286f6d48dbf0d181acacfc5961004902775
Sam Pettersson <sam@sampettersson.com>
2020-06-03 10:46:48 +0200

    Add test for ImageTextAction

---
+++ Projects/hCoreUI/Tests/ImageTextActionTests.swift
@@ -0,0 +1,62 @@
+//
+//  ImageTextActionTests.swift
+//  hCoreUITests
+//
+//  Created by sam on 3.6.20.
+//  Copyright © 2020 Hedvig AB. All rights reserved.
+//
+
+import Foundation
+import XCTest
+@testable import hCoreUI
+import Testing
+import SnapshotTesting
+import Form
+import Flow
+
+final class ImageTextActionTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+        setupScreenShotTests()
+        DefaultStyling.installCustom()
+    }
+    
+    func test() {
+        let imageTextAction = ImageTextAction<Void>(
+            image: .init(image: hCoreUIAssets.wordmark.image),
+            title: "mock",
+            body: "a long body",
+            actions: [((), Button(title: "I am a button", type: .standard(backgroundColor: .brand(.primaryButtonBackgroundColor), textColor: .brand(.primaryButtonTextColor))))],
+            showLogo: false
+        )
+       
+        let viewController = UIViewController()
+        let bag = DisposeBag()
+        
+        bag += viewController.install(imageTextAction).nil()
+        
+        assertSnapshot(matching: viewController, as: .image(on: .iPhoneX))
+        
+        bag.dispose()
+    }
+    
+    func testWithLogo() {
+        let imageTextAction = ImageTextAction<Void>(
+            image: .init(image: hCoreUIAssets.wordmark.image),
+            title: "mock",
+            body: "a long body",
+            actions: [((), Button(title: "I am a button", type: .standard(backgroundColor: .brand(.primaryButtonBackgroundColor), textColor: .brand(.primaryButtonTextColor))))],
+            showLogo: true
+        )
+       
+        let viewController = UIViewController()
+        let bag = DisposeBag()
+        
+        bag += viewController.install(imageTextAction).nil()
+        
+        assertSnapshot(matching: viewController, as: .image(on: .iPhoneX))
+        
+        bag.dispose()
+    }
+}
+
