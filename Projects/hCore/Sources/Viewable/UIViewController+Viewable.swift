d6ec4d4d6000956f57d10d0bafd93a211b6c00b4
Sam Pettersson <sam@sampettersson.com>
2020-06-03 11:12:00 +0200

    Fix ambiguity

--- Projects/hCore/Sources/Viewable/UIViewController+Viewable.swift
+++ Projects/hCore/Sources/Viewable/UIViewController+Viewable.swift
@@ -11,23 +11,54 @@ import UIKit
 import Flow
 
 extension UIViewController {
-    public func install<View: Viewable, Matter: UIView, SignalKind, SignalValue>(
-        _ viewable: View,
-        onCreate: (_ view: View.Matter) -> Void = defaultOnCreateClosure
-    ) -> CoreSignal<SignalKind, SignalValue> where
-        View.Matter == Matter,
-        View.Result == CoreSignal<SignalKind, SignalValue>,
-        View.Events == ViewableEvents {
-            let wasAddedCallbacker = Callbacker<Void>()
-            
-            let (matter, result) = viewable.materialize(events: ViewableEvents(wasAddedCallbacker: wasAddedCallbacker))
-                        
-            view = matter
-            
-            onCreate(matter)
-            
-            wasAddedCallbacker.callAll()
-            
-            return result
+   public func install<V: Viewable, View: UIView>(
+        _ viewable: V
+    ) -> Disposable where V.Matter == View, V.Result == Disposable, V.Events == ViewableEvents {
+        let wasAddedCallbacker = Callbacker<Void>()
+        let viewableEvents = ViewableEvents(
+            wasAddedCallbacker: wasAddedCallbacker
+        )
+        let (matter, result) = viewable.materialize(events: viewableEvents)
+
+        view = matter
+
+        wasAddedCallbacker.callAll()
+
+        return result
+    }
+
+    public func install<V: Viewable, View: UIView, SignalKind, SignalValue>(
+        _ viewable: V,
+        onInstall: (_ view: View) -> Void = { _ in }
+    ) -> V.Result where V.Matter == View, V.Result == CoreSignal<SignalKind, SignalValue>, V.Events == ViewableEvents {
+        let wasAddedCallbacker = Callbacker<Void>()
+        let viewableEvents = ViewableEvents(
+            wasAddedCallbacker: wasAddedCallbacker
+        )
+        let (matter, result) = viewable.materialize(events: viewableEvents)
+
+        view = matter
+
+        onInstall(matter)
+
+        wasAddedCallbacker.callAll()
+
+        return result
+    }
+
+    public func install<V: Viewable, View: UIView, FutureResult: Any>(
+        _ viewable: V
+    ) -> Future<FutureResult> where V.Matter == View, V.Result == Future<FutureResult>, V.Events == ViewableEvents {
+        let wasAddedCallbacker = Callbacker<Void>()
+        let viewableEvents = ViewableEvents(
+            wasAddedCallbacker: wasAddedCallbacker
+        )
+        let (matter, result) = viewable.materialize(events: viewableEvents)
+
+        view = matter
+
+        wasAddedCallbacker.callAll()
+
+        return result
     }
 }
