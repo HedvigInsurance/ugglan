1e83f697196905a13ea23f4106aa18595dcc5c9b
Sam Pettersson <sam@sampettersson.com>
2020-06-03 16:16:48 +0200

    Dont run hooks script in CI

--- scripts/githooks.sh
+++ scripts/githooks.sh
@@ -1,1 +1,4 @@
-sh -c "$(curl -fsSL https://r.viktoradam.net/githooks)" -- --non-interactive --single
+if test -z "$CI"
+then
+    sh -c "$(curl -fsSL https://r.viktoradam.net/githooks)" -- --non-interactive
+fi
