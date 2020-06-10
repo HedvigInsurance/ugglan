eb8bb89bf5d93ffacde5516bd7dacfa4f4f7d578
Sam Pettersson <sam@sampettersson.com>
2020-06-10 15:43:18 +0200

    Fix bash misstake

--- scripts/build_copy.sh
+++ scripts/build_copy.sh
@@ -9,7 +9,7 @@ frameworks=$(find "${mainDir}" -name '*.framework')
 
 for file in $frameworks
 do
-    if [ ! -d "${SRCROOT}/../../Carthage/Build/iOS/$(basename -- $file)" && $file != *".app"* ]; then
+    if [ ! -d "${SRCROOT}/../../Carthage/Build/iOS/$(basename -- $file)" ] && [ $file != *".app"* ]; then
         rsync --progress -a -u -v "${file}" "${CONFIGURATION_BUILD_DIR}"
     else
         echo "Skipping $file as it seems to be a Carthage dependency or nested inside an app bundle!"
