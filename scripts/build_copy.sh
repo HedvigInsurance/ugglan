f15c1ce5870e8dd0120b10e2c39c5b347ce3cc67
Sam Pettersson <sam@sampettersson.com>
2020-06-10 15:33:19 +0200

    Enhance bash script

--- scripts/build_copy.sh
+++ scripts/build_copy.sh
@@ -5,9 +5,13 @@ set -o pipefail
 
 mainDir=${CONFIGURATION_BUILD_DIR}/../
 
-frameworks=$(find "${mainDir}" -name '*.framework' -not \( -path "*.app" -prune \))
+frameworks=$(find "${mainDir}" -name '*.framework')
 
 for file in $frameworks
 do
-  rsync --progress -a -u -v "${file}" "${CONFIGURATION_BUILD_DIR}"
+    if [ ! -d "${SRCROOT}/../../Carthage/Build/iOS/$(basename -- $file)" && $file != *".app"* ]; then
+        rsync --progress -a -u -v "${file}" "${CONFIGURATION_BUILD_DIR}"
+    else
+        echo "Skipping $file as it seems to be a Carthage dependency or nested inside an app bundle!"
+    fi
 done
