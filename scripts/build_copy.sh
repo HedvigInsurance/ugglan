cf37197c31a02eb365449ca7e076d12c7631e224
Sam Pettersson <sam@sampettersson.com>
2020-06-10 16:10:24 +0200

    Actually ignore files

--- scripts/build_copy.sh
+++ scripts/build_copy.sh
@@ -9,7 +9,7 @@ frameworks=$(find "${mainDir}" -name '*.framework')
 
 for file in $frameworks
 do
-    if [ ! -d "${SRCROOT}/../../Carthage/Build/iOS/$(basename -- $file)" ] && [ $file != *".app"* ] && [ $file != *"XC"* ]; then
+    if [ ! -d "${SRCROOT}/../../Carthage/Build/iOS/$(basename -- $file)" ] && [[ $file != *".app"* ]] && [[ $file != *"XCTest.framework"* ]] && [[ $file != *"XCTAutomationSupport.framework"* ]]; then
         rsync --progress -a -u -v "${file}" "${CONFIGURATION_BUILD_DIR}"
     else
         echo "Skipping $file as it seems to be a Carthage dependency, nested inside an app bundle or being XC*!"
