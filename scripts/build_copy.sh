10d3da5c64d79859d8c6131f0650e54f330da254
Sam Pettersson <sam@sampettersson.com>
2020-06-10 16:05:06 +0200

    More bash script fixes

--- scripts/build_copy.sh
+++ scripts/build_copy.sh
@@ -9,9 +9,9 @@ frameworks=$(find "${mainDir}" -name '*.framework')
 
 for file in $frameworks
 do
-    if [ ! -d "${SRCROOT}/../../Carthage/Build/iOS/$(basename -- $file)" ] && [ $file != *".app"* ]; then
+    if [ ! -d "${SRCROOT}/../../Carthage/Build/iOS/$(basename -- $file)" ] && [ $file != *".app"* ] && [ $file != *"XC"* ]; then
         rsync --progress -a -u -v "${file}" "${CONFIGURATION_BUILD_DIR}"
     else
-        echo "Skipping $file as it seems to be a Carthage dependency or nested inside an app bundle!"
+        echo "Skipping $file as it seems to be a Carthage dependency, nested inside an app bundle or being XC*!"
     fi
 done
