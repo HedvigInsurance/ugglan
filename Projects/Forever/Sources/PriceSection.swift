a7f38d6f7e31fec7a64b75c849c55583d7021685
Sam Pettersson <sam@sampettersson.com>
2020-06-24 09:57:20 +0200

    Currency fix

--- Projects/Forever/Sources/PriceSection.swift
+++ Projects/Forever/Sources/PriceSection.swift
@@ -36,8 +36,14 @@ extension PriceSection: Viewable {
 
         discountStackView.addArrangedSubview(UILabel(value: L10n.ReferralsActive.Discount.Per.Month.title, style: .brand(.footnote(color: .tertiary))))
         bag += discountStackView.addArranged(AnimatedSavingsLabel(
-            from: combineLatest(grossAmountSignal, netAmountSignal).filter { grossAmount, netAmount in grossAmount != nil && netAmount != nil }.toVoid().map { _ in MonetaryAmount(amount: "0.00", currency: "SEK") }.readable(initial: nil).map { $0?.negative },
-            to: combineLatest(grossAmountSignal, netAmountSignal).map { grossAmount, netAmount in MonetaryAmount(amount: (grossAmount?.value ?? 0) - (netAmount?.value ?? 0), currency: grossAmount?.currency ?? "") }.map { $0.negative })
+            from: combineLatest(grossAmountSignal, netAmountSignal)
+                .filter { grossAmount, netAmount in grossAmount != nil && netAmount != nil }
+                .map { grossAmount, _ in MonetaryAmount(amount: "0.00", currency: grossAmount?.currency ?? "") }
+                .readable(initial: nil)
+                .map { $0?.negative },
+            to: combineLatest(grossAmountSignal, netAmountSignal)
+                .map { grossAmount, netAmount in MonetaryAmount(amount: (grossAmount?.value ?? 0) - (netAmount?.value ?? 0), currency: grossAmount?.currency ?? "") }
+                .map { $0.negative })
         )
 
         let netAmountStackView = UIStackView()
