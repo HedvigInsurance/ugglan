6421e283aa640b6f32f97e9e578fa52c743bd06d
Sam Pettersson <sam@sampettersson.com>
2020-08-17 15:06:04 +0200

    Fix lint violation

--- Setup.swift
+++ Setup.swift
@@ -15,5 +15,5 @@ let setup = Setup([
     !isCI() ? .custom(name: "Install Git Hooks", meet: ["./scripts/githooks.sh"], isMet: ["exit 0"]) : nil,
     .custom(name: "Translations", meet: ["./scripts/translations.sh"], isMet: ["exit 1"]),
     .custom(name: "Swiftgen", meet: ["./scripts/swiftgen.sh"], isMet: ["exit 1"]),
-    .custom(name: "Apollo Codegen", meet: ["./scripts/codegen.sh"], isMet: ["exit 1"]),
+    .custom(name: "Apollo Codegen", meet: ["./scripts/codegen.sh"], isMet: ["exit 1"])
 ].compactMap { $0 })
