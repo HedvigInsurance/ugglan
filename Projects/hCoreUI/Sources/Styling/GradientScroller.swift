b4cdf082e90f89ba21aba1dc269b44ce9f669a85
Sam Pettersson <sam@sampettersson.com>
2020-08-27 11:54:09 +0200

    makeGradientLayer before hand

--- Projects/hCoreUI/Sources/Styling/GradientScroller.swift
+++ Projects/hCoreUI/Sources/Styling/GradientScroller.swift
@@ -16,10 +16,97 @@ protocol GradientScroller where Self: UIScrollView {}
 let colorViewTag = 88888
 
 extension GradientScroller {
+    func makeGradientLayer(into bag: DisposeBag) -> CAGradientLayer {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.name = "gradientLayer"
+        gradientLayer.zPosition = -1
+        gradientLayer.locations = [0, 1]
+        gradientLayer.startPoint = CGPoint(x: 0.25, y: 0.5)
+        gradientLayer.endPoint = CGPoint(x: 0.75, y: 0.5)
+
+        let originalTransform = CGAffineTransform(a: 0, b: 1, c: -1, d: 0, tx: 1, ty: 0)
+        gradientLayer.transform = CATransform3DMakeAffineTransform(originalTransform)
+
+        layer.insertSublayer(gradientLayer, at: 0)
+
+        bag += {
+            gradientLayer.removeFromSuperlayer()
+        }
+
+        bag += combineLatest(
+            traitCollectionSignal.atOnce().plain(),
+            ContextGradient.$currentOption.atOnce().latestTwo()
+        ).onValue { traitCollection, option in
+            if #available(iOS 13.0, *) {
+                if traitCollection.userInterfaceLevel == .elevated {
+                    gradientLayer.isHidden = true
+                    return
+                } else {
+                    gradientLayer.isHidden = false
+                }
+            }
+
+            let (prevOption, option) = option
+
+            func optionToColors(_ option: ContextGradient.Option) -> [CGColor] {
+                if #available(iOS 13, *) {
+                    return option.colors(for: traitCollection).map { $0.resolvedColor(with: traitCollection).cgColor }
+                } else {
+                    return option.colors(for: traitCollection).map { $0.cgColor }
+                }
+            }
+
+            gradientLayer.locations = option.locations(for: traitCollection)
+            gradientLayer.colors = optionToColors(option)
+
+            let animation = CABasicAnimation(keyPath: "colors")
+            animation.fromValue = optionToColors(prevOption)
+            animation.toValue = optionToColors(option)
+            animation.duration = 1
+
+            gradientLayer.add(animation, forKey: "animateGradient")
+
+            let locationsAnimation = CABasicAnimation(keyPath: "locations")
+            locationsAnimation.fromValue = prevOption.locations(for: traitCollection)
+            locationsAnimation.toValue = option.locations(for: traitCollection)
+
+            let animationGroup = CAAnimationGroup()
+            animationGroup.animations = [animation, locationsAnimation]
+            animationGroup.duration = 1
+            animationGroup.fillMode = CAMediaTimingFillMode.forwards
+            animationGroup.isRemovedOnCompletion = true
+            animationGroup.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+
+            gradientLayer.add(animationGroup, forKey: "gradientAnimation")
+        }
+
+        bag += combineLatest(
+            signal(for: \.contentOffset).atOnce(),
+            signal(for: \.bounds).atOnce()
+        ).onValue { _, bounds in
+            CATransaction.begin()
+            CATransaction.setDisableActions(true)
+            let navigationBarHeight = self.viewController?.navigationController?.navigationBar.frame.height ?? 0
+
+            gradientLayer.transform = CATransform3DMakeAffineTransform(
+                originalTransform.concatenating(
+                    CGAffineTransform(translationX: 0, y: min(-navigationBarHeight, 0))
+                )
+            )
+            gradientLayer.frame = bounds
+
+            CATransaction.commit()
+        }
+
+        return gradientLayer
+    }
+
     func addGradient(into bag: DisposeBag) {
         guard bag.isEmpty else {
             return
         }
+
+        let gradientLayer = makeGradientLayer(into: bag)
 
         bag += didLayoutSignal
             .filter(predicate: { self.layer.sublayers?.first { $0.name == "gradientLayer" } == nil })
@@ -63,85 +150,10 @@ extension GradientScroller {
                         bag += ContextGradient.animateTabBarColor(colorView)
                     }
 
-                    let gradientLayer = CAGradientLayer()
-                    gradientLayer.name = "gradientLayer"
-                    gradientLayer.zPosition = -1
-                    gradientLayer.locations = [0, 1]
-                    gradientLayer.startPoint = CGPoint(x: 0.25, y: 0.5)
-                    gradientLayer.endPoint = CGPoint(x: 0.75, y: 0.5)
-
-                    let originalTransform = CGAffineTransform(a: 0, b: 1, c: -1, d: 0, tx: 1, ty: 0)
-                    gradientLayer.transform = CATransform3DMakeAffineTransform(originalTransform)
-
                     self.layer.insertSublayer(gradientLayer, at: 0)
 
                     bag += {
                         gradientLayer.removeFromSuperlayer()
-                    }
-
-                    bag += combineLatest(
-                        self.traitCollectionSignal.atOnce().plain(),
-                        ContextGradient.$currentOption.atOnce().latestTwo()
-                    ).onValue { traitCollection, option in
-                        if #available(iOS 13.0, *) {
-                            if traitCollection.userInterfaceLevel == .elevated {
-                                gradientLayer.isHidden = true
-                                return
-                            } else {
-                                gradientLayer.isHidden = false
-                            }
-                        }
-
-                        let (prevOption, option) = option
-
-                        func optionToColors(_ option: ContextGradient.Option) -> [CGColor] {
-                            if #available(iOS 13, *) {
-                                return option.colors(for: traitCollection).map { $0.resolvedColor(with: traitCollection).cgColor }
-                            } else {
-                                return option.colors(for: traitCollection).map { $0.cgColor }
-                            }
-                        }
-
-                        gradientLayer.locations = option.locations(for: traitCollection)
-                        gradientLayer.colors = optionToColors(option)
-
-                        let animation = CABasicAnimation(keyPath: "colors")
-                        animation.fromValue = optionToColors(prevOption)
-                        animation.toValue = optionToColors(option)
-                        animation.duration = 1
-
-                        gradientLayer.add(animation, forKey: "animateGradient")
-
-                        let locationsAnimation = CABasicAnimation(keyPath: "locations")
-                        locationsAnimation.fromValue = prevOption.locations(for: traitCollection)
-                        locationsAnimation.toValue = option.locations(for: traitCollection)
-
-                        let animationGroup = CAAnimationGroup()
-                        animationGroup.animations = [animation, locationsAnimation]
-                        animationGroup.duration = 1
-                        animationGroup.fillMode = CAMediaTimingFillMode.forwards
-                        animationGroup.isRemovedOnCompletion = true
-                        animationGroup.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
-
-                        gradientLayer.add(animationGroup, forKey: "gradientAnimation")
-                    }
-
-                    bag += combineLatest(
-                        self.signal(for: \.contentOffset).atOnce(),
-                        self.signal(for: \.bounds).atOnce()
-                    ).onValue { _, bounds in
-                        CATransaction.begin()
-                        CATransaction.setDisableActions(true)
-                        let navigationBarHeight = navigationController.navigationBar.frame.height
-
-                        gradientLayer.transform = CATransform3DMakeAffineTransform(
-                            originalTransform.concatenating(
-                                CGAffineTransform(translationX: 0, y: min(-navigationBarHeight, 0))
-                            )
-                        )
-                        gradientLayer.frame = bounds
-
-                        CATransaction.commit()
                     }
                 }
             }
