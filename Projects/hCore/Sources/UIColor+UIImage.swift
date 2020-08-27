e36a8ed54005f64f292bf63b7c346a877eefc60a
Sam Pettersson <sam@sampettersson.com>
2020-08-27 12:19:29 +0200

    Fix separator height

--- Projects/hCore/Sources/UIColor+UIImage.swift
+++ Projects/hCore/Sources/UIColor+UIImage.swift
@@ -10,11 +10,11 @@ import Foundation
 import UIKit
 
 extension UIColor {
-    public func asImage(_ size: CGSize = CGSize(width: 1, height: 1)) -> UIImage {
+    public func asImage(_ size: CGSize = CGSize(width: 1, height: 1 / UIScreen.main.scale)) -> UIImage {
         UIGraphicsBeginImageContext(size)
         let ctx = UIGraphicsGetCurrentContext()
         setFill()
-        ctx?.fill(CGRect(x: 0, y: 0, width: 1, height: 1))
+        ctx?.fill(CGRect(x: 0, y: 0, width: size.width, height: size.height))
         let image = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         return image!
