418769628810157520eaeabc2cd81e2217f6a8eb
Sam Pettersson <sam@sampettersson.com>
2020-09-16 11:22:46 +0200

    Add post-checkout githook

--- scripts/githooks.sh
+++ scripts/githooks.sh
@@ -1,2 +1,5 @@
 ln -sfn "${PWD}/.githooks/pre-commit" "${PWD}/.git/hooks/pre-commit"
 echo "Git pre-commit hook initialised"
+
+ln -sfn "${PWD}/.githooks/post-checkout" "${PWD}/.git/hooks/post-checkout"
+echo "Git post-checkout hook initialised"
