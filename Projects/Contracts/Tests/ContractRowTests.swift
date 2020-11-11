3b32c8da9c6ca19711de8e9af2454cbb4ec3d5b9
Sam Pettersson <sam@sampettersson.com>
2020-11-11 17:25:39 +0100

    Add tests for ContractRow

---
+++ Projects/Contracts/Tests/ContractRowTests.swift
@@ -0,0 +1,102 @@
+@testable import Contracts
+import ContractsTesting
+import Flow
+import Foundation
+import hCoreUI
+import SnapshotTesting
+import Testing
+import TestingUtil
+import XCTest
+
+final class ContractRowTests: XCTestCase {
+    let bag = DisposeBag()
+
+    override func setUp() {
+        super.setUp()
+        setupScreenShotTests()
+    }
+
+    func assert(_ row: ContractRow) {
+        let view = row.reuseType(bag: bag)
+
+        view.snp.makeConstraints { make in
+            make.width.equalTo(400)
+        }
+
+        assertSnapshot(
+            matching: view,
+            as: .image,
+            named: "\(row.displayName)_\(row.contract.status.__typename)"
+        )
+
+        view.overrideUserInterfaceStyle = .dark
+
+        assertSnapshot(
+            matching: view,
+            as: .image,
+            named: "\(row.displayName)_\(row.contract.status.__typename)_dark"
+        )
+    }
+
+    func testNorwegianHome() {
+        let activeContractRow = ContractRow(
+            contract: try! .init(jsonObject: .makeNorwegianHomeContentContract(status: .makeActiveStatus())),
+            displayName: "NorwegianHome",
+            type: .norwegianHome
+        )
+
+        assert(activeContractRow)
+
+        let activeInFutureContractRow = ContractRow(
+            contract: try! .init(jsonObject: .makeNorwegianHomeContentContract(status: .makeActiveInFutureStatus())),
+            displayName: "NorwegianHome",
+            type: .norwegianHome
+        )
+
+        assert(activeInFutureContractRow)
+
+        let pendingContractRow = ContractRow(
+            contract: try! .init(jsonObject: .makeNorwegianHomeContentContract(status: .makePendingStatus())),
+            displayName: "NorwegianHome",
+            type: .norwegianHome
+        )
+
+        assert(pendingContractRow)
+
+        let activeInFutureAndTerminatedInFutureContractRow = ContractRow(
+            contract: try! .init(jsonObject: .makeNorwegianHomeContentContract(status: .makeActiveInFutureAndTerminatedInFutureStatus())),
+            displayName: "NorwegianHome",
+            type: .norwegianHome
+        )
+
+        assert(activeInFutureAndTerminatedInFutureContractRow)
+
+        let terminatedContractRow = ContractRow(
+            contract: try! .init(jsonObject: .makeNorwegianHomeContentContract(status: .makeTerminatedStatus())),
+            displayName: "NorwegianHome",
+            type: .norwegianHome
+        )
+
+        assert(terminatedContractRow)
+    }
+
+    func testNorwegianTravel() {
+        let activeContractRow = ContractRow(
+            contract: try! .init(jsonObject: .makeNorwegianTravelContract(status: .makeActiveStatus())),
+            displayName: "NorwegianTravel",
+            type: .norwegianTravel
+        )
+
+        assert(activeContractRow)
+    }
+
+    func testSwedishHouse() {
+        let activeContractRow = ContractRow(
+            contract: try! .init(jsonObject: .makeSwedishHouseContract(status: .makeActiveStatus())),
+            displayName: "SwedishHouse",
+            type: .norwegianTravel
+        )
+
+        assert(activeContractRow)
+    }
+}
