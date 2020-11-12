8900529d8b6132f45f00a50468303638d51713d6
Sam Pettersson <sam@sampettersson.com>
2020-11-12 15:29:25 +0100

    Remove status from ImportantMessagesTestingData

--- Projects/Home/Testing/ImportantMessagesTestingData.swift
+++ Projects/Home/Testing/ImportantMessagesTestingData.swift
@@ -6,7 +6,7 @@ import Home
 extension JSONObject {
     public static func makeImportantMessages() -> JSONObject {
         GraphQL.ImportantMessagesQuery.Data(importantMessages: [
-            .init(id: "mock", status: .published, message: "Mock important message", link: "https://www.hedvig.com"),
+            .init(id: "mock", message: "Mock important message", link: "https://www.hedvig.com"),
         ]).jsonObject
     }
 }
