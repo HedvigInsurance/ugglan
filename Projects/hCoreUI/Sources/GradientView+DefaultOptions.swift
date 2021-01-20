101470946e9c0dfec02e1f9e22581b42598ab11a
Tarik Stafford <tarikstafford@Tariks-MacBook-Pro.local>
2021-01-20 18:56:42 +0100

    gradientView

--- Projects/hCoreUI/Sources/GradientView+DefaultOptions.swift
+++ Projects/hCoreUI/Sources/GradientView+DefaultOptions.swift
@@ -1,9 +1,131 @@
-//
-//  GradientView+DefaultOptions.swift
-//  hCoreUI
-//
-//  Created by Tarik Stafford on 2021-01-20.
-//  Copyright © 2021 Hedvig AB. All rights reserved.
-//
+import UIKit
+import CoreGraphics
+import Flow
 
-import Foundation
+extension GradientView {
+    public struct GradientOption {
+        public init(preset: GradientView.Preset, traitCollection: UITraitCollection) {
+            self.preset = preset
+            self.traitCollection = traitCollection
+        }
+        
+        public let preset: Preset
+        public let traitCollection: UITraitCollection
+        
+        public var orbLayer: CALayer {
+
+           
+
+            var colors = [UIColor]()
+
+            switch (preset, traitCollection.userInterfaceStyle) {
+
+            case (.insuranceOne, .light):
+                colors.append(UIColor(red: 0.937, green: 0.918, blue: 0.776, alpha: 1))
+            case (.insuranceOne, .dark):
+                colors.append(UIColor(red: 0.796, green: 0.71, blue: 0.514, alpha: 1))
+            case (.insuranceTwo, .light):
+                colors.append(UIColor(red: 0.886, green: 0.8, blue: 0.808, alpha: 1))
+            case (.insuranceTwo, .dark):
+                colors.append(UIColor(red: 0.776, green: 0.678, blue: 0.541, alpha: 1))
+            case (.insuranceThree, .light):
+                colors.append(UIColor(red: 0.973, green: 0.726, blue: 0.574, alpha: 1))
+            case (.insuranceThree, .dark):
+                colors.append(UIColor(red: 0.925, green: 0.584, blue: 0.374, alpha: 1))
+            default:
+                colors.append(.white)
+            }
+            
+            let alphaWhite = UIColor.white.withAlphaComponent(0.0)
+            colors.append(alphaWhite)
+            
+            let layer = CAGradientLayer()
+            layer.type = .radial
+            layer.colors = colors.map { $0.cgColor }
+            layer.locations = [0, 1.0]
+            layer.startPoint = CGPoint(x: 0.5, y: 0.5)
+            layer.endPoint = CGPoint(x: 1.0, y: 1.0)
+            return layer
+        }
+        
+        public var colors: [UIColor] {
+            switch (preset, traitCollection.userInterfaceStyle) {
+            
+            case (.insuranceOne, .light):
+                return [
+                    
+                    UIColor(red: 0.921, green: 0.825, blue: 0.834, alpha: 1),
+                    
+                    UIColor(red: 0.85, green: 0.82, blue: 0.946, alpha: 1)
+                    
+                ]
+            case (.insuranceOne, .dark):
+                return [
+                    
+                    UIColor(red: 0.416, green: 0.302, blue: 0.212, alpha: 1),
+                    
+                    UIColor(red: 0.247, green: 0.463, blue: 0.682, alpha: 1)
+                    
+                ]
+            case (.insuranceTwo, .light):
+                return [
+                    
+                    UIColor(red: 0.725, green: 0.686, blue: 0.89, alpha: 1),
+                    
+                    UIColor(red: 0.973, green: 0.725, blue: 0.573, alpha: 1)
+                    
+                ]
+            case (.insuranceTwo, .dark):
+                return [
+                    
+                    UIColor(red: 0.247, green: 0.463, blue: 0.682, alpha: 1),
+                    
+                    UIColor(red: 0.627, green: 0.467, blue: 0.325, alpha: 1)
+                    
+                ]
+            case (.insuranceThree, .light):
+                return [
+                    
+                    UIColor(red: 0.831, green: 0.812, blue: 0.8, alpha: 1),
+                    
+                    UIColor(red: 0.886, green: 0.8, blue: 0.808, alpha: 1)
+                    
+                ]
+                
+            case (.insuranceThree, .dark):
+                return [
+                    
+                    UIColor(red: 0.512, green: 0.326, blue: 0.162, alpha: 1),
+                    
+                    UIColor(red: 0.796, green: 0.481, blue: 0.481, alpha: 1)
+                    
+                ]
+            default:
+                return []
+            }
+        }
+    }
+    
+    public enum Preset {
+        
+        case insuranceOne
+        case insuranceTwo
+        case insuranceThree
+        
+        public var locations: [NSNumber] {
+            return [0,1]
+        }
+        
+        public var startPoint: CGPoint {
+            return CGPoint(x: 0.25, y: 0.5)
+        }
+        
+        public var endPoint: CGPoint {
+            return CGPoint(x: 0.75, y: 0.5)
+        }
+        
+        public var transform: CATransform3D {
+            CATransform3DMakeAffineTransform(CGAffineTransform(a: 1, b: 0, c: 0, d: 2.94, tx: 0, ty: -0.97))
+        }
+    }
+}
--- Projects/hCoreUI/Sources/GradientView.swift
+++ Projects/hCoreUI/Sources/GradientView.swift
@@ -5,5 +5,99 @@
 //  Created by Tarik Stafford on 2021-01-19.
 //  Copyright © 2021 Hedvig AB. All rights reserved.
 //
+import UIKit
+import Flow
+import hCore
 
-import Foundation
+public struct GradientView {
+    public init(locations: [NSNumber], startPoint: CGPoint, endPoint: CGPoint, transform: CATransform3D, colors: [UIColor], orbLayer: CALayer? = nil) {
+        self.locations = locations
+        self.startPoint = startPoint
+        self.endPoint = endPoint
+        self.transform = transform
+        self.colors = colors
+        self.orbLayer = orbLayer
+    }
+    
+    public init(gradientOption: GradientOption, signal: ReadWriteSignal<Bool>) {
+        self.locations = gradientOption.preset.locations
+        self.startPoint = gradientOption.preset.startPoint
+        self.endPoint = gradientOption.preset.endPoint
+        self.transform = gradientOption.preset.transform
+        self.colors = gradientOption.colors
+        self.orbLayer = gradientOption.orbLayer
+        self._shouldShowGradient = .init(wrappedValue: signal)
+    }
+    
+    public var locations: [NSNumber]
+    public var startPoint: CGPoint
+    public var endPoint: CGPoint
+    public var transform: CATransform3D
+    public var colors: [UIColor]
+    public var orbLayer: CALayer?
+    
+    @ReadWriteState public var shouldShowGradient = false
+}
+
+extension GradientView: Viewable {
+    
+    var gradientLayer: CAGradientLayer {
+        let layer = CAGradientLayer()
+        layer.colors = colors.map({ $0.cgColor })
+        layer.locations = locations
+        layer.startPoint = startPoint
+        layer.endPoint = endPoint
+        layer.transform = transform
+        return layer
+    }
+    
+    public func materialize(events: ViewableEvents) -> (UIView, Disposable) {
+        let bag = DisposeBag()
+        
+        let gradientView = UIView()
+        gradientView.isUserInteractionEnabled = false
+        
+        let orbContainerView = UIView()
+        orbContainerView.backgroundColor = .clear
+        gradientView.addSubview(orbContainerView)
+        orbContainerView.snp.makeConstraints { make in
+            make.height.width.equalTo(gradientView.snp.height)
+            make.centerX.equalTo(gradientView.snp.trailing)
+            make.centerY.equalTo(gradientView.snp.bottom)
+        }
+        
+        bag += $shouldShowGradient.atOnce().onValueDisposePrevious({ (shouldShow) -> Disposable? in
+            
+            let innerBag = DisposeBag()
+            
+            let layer = gradientLayer
+            
+            if shouldShow {
+                
+                gradientView.layer.addSublayer(layer)
+                
+                if let orbLayer = orbLayer {
+                    gradientView.bringSubviewToFront(orbContainerView)
+                    orbContainerView.layer.addSublayer(orbLayer)
+                }
+    
+                innerBag += gradientView.didLayoutSignal.onValue {
+                    layer.bounds = gradientView.layer.bounds
+                    layer.frame = gradientView.layer.frame
+                    layer.position = gradientView.layer.position
+                    orbLayer?.frame = orbContainerView.bounds
+                    orbLayer?.cornerRadius = orbContainerView.bounds.width / 2
+                }
+                
+                innerBag += {
+                    layer.removeFromSuperlayer()
+                    orbLayer?.removeFromSuperlayer()
+                }
+            }
+            
+            return innerBag
+        })
+        
+        return (gradientView, bag)
+    }
+}
