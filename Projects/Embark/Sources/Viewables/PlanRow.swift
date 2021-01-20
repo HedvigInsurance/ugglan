77faf40e617dfbf761662cda25f7078c43625405
Tarik Stafford <tarikstafford@Tariks-MacBook-Pro.local>
2021-01-20 18:57:54 +0100

    add plan row

--- Projects/Embark/Sources/Viewables/PlanRow.swift
+++ Projects/Embark/Sources/Viewables/PlanRow.swift
@@ -5,34 +5,123 @@ import hCore
 import hCoreUI
 import UIKit
 
-struct PlanRow: Hashable {
+struct PlanRow: Equatable, Hashable {
+    static func == (lhs: PlanRow, rhs: PlanRow) -> Bool {
+        lhs.hashValue == rhs.hashValue
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(title)
+        hasher.combine(discount)
+        hasher.combine(message)
+    }
+    
     let title: String
-    let discount: String
+    let discount: String?
     let message: String
+    let isSelected: ReadWriteSignal<Bool>
 }
 
 extension PlanRow: Reusable {
     
     static func makeAndConfigure() -> (make: UIView, configure: (PlanRow) -> Disposable) {
-        let titleLabel = UILabel(value: "", style: .brand(.title1(color: .primary)))
         
-        let discountLabel = UILabel(value: "", style: .brand(.title1(color: .primary)))
+        let view = UIStackView()
+        view.edgeInsets = UIEdgeInsets(horizontalInset: 15, verticalInset: 0)
+        view.insetsLayoutMarginsFromSafeArea = true
         
-        let containerView = UIStackView()
-        containerView.isLayoutMarginsRelativeArrangement = true
-        containerView.insetsLayoutMarginsFromSafeArea = false
-        containerView.axis = .vertical
-        containerView.layoutMargins = UIEdgeInsets(horizontalInset: 15, verticalInset: 10)
+        let contentView = UIControl()
+        contentView.layer.masksToBounds = true
+        contentView.layer.cornerRadius = .defaultCornerRadius
+        contentView.layer.borderWidth = .hairlineWidth
+        contentView.snp.makeConstraints { $0.height.greaterThanOrEqualTo(124) }
         
-        containerView.addArrangedSubview(titleLabel)
+        view.addArrangedSubview(contentView)
         
-        containerView.addArrangedSubview(discountLabel)
+        let horizontalContentContainer = UIStackView()
+        horizontalContentContainer.axis = .horizontal
+        horizontalContentContainer.spacing = 10
         
-        return (containerView, { `self` in
-            titleLabel.text = self.title
-            discountLabel.text = self.message
+       
+        let verticalContentContainer = UIStackView()
+        verticalContentContainer.isUserInteractionEnabled = false
+        verticalContentContainer.axis = .vertical
+        verticalContentContainer.distribution = .equalSpacing
+        verticalContentContainer.edgeInsets = UIEdgeInsets(top: 24, left: 16, bottom: 24, right: 16)
+        verticalContentContainer.spacing = 20
+        
+        verticalContentContainer.addArrangedSubview(horizontalContentContainer)
+        
+        let titleLabel = UILabel(value: "", style: .brand(.title2(color: .primary)))
+        titleLabel.setContentHuggingPriority(.required, for: .vertical)
+        horizontalContentContainer.addArrangedSubview(titleLabel)
+        
+        contentView.addSubview(verticalContentContainer)
+        
+        verticalContentContainer.snp.makeConstraints { $0.top.bottom.trailing.leading.equalToSuperview() }
+        
+        return (view, { `self` in
+            let bag = DisposeBag()
+        
+            titleLabel.value = self.title
             
-            return NilDisposer()
+            bag += contentView.applyBorderColor { _ in
+                .brand(.primaryBorderColor)
+            }
+            
+            bag += contentView.signal(for: .touchUpInside).map {
+                return true
+            }.bindTo(self.isSelected)
+            
+            let descriptionLabel = MultilineLabel(
+                value: self.message,
+                style: .brand(.body(color: .primary)))
+            
+            let checkmark = CheckMark(isSelectedSignal: self.isSelected)
+            
+            bag += verticalContentContainer.addArranged(descriptionLabel) { labelview in
+                labelview.setContentHuggingPriority(.defaultHigh, for: .vertical)
+            }
+            
+            let gradientView = GradientView(
+                gradientOption: .init(preset: .insuranceThree, traitCollection: view.traitCollection),
+                signal: self.isSelected)
+            
+            bag += contentView.add(gradientView) { view in
+                contentView.sendSubviewToBack(view)
+                view.snp.makeConstraints { (make) in
+                    make.top.bottom.trailing.leading.equalToSuperview()
+                }
+            }
+            
+            if let discount = self.discount {
+                let tintColor = UIColor.brand(.primaryBackground(true))
+                bag += horizontalContentContainer.addArranged(
+                    PillCollection(
+                        pills: [.make(
+                                    Pill(
+                                        tintColor: tintColor,
+                                        title: discount,
+                                        textStyle: .brand(.caption1(color: .primary(state: .matching(tintColor))))))
+                        ]
+                    ).wrappedIn({
+                        let stack = UIStackView()
+                        stack.axis = .vertical
+                        return stack
+                    }())
+                )
+            }
+            
+            horizontalContentContainer.addArrangedSubview(UIView())
+           
+            bag += horizontalContentContainer.addArranged(checkmark.wrappedIn({
+                let stackView = UIStackView()
+                stackView.axis = .vertical
+                stackView.setContentHuggingPriority(.required, for: .horizontal)
+                return stackView
+            }()))
+            
+            return bag
         })
     }
 }
