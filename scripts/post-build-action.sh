1510c7fee95c818fa164245f5fdbbc4ab8e29db5
Sam Pettersson <sampettersson@Sams-Mac-mini.local>
2021-01-25 14:58:22 +0100

    Stop building example apps for now

--- scripts/post-build-action.sh
+++ scripts/post-build-action.sh
@@ -1,1 +1,2 @@
-rm -rf "${TARGET_BUILD_DIR}/${TARGET_NAME}.app"/Frameworks/*.framework
+rm -rf "${TARGET_BUILD_DIR}/${TARGET_NAME}.framework"/Frameworks/FirebaseAnalytics.framework
+rm -rf "${TARGET_BUILD_DIR}/${TARGET_NAME}.framework"/Frameworks/GoogleAppMeasurement.framework
