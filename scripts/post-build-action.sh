29777df256452ad0c09c09e5504a1d4107f1c2b0
Sam Pettersson <sampettersson@Sams-Mac-mini.local>
2021-01-25 10:36:05 +0100

    Remove from plugins dir

--- scripts/post-build-action.sh
+++ scripts/post-build-action.sh
@@ -1,4 +1,1 @@
-rm -rf "${TARGET_BUILD_DIR}/${TARGET_NAME}.app/PlugIns/FirebaseAnalytics.framework"
-rm -rf "${TARGET_BUILD_DIR}/${TARGET_NAME}.app/PlugIns/GoogleAppMeasurement.framework"
-rm -rf "${TARGET_BUILD_DIR}/${TARGET_NAME}.app/Frameworks/FirebaseAnalytics.framework"
-rm -rf "${TARGET_BUILD_DIR}/${TARGET_NAME}.app/Frameworks/GoogleAppMeasurement.framework"
+rm -rf "${TARGET_BUILD_DIR}/${TARGET_NAME}.app"/PlugIns/*.framework
