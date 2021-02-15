37d8f548f10148c2b9c21338507343fc751db187
Sam Pettersson <sam@sampettersson.com>
2021-02-15 10:28:59 +0100

    Fix stuffs

---
+++ Projects/App/AppClip/Sources/AppClip.swift
@@ -0,0 +1,12 @@
+import hCoreUI
+import SwiftUI
+import UIKit
+
+@main
+struct AppClip: App {
+    var body: some Scene {
+        WindowGroup {
+            Image(uiImage: hCoreUIAssets.addButton.image)
+        }
+    }
+}
