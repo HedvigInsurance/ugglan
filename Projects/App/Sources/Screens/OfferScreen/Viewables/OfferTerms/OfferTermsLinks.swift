535cd865a7ac87a4fb4c6ebe4a8bc2d636950371
Tarik Stafford <tarikstafford@Tariks-MacBook-Pro.local>
2021-03-22 15:53:12 +0530

    fix typo

--- Projects/App/Sources/Screens/OfferScreen/Viewables/OfferTerms/OfferTermsLinks.swift
+++ Projects/App/Sources/Screens/OfferScreen/Viewables/OfferTerms/OfferTermsLinks.swift
@@ -24,44 +24,48 @@ extension OfferTermsLinks: Viewable {
             stackView.viewController?.present(SFSafariViewController(url: url), animated: true, completion: nil)
         }
 
-        bag += client.fetch(query: GraphQL.OfferQuery()).valueSignal.compactMap(\.lastQuoteOfMember).onValueDisposePrevious { lastQuoteOfMember in
-            let innerBag = DisposeBag()
+        bag += client
+            .fetch(query: GraphQL.OfferQuery())
+            .valueSignal
+            .compactMap { $0.lastQuoteOfMember }
+            .onValueDisposePrevious { lastQuoteOfMember in
+                let innerBag = DisposeBag()
 
-            lastQuoteOfMember.asCompleteQuote?.insuranceTerms.forEach { term in
+                lastQuoteOfMember.asCompleteQuote?.insuranceTerms.forEach { term in
 
-                var url: URL?
+                    var url: URL?
 
-                let isSwedishMarket = Localization.Locale.currentLocale.market == .se
+                    let isSwedishMarket = Localization.Locale.currentLocale.market == .se
 
-                if let termType = term.type,
-                   termType == .termsAndConditions, isSwedishMarket
-                {
-                    url = URL(string: "https://www.hedvig.com/se/villkor")
-                } else {
-                    url = URL(string: term.url)
-                }
-
-                if let url = url {
-                    let button = Button(title: term.displayName, type: .standard(backgroundColor: .lightGray, textColor: .black))
-
-                    innerBag += button.onTapSignal.onValue { _ in
-                        openUrl(url)
+                    if let termType = term.type,
+                       termType == .termsAndConditions, isSwedishMarket
+                    {
+                        url = URL(string: "https://www.hedvig.com/se/villkor")
+                    } else {
+                        url = URL(string: term.url)
                     }
 
-                    innerBag += stackView.addArranged(button.wrappedIn(UIStackView()))
+                    if let url = url {
+                        let button = Button(title: term.displayName, type: .standard(backgroundColor: .lightGray, textColor: .black))
+
+                        innerBag += button.onTapSignal.onValue { _ in
+                            openUrl(url)
+                        }
+
+                        innerBag += stackView.addArranged(button.wrappedIn(UIStackView()))
+                    }
                 }
-            }
 
-            if let privacyPolicyUrl = URL(string: L10n.privacyPolicyUrl) {
-                let button = Button(title: L10n.offerPrivacyPolicy, type: .standard(backgroundColor: .lightGray, textColor: .black))
+                if let privacyPolicyUrl = URL(string: L10n.privacyPolicyUrl) {
+                    let button = Button(title: L10n.offerPrivacyPolicy, type: .standard(backgroundColor: .lightGray, textColor: .black))
 
-                innerBag += button.onTapSignal.onValue { _ in
-                    openUrl(privacyPolicyUrl)
+                    innerBag += button.onTapSignal.onValue { _ in
+                        openUrl(privacyPolicyUrl)
+                    }
                 }
-            }
 
-            return innerBag
-        }
+                return innerBag
+            }
 
         return (stackView, bag)
     }
