b7f0a4b8ec5623eaa93886ce827e1ffbbec8f24a
Sam Pettersson <sam@sampettersson.com>
2021-03-22 10:12:00 +0100

    Fix build action

--- scripts/post-build-action.sh
+++ scripts/post-build-action.sh
@@ -1,4 +1,4 @@
-rm -rf "${TARGET_BUILD_DIR}/${TARGET_NAME}.app/Frameworks/*.framework"/Frameworks/FirebaseAnalytics.framework
-rm -rf "${TARGET_BUILD_DIR}/${TARGET_NAME}.app/Frameworks/*.framework"/Frameworks/GoogleAppMeasurement.framework
+rm -rf "${TARGET_BUILD_DIR}/${TARGET_NAME}.app/Frameworks/"*".framework"/Frameworks/FirebaseAnalytics.framework
+rm -rf "${TARGET_BUILD_DIR}/${TARGET_NAME}.app/Frameworks/"*".framework"/Frameworks/GoogleAppMeasurement.framework
 mv -iv -- "${TARGET_BUILD_DIR}/${TARGET_NAME}.app/Frameworks/"*".framework"/Frameworks/Adyen3DS2.framework "${TARGET_BUILD_DIR}/${TARGET_NAME}.app/Frameworks/"Adyen3DS2.framework
-rm -rf "${TARGET_BUILD_DIR}/${TARGET_NAME}.app/Frameworks/*.framework"/Frameworks
+rm -rf "${TARGET_BUILD_DIR}/${TARGET_NAME}.app/Frameworks/"*".framework"/Frameworks
