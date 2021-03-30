e50dbbc6d6d39bd8c4df532202fd6b2db8684422
Sam Pettersson <sam@sampettersson.com>
2021-03-30 11:19:21 +0200

    Remove auto recording

--- Dependencies/CoreDependencies/Sources/ShakeSetup.swift
+++ Dependencies/CoreDependencies/Sources/ShakeSetup.swift
@@ -4,7 +4,6 @@ import UIKit
 
 public extension Shake {
     static func setup() {
-        Shake.configuration.isAutoVideoRecordingEnabled = true
         Shake.configuration.isInvokedByScreenshot = true
         Shake.start(
             clientId: "UL4cR8O6F49Vac5LzphITIEMDi1bp6GhbaE0Cj1O",
