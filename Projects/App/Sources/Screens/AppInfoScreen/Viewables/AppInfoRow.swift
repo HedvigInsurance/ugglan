4747a5e8b1f3b9640567c11da5bd98083503e04c
Sam Pettersson <sam@sampettersson.com>
2021-04-11 10:07:33 +0200

    Add AppInfoRow again

---
+++ Projects/App/Sources/Screens/AppInfoScreen/Viewables/AppInfoRow.swift
@@ -0,0 +1,76 @@
+import Flow
+import Form
+import Foundation
+import hCore
+import hCoreUI
+import Market
+import UIKit
+
+public struct AppInfoRow {
+    public init(title: String, icon: UIImage?, isTappable: Bool, value: Future<String>) {
+        self.title = title
+        self.icon = icon
+        self.isTappable = isTappable
+        self.value = value
+        onSelect = onSelectCallbacker.providedSignal
+    }
+
+    let title: String
+    let icon: UIImage?
+    let isTappable: Bool
+    let value: Future<String>
+
+    private let onSelectCallbacker = Callbacker<Void>()
+    public let onSelect: Signal<Void>
+}
+
+extension AppInfoRow: Viewable {
+    public func materialize(events: SelectableViewableEvents) -> (RowView, Disposable) {
+        let bag = DisposeBag()
+        let row = RowView(
+            title: title,
+            subtitle: "",
+            style: TitleSubtitleStyle.default.restyled { (style: inout TitleSubtitleStyle) in
+                style.title = .brand(.headline(color: .primary))
+                style.subtitle = .brand(.subHeadline(color: .secondary))
+            }
+        )
+
+        let imageView = UIImageView()
+        imageView.image = icon
+        imageView.contentMode = .scaleAspectFit
+        row.prepend(imageView)
+
+        let activityIndicator = UIActivityIndicatorView()
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.color = .brand(.primaryTintColor)
+        activityIndicator.startAnimating()
+
+        row.append(activityIndicator)
+
+        imageView.snp.makeConstraints { make in
+            make.width.equalTo(24)
+        }
+
+        row.setCustomSpacing(16, after: imageView)
+
+        let chevronImageView = UIImageView()
+        chevronImageView.image = hCoreUIAssets.chevronRight.image
+
+        if isTappable {
+            row.append(chevronImageView)
+            bag += events.onSelect.lazyBindTo(callbacker: onSelectCallbacker)
+        }
+
+        bag += value.onValue { value in
+            row.subtitle = value
+            activityIndicator.stopAnimating()
+            
+            bag += row.subtitleLabel?.copySignal.onValue { _ in
+                UIPasteboard.general.value = value
+            }
+        }
+
+        return (row, bag)
+    }
+}
