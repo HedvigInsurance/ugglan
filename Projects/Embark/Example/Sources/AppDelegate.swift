706989796b2e59e5ce3b04c7bc27d665efa6f36c
tarikstafford <tarikstafford@gmail.com>
2021-05-10 15:12:45 +0530

    embark debug mode
    # Conflicts:
    #	Projects/Embark/Example/Sources/AppDelegate.swift

--- Projects/Embark/Example/Sources/AppDelegate.swift
+++ Projects/Embark/Example/Sources/AppDelegate.swift
@@ -1,44 +1,32 @@
 import Apollo
 import ApolloWebSocket
 import Embark
+import EmbarkTesting
 import Flow
 import Form
 import Foundation
 import Presentation
 import UIKit
-import hCore
-import hGraphQL
 
-@UIApplicationMain class AppDelegate: UIResponder, UIApplicationDelegate {
-	var window: UIWindow?
-	let bag = DisposeBag()
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    let bag = DisposeBag()
 
-	func application(
-		_: UIApplication,
-		didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?
-	) -> Bool {
-		window = UIWindow(frame: UIScreen.main.bounds)
-		window?.makeKeyAndVisible()
+    func application(
+        _: UIApplication,
+        didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
 
-		ApolloClient.saveToken(token: "tBmMTBw4OAPC5w==.TNrYtXtgMrDzxw==.KyJBBOTLaw1/Pg==")
+        let navigationController = UINavigationController()
+        navigationController.navigationBar.prefersLargeTitles = true
 
-		ApolloClient.initClient()
-			.onValue { store, client in let navigationController = UINavigationController()
-				navigationController.navigationBar.prefersLargeTitles = true
-				self.window?.rootViewController = navigationController
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
 
-				Dependencies.shared.add(module: Module { client })
+        bag += navigationController.present(Debug(), style: .default, options: [.largeTitleDisplayMode(.always)])
 
-				Dependencies.shared.add(module: Module { store })
-
-				Localization.Locale.currentLocale = .en_NO
-				DefaultStyling.installCustom()
-
-				self.bag += navigationController.present(
-					StoryList(),
-					options: [.defaults, .largeTitleDisplayMode(.never)]
-				)
-			}
-		return true
-	}
+        return true
+    }
 }
