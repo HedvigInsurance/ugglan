d3de561ecee68bdef76863feead8382d1c31ec74
Sam Pettersson <sam@sampettersson.com>
2021-06-17 13:35:51 +0200

    Auto pick locale

--- Projects/ExampleUtil/Sources/Setup.swift
+++ Projects/ExampleUtil/Sources/Setup.swift
@@ -3,10 +3,12 @@ import Form
 import Foundation
 import Shake
 import UIKit
+import hCore
 
 extension UIApplication {
 	public func setup() {
 		DefaultStyling.installCustom()
 		Shake.setup()
+        Localization.Locale.currentLocale = .en_SE
 	}
 }
