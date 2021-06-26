26fc80e79d78a3015d80194e1e9b52bf5929cbcf
Sam Pettersson <sam@sampettersson.com>
2021-06-26 11:50:39 +0200

    Add id to HomeTestingData & Update screenshots

--- Projects/Payment/Project.swift
+++ Projects/Payment/Project.swift
@@ -5,7 +5,6 @@ let project = Project.framework(
 	name: "Payment",
 	targets: Set([.framework, .tests, .example, .testing]),
 	projects: ["hCore", "hCoreUI"],
-	dependencies: ["CoreDependencies", "ResourceBundledDependencies"],
 	sdks: [],
 	includesGraphQL: true
 )
