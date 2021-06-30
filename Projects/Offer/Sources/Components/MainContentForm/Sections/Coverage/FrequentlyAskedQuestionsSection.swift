357026c66d0285477b7b37f39ac931d081d12102
tarikstafford <tarikstafford@gmail.com>
2021-06-30 16:27:12 +0200

    stash

--- Projects/Offer/Sources/Components/MainContentForm/Sections/Coverage/FrequentlyAskedQuestionsSection.swift
+++ Projects/Offer/Sources/Components/MainContentForm/Sections/Coverage/FrequentlyAskedQuestionsSection.swift
@@ -13,13 +13,32 @@ struct FrequentlyAskedQuestionsSection {
 
 extension FrequentlyAskedQuestionsSection: Presentable {
 	func materialize() -> (SectionView, Disposable) {
-		let section = SectionView(
-			headerView: UILabel(value: "Common questions", style: .default),
-			footerView: nil
-		)
-		section.dynamicStyle = .brandGrouped(separatorType: .none)
-
 		let bag = DisposeBag()
+        
+        let footerStack = UIStackView()
+        
+        let footerDescription = UILabel(value: L10n.offerFooterSubtitle, style: .brand(.subHeadline(color: .primary)))
+        let button = ButtonRowViewWrapper(title: L10n.offerFooterButtonText, type: .outlineIcon(borderColor: .brand(.primaryText()), textColor: .brand(.primaryText()), icon: .left(image: hCoreUIAssets.chat.image, width: 28.5)))
+        
+        footerStack.addArrangedSubview(footerDescription)
+        
+        let section = SectionView(
+            headerView: UILabel(value: L10n.Offer.faqTitle, style: .default),
+            footerView: nil
+        )
+        
+        let innerSection = SectionView()
+        innerSection.dynamicStyle = .brandGrouped(separatorType: .standard)
+        
+        let footerSection = SectionView()
+        footerSection.append(RowView().append(footerDescription))
+        bag += footerSection.append(button)
+        footerSection.dynamicStyle = .brandGrouped(separatorType: .none)
+        
+        section.append(innerSection)
+        section.append(footerSection)
+        
+        section.dynamicStyle = .brandGrouped(separatorType: .none)
 
 		bag += state.dataSignal.compactMap { $0.quoteBundle.frequentlyAskedQuestions }
 			.onValueDisposePrevious { frequentlyAskedQuestions in
@@ -27,9 +46,9 @@ extension FrequentlyAskedQuestionsSection: Presentable {
 
 				innerBag += frequentlyAskedQuestions.map { frequentlyAskedQuestion in
 					let innerBag = DisposeBag()
-
+                    
 					let row = RowView(title: frequentlyAskedQuestion.headline ?? "")
-					innerBag += section.append(row).compactMap { _ in row.viewController }
+					innerBag += innerSection.append(row).compactMap { _ in row.viewController }
 						.onValue { viewController in
 							viewController.present(
 								FrequentlyAskedQuestionDetail(
@@ -41,7 +60,7 @@ extension FrequentlyAskedQuestionsSection: Presentable {
 						}
 
 					innerBag += Disposer {
-						section.remove(row)
+						innerSection.remove(row)
 					}
 
 					row.append(hCoreUIAssets.chevronRight.image)
