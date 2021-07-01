4967968376a2684cce7f45809bff344fba5c8b33
tarikstafford <tarikstafford@gmail.com>
2021-07-01 13:30:21 +0200

    add button

--- Projects/Offer/Sources/Components/MainContentForm/Sections/Coverage/FrequentlyAskedQuestionsSection.swift
+++ Projects/Offer/Sources/Components/MainContentForm/Sections/Coverage/FrequentlyAskedQuestionsSection.swift
@@ -18,26 +18,64 @@ extension FrequentlyAskedQuestionsSection: Presentable {
         let footerStack = UIStackView()
         
         let footerDescription = UILabel(value: L10n.offerFooterSubtitle, style: .brand(.subHeadline(color: .primary)))
-        let button = ButtonRowViewWrapper(title: L10n.offerFooterButtonText, type: .outlineIcon(borderColor: .brand(.primaryText()), textColor: .brand(.primaryText()), icon: .left(image: hCoreUIAssets.chat.image, width: 28.5)))
+        footerDescription.textAlignment = .center
+        
+        let button = Button(title: L10n.offerFooterButtonText, type: .standardOutlineIcon(borderColor: .brand(.primaryText()), textColor: .brand(.primaryText()), icon: .left(image: hCoreUIAssets.chat.image, width: 24)))
         
         footerStack.addArrangedSubview(footerDescription)
         
         let section = SectionView(
             headerView: UILabel(value: L10n.Offer.faqTitle, style: .default),
-            footerView: nil
+            footerView: {
+                let footerStackView = UIStackView()
+                footerStackView.axis = .vertical
+                footerStackView.spacing = 16
+                footerStackView.addArrangedSubview(footerDescription)
+                
+                bag += footerStackView.addArranged(button) { button in
+                    
+                }
+                
+                return footerStackView
+            }()
         )
         
+        let rowContentContainer = UIStackView()
+        rowContentContainer.isUserInteractionEnabled = true
+        rowContentContainer.spacing = 8
+        rowContentContainer.edgeInsets = UIEdgeInsets(inset: 16)
+        
+        let rowContentBackground = UIView()
+        rowContentBackground.layer.cornerRadius = .defaultCornerRadius
+        rowContentBackground.backgroundColor = .brand(.secondaryBackground())
+        
+        rowContentContainer.addArrangedSubview(rowContentBackground)
+        
+        let innerContentContainer = UIStackView()
+        innerContentContainer.isUserInteractionEnabled = true
+        innerContentContainer.clipsToBounds = true
+        innerContentContainer.layer.cornerRadius = 8
+        
+        rowContentBackground.addSubview(innerContentContainer)
+        innerContentContainer.snp.makeConstraints { make in make.edges.equalToSuperview()}
+        
         let innerSection = SectionView()
-        innerSection.dynamicStyle = .brandGrouped(separatorType: .standard)
+        innerSection.dynamicStyle = .brandGrouped(separatorType: .standard,  backgroundColor: .white)
         
-        let footerSection = SectionView()
-        footerSection.append(RowView().append(footerDescription))
-        bag += footerSection.append(button)
-        footerSection.dynamicStyle = .brandGrouped(separatorType: .none)
+        bag += rowContentBackground.applyShadow { _ in
+            UIView.ShadowProperties(
+                opacity: 1,
+                offset: CGSize(width: 0, height: 1),
+                blurRadius: nil,
+                color: UIColor.black.withAlphaComponent(0.1),
+                path: nil,
+                radius: 2
+            )
+        }
         
-        section.append(innerSection)
-        section.append(footerSection)
+        innerContentContainer.append(innerSection)
         
+        section.append(rowContentContainer)
         section.dynamicStyle = .brandGrouped(separatorType: .none)
 
 		bag += state.dataSignal.compactMap { $0.quoteBundle.frequentlyAskedQuestions }
@@ -47,7 +85,14 @@ extension FrequentlyAskedQuestionsSection: Presentable {
 				innerBag += frequentlyAskedQuestions.map { frequentlyAskedQuestion in
 					let innerBag = DisposeBag()
                     
-					let row = RowView(title: frequentlyAskedQuestion.headline ?? "")
+                    let titleLabel = MultilineLabel(
+                        value: frequentlyAskedQuestion.headline ?? "",
+                        style: .brand(.body(color: .primary))
+                    )
+
+                    let row = RowView()
+                    innerBag += row.append(titleLabel)
+                    
 					innerBag += innerSection.append(row).compactMap { _ in row.viewController }
 						.onValue { viewController in
 							viewController.present(
@@ -62,8 +107,12 @@ extension FrequentlyAskedQuestionsSection: Presentable {
 					innerBag += Disposer {
 						innerSection.remove(row)
 					}
-
-					row.append(hCoreUIAssets.chevronRight.image)
+                    
+                    let imageView = UIImageView()
+                    imageView.image = hCoreUIAssets.chevronRight.image
+                    imageView.setContentHuggingPriority(.required, for: .horizontal)
+                    
+					row.append(imageView)
 
 					return innerBag
 				}
--- Projects/hCoreUI/Sources/Button.swift
+++ Projects/hCoreUI/Sources/Button.swift
@@ -9,6 +9,7 @@ public enum ButtonType {
 	case standardIcon(backgroundColor: UIColor, textColor: UIColor, icon: ButtonIcon)
 	case standardSmall(backgroundColor: UIColor, textColor: UIColor)
 	case standardOutline(borderColor: UIColor, textColor: UIColor)
+    case standardOutlineIcon(borderColor: UIColor, textColor: UIColor, icon: ButtonIcon)
 	case tinyIcon(backgroundColor: UIColor, textColor: UIColor, icon: ButtonIcon)
 	case outline(borderColor: UIColor, textColor: UIColor)
 	case outlineIcon(borderColor: UIColor, textColor: UIColor, icon: ButtonIcon)
@@ -41,22 +42,22 @@ public enum ButtonType {
 
 	var backgroundOpacity: CGFloat {
 		switch self {
-		case .standard, .standardSmall, .standardIcon, .tinyIcon:
+        case .standard, .standardSmall, .standardIcon, .tinyIcon:
 			return 1
-		case .outline, .outlineIcon, .transparent, .standardOutline:
+        case .outline, .outlineIcon, .transparent, .standardOutline, .standardOutlineIcon:
 			return 0
 		case .pillSemiTransparent:
 			return 0.6
 		case .iconTransparent:
 			return 0.0
-		}
+        }
 	}
 
 	var disabledBackgroundOpacity: CGFloat {
 		switch self {
 		case .standard, .standardSmall, .standardIcon, .tinyIcon:
 			return 1
-		case .outline, .outlineIcon, .standardOutline:
+        case .outline, .outlineIcon, .standardOutline, .standardOutlineIcon:
 			return 0.05
 		case .pillSemiTransparent:
 			return 0.6
@@ -64,14 +65,14 @@ public enum ButtonType {
 			return 0.05
 		case .transparent:
 			return 0
-		}
+        }
 	}
 
 	var highlightedBackgroundOpacity: CGFloat {
 		switch self {
-		case .standard, .standardSmall, .standardIcon, .tinyIcon:
+        case .standard, .standardSmall, .standardIcon, .tinyIcon:
 			return 1
-		case .outline, .outlineIcon, .standardOutline:
+        case .outline, .outlineIcon, .standardOutline, .standardOutlineIcon:
 			return 0.05
 		case .pillSemiTransparent:
 			return 0.6
@@ -79,7 +80,7 @@ public enum ButtonType {
 			return 0.05
 		case .transparent:
 			return 0
-		}
+        }
 	}
 
 	public var backgroundColor: UIColor {
@@ -99,7 +100,9 @@ public enum ButtonType {
 		case let .standardOutline(borderColor, _):
 			return borderColor
 		case let .pillSemiTransparent(backgroundColor, _):
-			return backgroundColor
+            return backgroundColor
+        case let .standardOutlineIcon(borderColor, _, _):
+			return borderColor
 		case .iconTransparent:
 			return .black
 		case .transparent:
@@ -129,12 +132,14 @@ public enum ButtonType {
 			return textColor
 		case let .transparent(textColor):
 			return textColor
+        case let .standardOutlineIcon(_, textColor, _):
+            return textColor
 		}
 	}
 
 	public var height: CGFloat {
 		switch self {
-		case .standard, .standardIcon, .standardOutline:
+        case .standard, .standardIcon, .standardOutline, .standardOutlineIcon:
 			return 50
 		case .standardSmall:
 			return 34
@@ -153,7 +158,7 @@ public enum ButtonType {
 
 	var textStyle: TextStyle {
 		switch self {
-		case .standard, .outline, .standardIcon, .standardOutline:
+        case .standard, .outline, .standardIcon, .standardOutline, .standardOutlineIcon:
 			return TextStyle.brand(.body(color: .primary(state: .negative))).colored(textColor)
 		case .standardSmall:
 			return TextStyle.brand(.subHeadline(color: .primary(state: .negative))).colored(textColor)
@@ -165,12 +170,12 @@ public enum ButtonType {
 			return TextStyle.brand(.caption2(color: .primary(state: .negative))).colored(textColor)
 		case .transparent:
 			return TextStyle.brand(.caption2(color: .primary(state: .negative))).colored(textColor)
-		}
+        }
 	}
 
 	public var extraWidthOffset: CGFloat {
 		switch self {
-		case .standard, .standardIcon, .standardOutline:
+        case .standard, .standardIcon, .standardOutline, .standardOutlineIcon:
 			return 50
 		case .standardSmall:
 			return 40
@@ -199,6 +204,8 @@ public enum ButtonType {
 			return icon
 		case let .tinyIcon(_, _, icon):
 			return icon
+        case let .standardOutlineIcon(_, _, icon):
+            return icon
 		default:
 			return nil
 		}
@@ -208,7 +215,7 @@ public enum ButtonType {
 		switch self {
 		case .iconTransparent:
 			return textColor
-		case .standardIcon:
+		case .standardIcon, .standardOutlineIcon:
 			return textColor
 		case .tinyIcon:
 			return textColor
@@ -227,7 +234,7 @@ public enum ButtonType {
 			return 4
 		case .tinyIcon:
 			return 4
-		case .outlineIcon:
+		case .outlineIcon, .standardOutlineIcon:
 			return 7
 		default:
 			return 0
@@ -236,7 +243,7 @@ public enum ButtonType {
 
 	var borderWidth: CGFloat {
 		switch self {
-		case .outline, .outlineIcon, .standardOutline:
+        case .outline, .outlineIcon, .standardOutline, .standardOutlineIcon:
 			return 1
 		default:
 			return 0
@@ -249,6 +256,8 @@ public enum ButtonType {
 			return borderColor
 		case let .standardOutline(borderColor, _):
 			return borderColor
+        case let .standardOutlineIcon(borderColor, _, _):
+            return borderColor
 		case let .outlineIcon(borderColor, _, _):
 			return borderColor
 		default:
