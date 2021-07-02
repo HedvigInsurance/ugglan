d1a0e4440c793e3e4cbe5af6d50cf05dd55d7188
Sam Pettersson <sam@sampettersson.com>
2021-07-02 13:50:18 +0200

    Add origin

--- scripts/format-ci.sh
+++ scripts/format-ci.sh
@@ -4,6 +4,6 @@ set -x
 
 SWIFT_FORMAT_PATH=$(find . -type f -name swift-format | grep -v '.dSYM')
 
-git diff --name-only $GITHUB_BASE_REF | grep -e '\(.*\).swift$' | while read line; do
+git diff --name-only origin/$GITHUB_BASE_REF | grep -e '\(.*\).swift$' | while read line; do
     $SWIFT_FORMAT_PATH -i "${line}";
 done
