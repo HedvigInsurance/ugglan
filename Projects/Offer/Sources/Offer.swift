375f0f154f1d54814f4d5f7aece5cf723aeba7bf
Sam Pettersson <sam@sampettersson.com>
2021-07-02 11:24:06 +0200

    Formatting

--- Projects/Offer/Sources/Offer.swift
+++ Projects/Offer/Sources/Offer.swift
@@ -303,10 +303,11 @@ extension Offer: Presentable {
 			make.height.equalTo(CGFloat.hairlineWidth)
 		}
 
-        bag += scrollView.signal(for: \.contentOffset)
-            .atOnce()
+		bag += scrollView.signal(for: \.contentOffset)
+			.atOnce()
 			.onValue { contentOffset in
-                navigationBarBackgroundView.alpha = (contentOffset.y + scrollView.safeAreaInsets.top) / (Header.insetTop)
+				navigationBarBackgroundView.alpha =
+					(contentOffset.y + scrollView.safeAreaInsets.top) / (Header.insetTop)
 				navigationBarBackgroundView.snp.updateConstraints { make in
 					if let navigationBar = viewController.navigationController?.navigationBar,
 						let insetTop = viewController.navigationController?.view.safeAreaInsets
