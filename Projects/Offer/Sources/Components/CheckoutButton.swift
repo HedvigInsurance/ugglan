23f9fd4d3c71c9b8ed32cca1522e995137600c90
sampettersson <sampettersson@users.noreply.github.com>
2021-07-02 12:36:20 +0000

    Formatting

--- Projects/Offer/Sources/Components/CheckoutButton.swift
+++ Projects/Offer/Sources/Components/CheckoutButton.swift
@@ -1,85 +1,85 @@
+import Flow
+import Form
 import Foundation
 import Presentation
-import hCore
-import Flow
 import UIKit
+import hCore
 import hCoreUI
-import Form
 
 struct CheckoutButton: Presentable {
-    @ReadWriteState var isEnabled: Bool = false
-    @ReadWriteState var isLoading: Bool = false
-    
-    private let onTapCallbacker = Callbacker<Void>()
-    
-    var onTapSignal: Signal<Void> {
-        onTapCallbacker.providedSignal
-    }
+	@ReadWriteState var isEnabled: Bool = false
+	@ReadWriteState var isLoading: Bool = false
 
-    func materialize() -> (UIView, Disposable) {
-        let view = AccessoryBaseView()
-        let bag = DisposeBag()
+	private let onTapCallbacker = Callbacker<Void>()
 
-        let safeAreaWrapperView = UIView()
-        view.addSubview(safeAreaWrapperView)
+	var onTapSignal: Signal<Void> {
+		onTapCallbacker.providedSignal
+	}
 
-        safeAreaWrapperView.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
-        }
+	func materialize() -> (UIView, Disposable) {
+		let view = AccessoryBaseView()
+		let bag = DisposeBag()
 
-        let baseLayoutMargins = UIEdgeInsets(top: 0, left: 15, bottom: 15, right: 15)
+		let safeAreaWrapperView = UIView()
+		view.addSubview(safeAreaWrapperView)
 
-        let containerView = UIStackView()
-        containerView.isLayoutMarginsRelativeArrangement = true
-        containerView.insetsLayoutMarginsFromSafeArea = true
-        containerView.layoutMargins = baseLayoutMargins
-        containerView.axis = .horizontal
-        safeAreaWrapperView.addSubview(containerView)
+		safeAreaWrapperView.snp.makeConstraints { make in
+			make.edges.equalToSuperview()
+		}
 
-        containerView.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
-        }
+		let baseLayoutMargins = UIEdgeInsets(top: 0, left: 15, bottom: 15, right: 15)
 
-        let button = Button(
-            title: L10n.offerSignButton,
-            type: .standard(
-                backgroundColor: .brand(.secondaryButtonBackgroundColor),
-                textColor: .brand(.secondaryButtonTextColor)
-            )
-        )
-        bag += $isEnabled.atOnce().bindTo(button.isEnabled)
+		let containerView = UIStackView()
+		containerView.isLayoutMarginsRelativeArrangement = true
+		containerView.insetsLayoutMarginsFromSafeArea = true
+		containerView.layoutMargins = baseLayoutMargins
+		containerView.axis = .horizontal
+		safeAreaWrapperView.addSubview(containerView)
 
-        let loadableButton = LoadableButton(button: button)
-        bag += $isLoading.atOnce().bindTo(loadableButton.isLoadingSignal)
-        
-        bag += loadableButton.onTapSignal.onValue { _ in
-            onTapCallbacker.callAll()
-        }
+		containerView.snp.makeConstraints { make in
+			make.edges.equalToSuperview()
+		}
 
-        bag += containerView.addArranged(
-            loadableButton
-        )
+		let button = Button(
+			title: L10n.offerSignButton,
+			type: .standard(
+				backgroundColor: .brand(.secondaryButtonBackgroundColor),
+				textColor: .brand(.secondaryButtonTextColor)
+			)
+		)
+		bag += $isEnabled.atOnce().bindTo(button.isEnabled)
 
-        bag += view.keyboardSignal(priority: .contentInsets)
-            .onValue({ keyboard in
-                guard let viewController = view.viewController else {
-                    return
-                }
+		let loadableButton = LoadableButton(button: button)
+		bag += $isLoading.atOnce().bindTo(loadableButton.isLoadingSignal)
 
-                let frameWidth = view.frame.width
-                let viewControllerWidth = viewController.view.frame.width
-                let halfWidth = (frameWidth - viewControllerWidth) / 2
+		bag += loadableButton.onTapSignal.onValue { _ in
+			onTapCallbacker.callAll()
+		}
 
-                containerView.layoutMargins =
-                    baseLayoutMargins
-                    + UIEdgeInsets(
-                        top: 0,
-                        left: halfWidth,
-                        bottom: view.safeAreaInsets.bottom == 0 ? 0 : 20,
-                        right: halfWidth
-                    )
-            })
+		bag += containerView.addArranged(
+			loadableButton
+		)
 
-        return (view, bag)
-    }
+		bag += view.keyboardSignal(priority: .contentInsets)
+			.onValue({ keyboard in
+				guard let viewController = view.viewController else {
+					return
+				}
+
+				let frameWidth = view.frame.width
+				let viewControllerWidth = viewController.view.frame.width
+				let halfWidth = (frameWidth - viewControllerWidth) / 2
+
+				containerView.layoutMargins =
+					baseLayoutMargins
+					+ UIEdgeInsets(
+						top: 0,
+						left: halfWidth,
+						bottom: view.safeAreaInsets.bottom == 0 ? 0 : 20,
+						right: halfWidth
+					)
+			})
+
+		return (view, bag)
+	}
 }
