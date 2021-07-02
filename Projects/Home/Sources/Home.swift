ccc3c06ff5eafda4e616d96139a73fbd594df524
sampettersson <sampettersson@users.noreply.github.com>
2021-07-02 12:40:04 +0000

    Formatting

--- Projects/Home/Sources/Home.swift
+++ Projects/Home/Sources/Home.swift
@@ -2,183 +2,195 @@ import Apollo
 import Flow
 import Form
 import Foundation
+import Presentation
+import UIKit
 import hCore
 import hCoreUI
 import hGraphQL
-import Presentation
-import UIKit
 
 public struct Home {
-    public static var openClaimsHandler: (_ viewController: UIViewController) -> Void = { _ in }
-    public static var openMovingFlowHandler: (_ viewController: UIViewController) -> Void = { _ in }
-    public static var openCallMeChatHandler: (_ viewController: UIViewController) -> Void = { _ in }
-    public static var openFreeTextChatHandler: (_ viewController: UIViewController) -> Void = { _ in }
-    public static var openConnectPaymentHandler: (_ viewController: UIViewController) -> Void = { _ in }
+	public static var openClaimsHandler: (_ viewController: UIViewController) -> Void = { _ in }
+	public static var openMovingFlowHandler: (_ viewController: UIViewController) -> Void = { _ in }
+	public static var openCallMeChatHandler: (_ viewController: UIViewController) -> Void = { _ in }
+	public static var openFreeTextChatHandler: (_ viewController: UIViewController) -> Void = { _ in }
+	public static var openConnectPaymentHandler: (_ viewController: UIViewController) -> Void = { _ in }
 
-    @Inject var client: ApolloClient
+	@Inject var client: ApolloClient
 
-    public init(sections: [HomeSection]) {
-        self.sections = sections
-    }
+	public init(
+		sections: [HomeSection]
+	) {
+		self.sections = sections
+	}
 
-    public let sections: [HomeSection]
+	public let sections: [HomeSection]
 }
 
 extension Future {
-    func wait(until signal: ReadSignal<Bool>) -> Future<Value> {
-        Future<Value> { completion in
-            let bag = DisposeBag()
+	func wait(until signal: ReadSignal<Bool>) -> Future<Value> {
+		Future<Value> { completion in
+			let bag = DisposeBag()
 
-            self.onValue { value in
-                bag += signal.atOnce().filter(predicate: { $0 }).onValue { _ in
-                    completion(.success(value))
-                }
-            }.onError { error in
-                completion(.failure(error))
-            }
+			self.onValue { value in
+				bag += signal.atOnce().filter(predicate: { $0 })
+					.onValue { _ in
+						completion(.success(value))
+					}
+			}
+			.onError { error in
+				completion(.failure(error))
+			}
 
-            return bag
-        }
-    }
+			return bag
+		}
+	}
 }
 
 enum HomeState {
-    case terminated
-    case future
-    case active
+	case terminated
+	case future
+	case active
 }
 
 extension Home: Presentable {
-    public func materialize() -> (UIViewController, Disposable) {
-        let viewController = UIViewController()
-        viewController.title = L10n.HomeTab.title
-        viewController.installChatButton(allowsChatHint: true)
+	public func materialize() -> (UIViewController, Disposable) {
+		let viewController = UIViewController()
+		viewController.title = L10n.HomeTab.title
+		viewController.installChatButton(allowsChatHint: true)
 
-        if #available(iOS 13.0, *) {
-            let scrollEdgeAppearance = UINavigationBarAppearance()
-            DefaultStyling.applyCommonNavigationBarStyling(scrollEdgeAppearance)
-            scrollEdgeAppearance.configureWithTransparentBackground()
-            scrollEdgeAppearance.largeTitleTextAttributes = scrollEdgeAppearance.largeTitleTextAttributes.merging([
-                NSAttributedString.Key.foregroundColor: UIColor.clear,
-            ], uniquingKeysWith: takeRight)
+		if #available(iOS 13.0, *) {
+			let scrollEdgeAppearance = UINavigationBarAppearance()
+			DefaultStyling.applyCommonNavigationBarStyling(scrollEdgeAppearance)
+			scrollEdgeAppearance.configureWithTransparentBackground()
+			scrollEdgeAppearance.largeTitleTextAttributes = scrollEdgeAppearance.largeTitleTextAttributes
+				.merging(
+					[
+						NSAttributedString.Key.foregroundColor: UIColor.clear
+					],
+					uniquingKeysWith: takeRight
+				)
 
-            viewController.navigationItem.scrollEdgeAppearance = scrollEdgeAppearance
-        }
+			viewController.navigationItem.scrollEdgeAppearance = scrollEdgeAppearance
+		}
 
-        let bag = DisposeBag()
+		let bag = DisposeBag()
 
-        let form = FormView()
-        bag += viewController.install(form) { scrollView in
-            let refreshControl = UIRefreshControl()
-            scrollView.refreshControl = refreshControl
-            bag += self.client.refetchOnRefresh(query: GraphQL.HomeQuery(), refreshControl: refreshControl)
+		let form = FormView()
+		bag += viewController.install(form) { scrollView in
+			let refreshControl = UIRefreshControl()
+			scrollView.refreshControl = refreshControl
+			bag += self.client.refetchOnRefresh(query: GraphQL.HomeQuery(), refreshControl: refreshControl)
 
-            bag += scrollView.performEntryAnimation(
-                contentView: form,
-                onLoad: self.client
-                    .fetch(query: GraphQL.HomeQuery())
-                    .wait(until: scrollView.safeToPerformEntryAnimationSignal)
-                    .delay(by: 0.1)
-            ) { error in
-                print(error)
-            }
-        }
+			bag += scrollView.performEntryAnimation(
+				contentView: form,
+				onLoad: self.client
+					.fetch(query: GraphQL.HomeQuery())
+					.wait(until: scrollView.safeToPerformEntryAnimationSignal)
+					.delay(by: 0.1)
+			) { error in
+				print(error)
+			}
+		}
 
-        bag += form.append(ImportantMessagesSection())
+		bag += form.append(ImportantMessagesSection())
 
-        let rowInsets = UIEdgeInsets(
-            top: 0,
-            left: 25,
-            bottom: 0,
-            right: 25
-        )
+		let rowInsets = UIEdgeInsets(
+			top: 0,
+			left: 25,
+			bottom: 0,
+			right: 25
+		)
 
-        let titleSection = form.appendSection()
-        let titleRow = RowView()
-        titleRow.isLayoutMarginsRelativeArrangement = true
-        titleRow.layoutMargins = rowInsets
-        titleSection.append(titleRow)
+		let titleSection = form.appendSection()
+		let titleRow = RowView()
+		titleRow.isLayoutMarginsRelativeArrangement = true
+		titleRow.layoutMargins = rowInsets
+		titleSection.append(titleRow)
 
-        func buildSections(functionBag: DisposeBag, state: HomeState) {
-            switch state {
-            case .active:
-                functionBag += titleRow.append(ActiveSection())
-            case .future:
-                functionBag += titleRow.append(FutureSection())
-            case .terminated:
-                functionBag += titleRow.append(TerminatedSection())
-            }
+		func buildSections(functionBag: DisposeBag, state: HomeState) {
+			switch state {
+			case .active:
+				functionBag += titleRow.append(ActiveSection())
+			case .future:
+				functionBag += titleRow.append(FutureSection())
+			case .terminated:
+				functionBag += titleRow.append(TerminatedSection())
+			}
 
-            sections.forEach { homeSection in
-                let bottomRow = RowView()
-                bottomRow.isLayoutMarginsRelativeArrangement = true
-                bottomRow.layoutMargins = rowInsets
-                titleSection.append(bottomRow)
+			sections.forEach { homeSection in
+				let bottomRow = RowView()
+				bottomRow.isLayoutMarginsRelativeArrangement = true
+				bottomRow.layoutMargins = rowInsets
+				titleSection.append(bottomRow)
 
-                switch homeSection.style {
-                case .horizontal:
-                    break
-                case .vertical:
-                    guard state == .active else { return }
-                    let section = HomeVerticalSection(section: homeSection)
-                    functionBag += bottomRow.append(section)
-                case .header:
-                    break
-                }
-            }
-        }
+				switch homeSection.style {
+				case .horizontal:
+					break
+				case .vertical:
+					guard state == .active else { return }
+					let section = HomeVerticalSection(section: homeSection)
+					functionBag += bottomRow.append(section)
+				case .header:
+					break
+				}
+			}
+		}
 
-        bag += NotificationCenter.default.signal(forName: UIApplication.didBecomeActiveNotification)
-            .mapLatestToFuture { _ in self.client.fetch(query: GraphQL.HomeQuery(), cachePolicy: .fetchIgnoringCacheData) }
-            .nil()
+		bag += NotificationCenter.default.signal(forName: UIApplication.didBecomeActiveNotification)
+			.mapLatestToFuture { _ in
+				self.client.fetch(query: GraphQL.HomeQuery(), cachePolicy: .fetchIgnoringCacheData)
+			}
+			.nil()
 
-        bag += client
-            .watch(query: GraphQL.HomeQuery())
-            .map { data in
-                data.homeState
-            }
-            .onValueDisposePrevious { homeState in
-                let innerBag = DisposeBag()
+		bag +=
+			client
+			.watch(query: GraphQL.HomeQuery())
+			.map { data in
+				data.homeState
+			}
+			.onValueDisposePrevious { homeState in
+				let innerBag = DisposeBag()
 
-                buildSections(functionBag: innerBag, state: homeState)
+				buildSections(functionBag: innerBag, state: homeState)
 
-                return innerBag
-            }
+				return innerBag
+			}
 
-        return (viewController, bag)
-    }
+		return (viewController, bag)
+	}
 }
 
 extension Home: Tabable {
-    public func tabBarItem() -> UITabBarItem {
-        UITabBarItem(
-            title: L10n.HomeTab.title,
-            image: Asset.tab.image,
-            selectedImage: Asset.tabSelected.image
-        )
-    }
+	public func tabBarItem() -> UITabBarItem {
+		UITabBarItem(
+			title: L10n.HomeTab.title,
+			image: Asset.tab.image,
+			selectedImage: Asset.tabSelected.image
+		)
+	}
 }
 
-private extension GraphQL.HomeQuery.Data {
-    var homeState: HomeState {
-        if isTerminated {
-            return .terminated
-        } else if isFuture {
-            return .future
-        } else {
-            return .active
-        }
-    }
+extension GraphQL.HomeQuery.Data {
+	fileprivate var homeState: HomeState {
+		if isTerminated {
+			return .terminated
+		} else if isFuture {
+			return .future
+		} else {
+			return .active
+		}
+	}
 
-    private var isTerminated: Bool {
-        contracts.allSatisfy({ (contract) -> Bool in
-            contract.status.asActiveInFutureStatus != nil || contract.status.asTerminatedStatus != nil || contract.status.asTerminatedTodayStatus != nil
-        })
-    }
+	private var isTerminated: Bool {
+		contracts.allSatisfy({ (contract) -> Bool in
+			contract.status.asActiveInFutureStatus != nil || contract.status.asTerminatedStatus != nil
+				|| contract.status.asTerminatedTodayStatus != nil
+		})
+	}
 
-    private var isFuture: Bool {
-        contracts.allSatisfy { (contract) -> Bool in
-            contract.status.asActiveInFutureStatus != nil || contract.status.asPendingStatus != nil
-        }
-    }
+	private var isFuture: Bool {
+		contracts.allSatisfy { (contract) -> Bool in
+			contract.status.asActiveInFutureStatus != nil || contract.status.asPendingStatus != nil
+		}
+	}
 }
