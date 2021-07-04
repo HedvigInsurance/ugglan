c7e29e6fee308130a9c366c8c675220bbb286612
Sam Pettersson <sam@sampettersson.com>
2021-07-04 10:27:08 +0200

    Patch Payment.framework aswell

--- scripts/patch-platform-filter.sh
+++ scripts/patch-platform-filter.sh
@@ -2,4 +2,5 @@
 set -e
 set -x
 
-patch Projects/App/Ugglan.xcodeproj/project.pbxproj scripts/macOS-platform-filter-patch.patch
+patch Projects/Payment/Payment.xcodeproj/project.pbxproj scripts/macOS-platform-filter-patch-2.patch
+patch Projects/App/Ugglan.xcodeproj/project.pbxproj scripts/macOS-platform-filter-patch-1.patch
