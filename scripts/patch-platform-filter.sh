974e952186785025a0e76a63fb44c7445747166e
Sam Pettersson <sam@sampettersson.com>
2021-07-05 13:51:36 +0200

    Remove unneeded patch and fix path

--- scripts/patch-platform-filter.sh
+++ scripts/patch-platform-filter.sh
@@ -2,5 +2,4 @@
 set -e
 set -x
 
-patch Projects/Payment/Payment.xcodeproj/project.pbxproj scripts/macOS-platform-filter-patch-2.patch
 patch Projects/App/Ugglan.xcodeproj/project.pbxproj scripts/macOS-platform-filter-patch-1.patch
