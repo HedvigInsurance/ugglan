f7e789a55e03bde5d72d794a86626ac2729f70a0
tarikstafford <tarikstafford@gmail.com>
2021-07-05 09:30:51 +0200

    add address table

--- Projects/Contracts/Sources/Viewables/MovingFlowIntro.swift
+++ Projects/Contracts/Sources/Viewables/MovingFlowIntro.swift
@@ -71,143 +71,112 @@ public enum MovingFlowRoute {
 }
 
 extension MovingFlowIntro: Presentable {
-	public func materialize() -> (UIViewController, FiniteSignal<MovingFlowRoute>) {
-		let bag = DisposeBag()
-		let viewController = UIViewController()
+    public func materialize() -> (UIViewController, FiniteSignal<MovingFlowRoute>) {
+        let bag = DisposeBag()
+        let viewController = UIViewController()
 
-		let view = UIView()
-		view.backgroundColor = .brand(.primaryBackground())
+        let view = UIView()
+        view.backgroundColor = .brand(.primaryBackground())
 
-		let stackView = UIStackView()
-		stackView.axis = .vertical
-		stackView.distribution = .fillProportionally
-		stackView.alignment = .center
-		stackView.spacing = 16
-		stackView.insetsLayoutMarginsFromSafeArea = true
-		stackView.edgeInsets = UIEdgeInsets(top: 20, left: 16, bottom: 0, right: 16)
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.distribution = .fillProportionally
+        stackView.alignment = .center
+        stackView.spacing = 16
+        stackView.insetsLayoutMarginsFromSafeArea = true
+        stackView.edgeInsets = UIEdgeInsets(top: 20, left: 16, bottom: 0, right: 16)
 
-		view.addSubview(stackView)
-		stackView.snp.makeConstraints { make in
-			make.width.centerX.centerY.equalToSuperview()
-			make.top.greaterThanOrEqualToSuperview()
-			make.bottom.lessThanOrEqualToSuperview()
-		}
+        view.addSubview(stackView)
+        stackView.snp.makeConstraints { make in
+            make.width.centerX.centerY.equalToSuperview()
+            make.top.greaterThanOrEqualToSuperview()
+            make.bottom.lessThanOrEqualToSuperview()
+        }
 
-		let optionsButton = UIBarButtonItem(
-			image: hCoreUIAssets.menuIcon.image,
-			style: .plain,
-			target: nil,
-			action: nil
-		)
-		viewController.navigationItem.rightBarButtonItem = optionsButton
+        let optionsButton = UIBarButtonItem(image: hCoreUIAssets.menuIcon.image, style: .plain, target: nil, action: nil)
+        viewController.navigationItem.rightBarButtonItem = optionsButton
 
-		bag += optionsButton.attachSinglePressMenu(
-			viewController: viewController,
-			menu: menu
-		)
+        bag += optionsButton.attachSinglePressMenu(
+            viewController: viewController,
+            menu: menu
+        )
 
-		viewController.view = view
+        viewController.view = view
 
-		let imageView = UIImageView()
-		imageView.contentMode = .scaleAspectFit
-		imageView.setContentHuggingPriority(.defaultLow, for: .vertical)
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.setContentHuggingPriority(.defaultLow, for: .vertical)
 
-		#warning("ADD ADDRESS DETAILS TABLE VIEW HERE LATER")
-		let tableView = UIView()
-		tableView.isHidden = true
+        var titleLabel = MultilineLabel(value: "", style: .brand(.title2(color: .primary)))
+        var descriptionLabel = MultilineLabel(value: "", style: .brand(.body(color: .secondary)))
 
-		var titleLabel = MultilineLabel(value: "", style: .brand(.title2(color: .primary)))
-		var descriptionLabel = MultilineLabel(value: "", style: .brand(.body(color: .secondary)))
+        stackView.addArrangedSubview(imageView)
+        bag += stackView.addArranged(titleLabel) { labelView in labelView.textAlignment = .center }
+        bag += stackView.addArranged(descriptionLabel) { labelView in labelView.textAlignment = .center }
 
-		stackView.addArrangedSubview(imageView)
-		bag += stackView.addArranged(titleLabel) { labelView in labelView.textAlignment = .center }
-		bag += stackView.addArranged(descriptionLabel) { labelView in labelView.textAlignment = .center }
-		stackView.addArrangedSubview(tableView)
+        bag += $section.onValueDisposePrevious { state in
+            let innerBag = DisposeBag()
+            switch state {
+            case .manual:
+                titleLabel.value = L10n.MovingIntro.manualHandlingButtonText
+                descriptionLabel.value = L10n.MovingIntro.manualHandlingDescription
+                imageView.image = hCoreUIAssets.helicopter.image
+            case let .existing(table):
+                titleLabel.value = L10n.MovingIntro.existingMoveTitle
+                descriptionLabel.value = L10n.MovingIntro.existingMoveDescription
+                imageView.image = nil
+                innerBag += stackView.add(table.fragments.detailsTableFragment)
+            case .normal:
+                titleLabel.value = L10n.MovingIntro.title
+                descriptionLabel.value = L10n.MovingIntro.description
+                imageView.image = hCoreUIAssets.notifications.image
+            case .none:
+                break
+            }
+            return innerBag
+        }
 
-		bag += $section.onValue { state in
-			switch state {
-			case .manual:
-				titleLabel.value = L10n.MovingIntro.manualHandlingButtonText
-				descriptionLabel.value = L10n.MovingIntro.manualHandlingDescription
-				imageView.image = hCoreUIAssets.helicopter.image
-			case let .existing(table):
-				titleLabel.value = L10n.MovingIntro.existingMoveTitle
-				descriptionLabel.value = L10n.MovingIntro.existingMoveDescription
-				imageView.image = nil
-				tableView.isHidden = false
-			case .normal:
-				titleLabel.value = L10n.MovingIntro.title
-				descriptionLabel.value = L10n.MovingIntro.description
-				imageView.image = hCoreUIAssets.notifications.image
-			case .none:
-				break
-			}
-		}
+        let activeContractBundles: Future<[GraphQL.ActiveContractBundlesQuery.Data.ActiveContractBundle]> = client.fetch(query: GraphQL.ActiveContractBundlesQuery())
+            .map { data in
+                data.activeContractBundles
+            }
 
-		let activeContractBundles: Future<[GraphQL.ActiveContractBundlesQuery.Data.ActiveContractBundle]> =
-			client.fetch(query: GraphQL.ActiveContractBundlesQuery())
-			.map { data in
-				data.activeContractBundles
-			}
+        bag += client.fetch(query: GraphQL.UpcomingAgreementQuery(locale: Localization.Locale.currentLocale.asGraphQLLocale())).onValue { data in
+            if let contract = data.contracts.first(where: { $0.status.asActiveStatus?.upcomingAgreementChange != nil }) {
+                $section.value = .existing(contract.upcomingAgreementDetailsTable)
+            } else {
+                bag += activeContractBundles.onValue { bundles in
+                    if let bundle = bundles.first(where: { $0.angelStories.addressChange != nil }) {
+                        $section.value = .normal(bundle.angelStories.addressChange?.displayValue ?? "")
+                    } else {
+                        $section.value = .manual
+                    }
+                }
+            }
+        }
 
-		bag +=
-			client.fetch(
-				query: GraphQL.UpcomingAgreementQuery(
-					locale: Localization.Locale.currentLocale.asGraphQLLocale()
-				)
-			)
-			.onValue { data in
-				if let contract = data.contracts.first(where: {
-					$0.status.asActiveStatus?.upcomingAgreementChange != nil
-				}) {
-					$section.value = .existing(contract.upcomingAgreementDetailsTable)
-				} else {
-					bag += activeContractBundles.onValue { bundles in
-						if let bundle = bundles.first(where: {
-							$0.angelStories.addressChange != nil
-						}) {
-							$section.value = .normal(
-								bundle.angelStories.addressChange?.displayValue ?? ""
-							)
-						} else {
-							$section.value = .manual
-						}
-					}
-				}
-			}
+        return (viewController, FiniteSignal { callbacker in
 
-		return (
-			viewController,
-			FiniteSignal { callbacker in
+            bag += $section.atOnce().onValueDisposePrevious { state in
+                let innerBag = DisposeBag()
 
-				bag += $section.atOnce()
-					.onValueDisposePrevious { state in
-						let innerBag = DisposeBag()
+                if let button = state.button {
+                    innerBag += view.add(button) { buttonView in
+                        buttonView.snp.makeConstraints { make in
+                            make.bottom.equalToSuperview().inset(20 + viewController.view.safeAreaInsets.bottom)
+                            make.leading.trailing.equalToSuperview().inset(16)
+                        }
+                    }
 
-						if let button = state.button {
-							innerBag += view.add(button) { buttonView in
-								buttonView.snp.makeConstraints { make in
-									make.bottom.equalToSuperview()
-										.inset(
-											20
-												+ viewController.view
-												.safeAreaInsets.bottom
-										)
-									make.leading.trailing.equalToSuperview()
-										.inset(16)
-								}
-							}
+                    innerBag += button.onTapSignal.onValue {
+                        callbacker(.value(state.route ?? .chat))
+                    }
+                }
 
-							innerBag += button.onTapSignal.onValue {
-								callbacker(.value(state.route ?? .chat))
-							}
-						}
+                return innerBag
+            }
 
-						return innerBag
-					}
-
-				return bag
-			}
-		)
-	}
+            return bag
+        })
+    }
 }
---
+++ Projects/hCoreUI/Sources/Views/DetailsTableView.swift
@@ -0,0 +1,65 @@
+import hGraphQL
+import Presentation
+import Form
+import Flow
+import hCore
+import UIKit
+
+
+extension hGraphQL.GraphQL.DetailsTableFragment: Viewable {
+    public func materialize(events _: ViewableEvents) -> (SectionView, Disposable) {
+        let bag = DisposeBag()
+        
+        let headerContainer = UIStackView()
+        headerContainer.addArrangedSubview(
+            UILabel(
+                value: title,
+                style: .brand(.title2(color: .primary))
+            )
+        )
+        
+        let sectionView = SectionView(headerView: headerContainer, footerView: nil)
+        bag += {
+            sectionView.removeFromSuperview()
+        }
+        
+        sections.enumerated()
+            .forEach { (offset, section) in
+                let headerContainer = UIStackView()
+                headerContainer.edgeInsets = UIEdgeInsets(
+                    top: offset == 0 ? 0 : 15,
+                    left: 0,
+                    bottom: 0,
+                    right: 0
+                )
+                headerContainer.addArrangedSubview(
+                    UILabel(
+                        value: section.title,
+                        style: .brand(.callout(color: .tertiary))
+                    )
+                )
+
+                let detailsSection = SectionView(
+                    headerView: headerContainer,
+                    footerView: nil
+                )
+                sectionView.append(detailsSection)
+
+                section.rows.forEach { tableRow in
+                    let row = RowView(
+                        title: tableRow.title,
+                        subtitle: tableRow.subtitle ?? ""
+                    )
+                    detailsSection.append(row)
+
+                    let valueLabel = UILabel(
+                        value: tableRow.value,
+                        style: .brand(.body(color: .secondary))
+                    )
+                    row.append(valueLabel)
+                }
+            }
+        
+        return (sectionView, bag)
+    }
+}
