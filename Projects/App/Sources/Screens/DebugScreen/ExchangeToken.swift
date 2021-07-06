345aad80e0a7747829f6debd08b31c24e5e78c34
Sam Pettersson <sam@sampettersson.com>
2021-07-06 09:08:02 +0200

    Formatting

--- Projects/App/Sources/Screens/DebugScreen/ExchangeToken.swift
+++ Projects/App/Sources/Screens/DebugScreen/ExchangeToken.swift
@@ -15,43 +15,39 @@ import hGraphQL
 	@State private var selectedLocale = Localization.Locale.currentLocale
 
 	let locales = Localization.Locale.allCases
-    
-    var pickerStyle: some PickerStyle {
-        #if targetEnvironment(macCatalyst)
-            return MenuPickerStyle()
-        #else
-            return WheelPickerStyle()
-        #endif
-    }
+	var pickerStyle: some PickerStyle {
+		#if targetEnvironment(macCatalyst)
+			return MenuPickerStyle()
+		#else
+			return WheelPickerStyle()
+		#endif
+	}
 
 	var body: some View {
-        Form {
-            TextField("Payment url", text: $paymentUrl)
-            Section {
-                Picker("Locale", selection: $selectedLocale) {
-                    ForEach(locales, id: \.self) { Text($0.code) }
-                }.pickerStyle(pickerStyle)
-            }
-            SwiftUI.Button("Exchange") {
-                let afterHashbang = paymentUrl.split(separator: "#").last
-                let exchangeToken =
-                    afterHashbang?.replacingOccurrences(of: "exchange-token=", with: "")
-                    ?? ""
+		Form {
+			TextField("Payment url", text: $paymentUrl)
+			Section {
+				Picker("Locale", selection: $selectedLocale) {
+					ForEach(locales, id: \.self) { Text($0.code) }
+				}
+				.pickerStyle(pickerStyle)
+			}
+			SwiftUI.Button("Exchange") {
+				let afterHashbang = paymentUrl.split(separator: "#").last
+				let exchangeToken =
+					afterHashbang?.replacingOccurrences(of: "exchange-token=", with: "") ?? ""
 
-                client.perform(
-                    mutation: GraphQL.ExchangeTokenMutation(
-                        exchangeToken: exchangeToken.removingPercentEncoding ?? ""
-                    )
-                )
-                .onValue { response in
-                    guard
-                        let token = response.exchangeToken
-                            .asExchangeTokenSuccessResponse?
-                            .token
-                    else { return }
-                    onToken(token, selectedLocale)
-                }
-            }
-        }
+				client.perform(
+					mutation: GraphQL.ExchangeTokenMutation(
+						exchangeToken: exchangeToken.removingPercentEncoding ?? ""
+					)
+				)
+				.onValue { response in
+					guard let token = response.exchangeToken.asExchangeTokenSuccessResponse?.token
+					else { return }
+					onToken(token, selectedLocale)
+				}
+			}
+		}
 	}
 }
