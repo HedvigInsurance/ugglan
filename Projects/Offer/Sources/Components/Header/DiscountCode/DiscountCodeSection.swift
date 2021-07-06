2802b1a134b4f4b46b8ac054c472b101ea4b5bfd
axelbacklund <19402158+axelbacklund@users.noreply.github.com>
2021-07-06 17:05:57 +0200

    Lint

--- Projects/Offer/Sources/Components/Header/DiscountCode/DiscountCodeSection.swift
+++ Projects/Offer/Sources/Components/Header/DiscountCode/DiscountCodeSection.swift
@@ -36,61 +36,60 @@ extension DiscountCodeSection: Presentable {
 		)
 
 		bag += row.append(button.alignedTo(alignment: .center))
-        
-        let removeRow = RowView()
-        section.append(removeRow)
-        let removeButton = Button(
-            title: L10n.Offer.removeDiscountButton,
-            type: .transparentLarge(textColor: .brand(.destructive))
-        )
-        let loadableButton = LoadableButton(button: removeButton)
-        bag += removeRow.append(loadableButton.alignedTo(alignment: .center))
-        removeRow.isHidden = true
-        removeRow.alpha = 0
-        
 
-        bag += state.dataSignal.animated(style: SpringAnimationStyle.lightBounce()) { data in
+		let removeRow = RowView()
+		section.append(removeRow)
+		let removeButton = Button(
+			title: L10n.Offer.removeDiscountButton,
+			type: .transparentLarge(textColor: .brand(.destructive))
+		)
+		let loadableButton = LoadableButton(button: removeButton)
+		bag += removeRow.append(loadableButton.alignedTo(alignment: .center))
+		removeRow.isHidden = true
+		removeRow.alpha = 0
+
+		bag += state.dataSignal.animated(style: SpringAnimationStyle.lightBounce()) { data in
 			if data.redeemedCampaigns.isEmpty {
-                removeRow.alpha = 0
-                row.alpha = 1
-                removeRow.isHidden = true
-                row.isHidden = false
+				removeRow.alpha = 0
+				row.alpha = 1
+				removeRow.isHidden = true
+				row.isHidden = false
 			} else {
-                row.alpha = 0
-                removeRow.alpha = 1
-                row.isHidden = true
-                removeRow.isHidden = false
+				row.alpha = 0
+				removeRow.alpha = 1
+				row.isHidden = true
+				removeRow.isHidden = false
 			}
 		}
 
 		let discountsPresent = ReadWriteSignal<Bool>(false)
 		bag += state.dataSignal.map { $0.redeemedCampaigns.count != 0 }.bindTo(discountsPresent)
 
-        bag += removeButton.onTapSignal.onValue { _ in
-            loadableButton.isLoadingSignal.value = true
-            state.removeRedeemedCampaigns()
-                .onValue { _ in
-                    loadableButton.isLoadingSignal.value = false
-                }
-                .onError { _ in
-                    loadableButton.isLoadingSignal.value = false
-                    section.viewController?
-                        .present(
-                            Alert<Void>(
-                                title: L10n.Offer.removeDiscountErrorAlertTitle,
-                                message:
-                                    L10n.Offer.removeDiscountErrorAlertBody,
-                                actions: [
-                                    .init(
-                                        title: L10n.alertOk,
-                                        action: { () }
-                                    )
-                                ]
-                            )
-                        )
-                }
-        }
-        
+		bag += removeButton.onTapSignal.onValue { _ in
+			loadableButton.isLoadingSignal.value = true
+			state.removeRedeemedCampaigns()
+				.onValue { _ in
+					loadableButton.isLoadingSignal.value = false
+				}
+				.onError { _ in
+					loadableButton.isLoadingSignal.value = false
+					section.viewController?
+						.present(
+							Alert<Void>(
+								title: L10n.Offer.removeDiscountErrorAlertTitle,
+								message:
+									L10n.Offer.removeDiscountErrorAlertBody,
+								actions: [
+									.init(
+										title: L10n.alertOk,
+										action: { () }
+									)
+								]
+							)
+						)
+				}
+		}
+
 		bag += button.onTapSignal.onValue { _ in
 <<<<<<< HEAD
             let redeemDiscount = RedeemDiscount()
