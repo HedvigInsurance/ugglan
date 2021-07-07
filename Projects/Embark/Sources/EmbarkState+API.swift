803073dca25a99fed35d172958d46b0cd928af64
Sam Pettersson <sam@sampettersson.com>
2021-07-07 10:30:46 +0200

    Formatting

--- Projects/Embark/Sources/EmbarkState+API.swift
+++ Projects/Embark/Sources/EmbarkState+API.swift
@@ -4,26 +4,28 @@ import Foundation
 import hGraphQL
 
 extension ResultMap {
-    private var arrayRegex: String {
-        "\\[[0-9]+\\]$"
-    }
-    
-    private func getArrayValue(_ path: String) -> Any? {
-        if path.range(of: ".*\(arrayRegex)", options: .regularExpression) != nil, let rangeOfIndex = path.range(of: arrayRegex, options: .regularExpression) {
-            let index = String(path[rangeOfIndex].dropFirst().dropLast())
-            
-            let pathWithoutIndex = String(path.replacingCharacters(in: rangeOfIndex, with: ""))
-            
-            let resultMap = self[pathWithoutIndex] as? [Any]
-            
-            if let intIndex = Int(index), (resultMap?.indices.contains(intIndex) ?? false) {
-                return resultMap?[intIndex]
-            }
-        }
-        
-        return nil
-    }
-    
+	private var arrayRegex: String {
+		"\\[[0-9]+\\]$"
+	}
+
+	private func getArrayValue(_ path: String) -> Any? {
+		if path.range(of: ".*\(arrayRegex)", options: .regularExpression) != nil,
+			let rangeOfIndex = path.range(of: arrayRegex, options: .regularExpression)
+		{
+			let index = String(path[rangeOfIndex].dropFirst().dropLast())
+
+			let pathWithoutIndex = String(path.replacingCharacters(in: rangeOfIndex, with: ""))
+
+			let resultMap = self[pathWithoutIndex] as? [Any]
+
+			if let intIndex = Int(index), resultMap?.indices.contains(intIndex) ?? false {
+				return resultMap?[intIndex]
+			}
+		}
+
+		return nil
+	}
+
 	func deepFind(_ path: String) -> Any? {
 		let splittedPath = path.split(separator: ".")
 
@@ -37,12 +39,12 @@ extension ResultMap {
 					in: path
 				)
 			{
-                let nextPath = String(path.replacingCharacters(in: range, with: "").dropFirst())
-                
-                if let arrayValue = getArrayValue(String(firstPath)) {
-                    return (arrayValue as? ResultMap)?.deepFind(nextPath)
-                }
-                
+				let nextPath = String(path.replacingCharacters(in: range, with: "").dropFirst())
+
+				if let arrayValue = getArrayValue(String(firstPath)) {
+					return (arrayValue as? ResultMap)?.deepFind(nextPath)
+				}
+
 				let resultMap = self[String(firstPath)] as? ResultMap
 				return resultMap?
 					.deepFind(nextPath)
