fb76761e746f2d677c18d26412d45a31db3ee40d
Sam Pettersson <sam@sampettersson.com>
2021-07-08 12:42:51 +0200

    Fix moving flow

--- Projects/App/Sources/Generic/AppFlow+MovingFlow.swift
+++ Projects/App/Sources/Generic/AppFlow+MovingFlow.swift
@@ -16,62 +16,60 @@ struct MovingFlow {
 }
 
 extension MovingFlow: Presentable {
-	public func materialize() -> (UIViewController, Disposable) {
-		let menuChildren: [MenuChildable] = [
-			MenuChild.appInformation,
-			MenuChild.appSettings,
-			MenuChild.login(onLogin: {
-				UIApplication.shared.appDelegate.appFlow.presentLoggedIn()
-			}),
-		]
-
-		let menu = Menu(
-			title: nil,
-			children: menuChildren
-		)
-
+	public func materialize() -> (UIViewController, Future<Void>) {
 		let bag = DisposeBag()
 
-		let (viewController, routeSignal) = MovingFlowIntro(menu: menu).materialize()
+		let (viewController, routeSignal) = MovingFlowIntro().materialize()
 
-		viewController.hidesBottomBarWhenPushed = true
-
-		bag += routeSignal.atValue { route in
-			switch route {
-			case .chat:
-				bag += viewController.present(Chat())
-			case let .embark(name):
-				bag +=
-					viewController
-					.present(
-						Embark(
-							name: name,
-							menu: menu
-						),
-						options: [.autoPop]
-					)
-					.onValue { redirect in
-						switch redirect {
-						case .mailingList:
-							break
-						case let .offer(ids: ids):
-							viewController.present(
-								Offer(
-									offerIDContainer: .exact(
-										ids: ids,
-										shouldStore: false
-									),
-									menu: Menu(title: nil, children: [])
-								)
-							)
-							.onValue { _ in
-								#warning("handle this")
-							}
-						}
-					}
-			}
-		}
-
-		return (viewController, bag)
+        return (viewController, Future { completion in
+            bag += routeSignal.atValue { route in
+                switch route {
+                case .chat:
+                    viewController.present(
+                        FreeTextChat().wrappedInCloseButton(),
+                        configure: { chatViewController, _ in
+                            chatViewController.navigationItem.hidesBackButton = true
+                        }
+                    ).onResult(completion)
+                case let .embark(name):
+                    bag +=
+                        viewController
+                        .present(
+                            Embark(
+                                name: name,
+                                menu: Menu(title: nil, children: [])
+                            ),
+                            options: [.autoPop]
+                        )
+                        .onValue { redirect in
+                            switch redirect {
+                            case .mailingList:
+                                break
+                            case let .offer(ids: ids):
+                                viewController.present(
+                                    Offer(
+                                        offerIDContainer: .exact(
+                                            ids: ids,
+                                            shouldStore: false
+                                        ),
+                                        menu: Menu(title: nil, children: [])
+                                    )
+                                )
+                                .onValue { _ in
+                                    Toasts.shared.displayToast(
+                                        toast: Toast(
+                                            symbol: .icon(hCoreUIAssets.circularCheckmark.image),
+                                            body: L10n.movingFlowSuccessToast
+                                        )
+                                    )
+                                    completion(.success)
+                                }
+                            }
+                        }
+                }
+            }
+            
+            return bag
+        })
 	}
 }
