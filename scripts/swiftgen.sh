54407f794d302cad258b3ab7b5351e3a2149b2e9
Sam Pettersson <sam@sampettersson.com>
2021-07-20 08:08:06 +0200

    Change swiftgen dir

--- scripts/swiftgen.sh
+++ scripts/swiftgen.sh
@@ -1,4 +1,4 @@
-TMPDIR=/tmp
+TMPDIR=/tmp/swiftgen-6.4.0
 
 mkdir Projects/hCoreUI/Sources/Derived
 mkdir Projects/App/Sources/Derived
@@ -9,14 +9,16 @@ mkdir Projects/Home/Sources/Derived
 mkdir Projects/Market/Sources/Derived
 mkdir Projects/Offer/Sources/Derived
 
-if [[ -f $TMPDIR/swiftgen/bin/swiftgen ]]
+if [[ -f $TMPDIR/bin/swiftgen ]]
 then
-    $TMPDIR/swiftgen/bin/swiftgen
+    $TMPDIR/bin/swiftgen
     exit 0
 fi
 
+mkdir $TMPDIR
+
 curl -o $TMPDIR/swiftgen.zip -L https://github.com/SwiftGen/SwiftGen/releases/download/6.4.0/swiftgen-6.4.0.zip
 
-unzip $TMPDIR/swiftgen.zip -d $TMPDIR/swiftgen
+unzip $TMPDIR/swiftgen.zip -d $TMPDIR
 
-$TMPDIR/swiftgen/bin/swiftgen
+$TMPDIR/bin/swiftgen
