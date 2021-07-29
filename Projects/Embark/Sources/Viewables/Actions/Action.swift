57de163104a555686fbcae1e9b39fbd5bc8d12a9
tarikstafford <tarikstafford@users.noreply.github.com>
2021-07-29 14:05:18 +0000

    Formatting

--- Projects/Embark/Sources/Viewables/Actions/Action.swift
+++ Projects/Embark/Sources/Viewables/Actions/Action.swift
@@ -136,9 +136,16 @@ extension Action: Viewable {
 									)
 								)
 								.onValue(performCallback)
-                        } else if let dateAction = actionData?.asEmbarkDatePickerAction {
-                            innerBag += view.addArranged(EmbarkDatePickerAction(state: self.state, data: dateAction)).onValue(performCallback)
-                        } else if let textAction = actionData?.asEmbarkTextAction {
+						} else if let dateAction = actionData?.asEmbarkDatePickerAction {
+							innerBag +=
+								view.addArranged(
+									EmbarkDatePickerAction(
+										state: self.state,
+										data: dateAction
+									)
+								)
+								.onValue(performCallback)
+						} else if let textAction = actionData?.asEmbarkTextAction {
 							innerBag +=
 								view.addArranged(
 									EmbarkTextAction(
