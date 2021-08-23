ff3ea3645fdcb0fc64be7560e7e3821943666ca9
Sam Pettersson <sam@sampettersson.com>
2021-08-23 10:32:22 +0200

    Add RUM metrics for apollo requests

--- Projects/App/Sources/TracingInterceptor.swift
+++ Projects/App/Sources/TracingInterceptor.swift
@@ -9,9 +9,11 @@ public class TracingInterceptor: ApolloInterceptor {
         response: HTTPResponse<Operation>?,
         completion: @escaping (Result<GraphQLResult<Operation.Data>, Error>) -> Void
     ) {
+        let resourceKey = "\(request.operation.operationType) \(request.operation.operationName)"
         let headersWritter = HTTPHeadersWriter()
+        let startDate = Date()
         let span = Global.sharedTracer.startSpan(
-            operationName: "\(request.operation.operationType) \(request.operation.operationName)"
+            operationName: resourceKey
         )
         Global.sharedTracer.inject(spanContext: span.context, writer: headersWritter)
 
@@ -22,6 +24,23 @@ public class TracingInterceptor: ApolloInterceptor {
             response: response,
             completion: { result in
                 completion(result)
+                
+                let endDate = Date()
+                
+                Global.rum.addResourceMetrics(
+                    resourceKey: resourceKey,
+                    fetch: (
+                        start: startDate,
+                        end: endDate
+                    ),
+                    redirection: nil,
+                    dns: nil,
+                    connect: nil,
+                    ssl: nil,
+                    firstByte: nil,
+                    download: nil,
+                    responseSize: Int64(response?.rawData.count ?? 0)
+                )
 
                 switch result {
                 case let .failure(error):
