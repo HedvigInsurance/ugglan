4969ba5c528b06afce813ff255aa07a4996b04b5
Sam Pettersson <sam@sampettersson.com>
2021-10-08 11:00:25 +0200

    Formatting

--- Projects/hCore/Sources/ScrollView+Inspect.swift
+++ Projects/hCore/Sources/ScrollView+Inspect.swift
@@ -1,33 +1,28 @@
-//
-//  ScrollView+Inspect.swift
-//  ScrollView+Inspect
-//
-//  Created by Sam Pettersson on 2021-10-08.
-//  Copyright © 2021 Hedvig AB. All rights reserved.
-//
-
-import Foundation
-import UIKit
 import Combine
+import Foundation
 import SwiftUI
+import UIKit
 
 struct ScrollViewIntrospector: UIViewRepresentable {
     var foundScrollView: (_ scrollView: UIScrollView) -> Void
-    
+
     func makeUIView(context: Context) -> some UIView {
         UIView()
     }
-    
+
     func findScrollView(from: UIView) {
-        if let scrollView = from.subviews.compactMap({ view in
-            view as? UIScrollView
-        }).first {
+        if let scrollView = from.subviews
+            .compactMap({ view in
+                view as? UIScrollView
+            })
+            .first
+        {
             foundScrollView(scrollView)
         } else if let parent = from.parent {
             findScrollView(from: parent)
         }
     }
-    
+
     func updateUIView(_ uiView: UIViewType, context: Context) {
         DispatchQueue.main.async {
             findScrollView(from: uiView)
@@ -44,24 +39,28 @@ extension View {
 public struct ForceScrollViewIndicatorInset: ViewModifier {
     @State var scrollView: UIScrollView?
     var insetBottom: CGFloat
-    
-    public init(insetBottom: CGFloat) {
+
+    public init(
+        insetBottom: CGFloat
+    ) {
         self.insetBottom = insetBottom
     }
-    
+
     var contentOffsetPublisher: AnyPublisher<CGPoint, Never> {
         if let scrollView = scrollView {
             return scrollView.publisher(for: \.contentOffset).eraseToAnyPublisher()
         }
-        
+
         return Just(CGPoint.zero).eraseToAnyPublisher()
     }
-    
+
     public func body(content: Content) -> some View {
         content.introspectScrollView { scrollView in
             self.scrollView = scrollView
-        }.onReceive(contentOffsetPublisher) { _ in
-            scrollView?.verticalScrollIndicatorInsets.bottom = insetBottom + (scrollView?.adjustedContentInset.bottom ?? 0)
+        }
+        .onReceive(contentOffsetPublisher) { _ in
+            scrollView?.verticalScrollIndicatorInsets.bottom =
+                insetBottom + (scrollView?.adjustedContentInset.bottom ?? 0)
         }
     }
 }
