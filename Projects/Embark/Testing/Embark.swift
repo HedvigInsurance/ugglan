fe65b3ba0c0b2838bc402cf224e715fff7495bb5
Sam Pettersson <sam@sampettersson.com>
2021-10-19 14:02:57 +0200

    Make embark build

--- Projects/Embark/Testing/Embark.swift
+++ Projects/Embark/Testing/Embark.swift
@@ -147,7 +147,7 @@ extension EmbarkStory {
         )
 
     fileprivate static let embarkAudioRecorderAction = GraphQL.EmbarkStoryQuery.Data.EmbarkStory.Passage.Action
-        .makeEmbarkAudioRecorderAction(component: "audioRec", data: .init(storeKey: "audioRec"))
+        .makeEmbarkAudioRecorderAction(component: "audioRec", audioRecorderData: .init(storeKey: "key", label: "Continue", next: .init(name: "mock", label: "mock"))
 }
 
 extension Debug: Presentable {
