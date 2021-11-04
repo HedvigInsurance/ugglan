c5317474aa4c03f0285bd6e48fa147b173ddc137
Sam Pettersson <sam@hedvig.com>
2021-11-04 08:55:27 +0100

    Formatting

--- Projects/hCoreUI/Sources/ExpandableContent.swift
+++ Projects/hCoreUI/Sources/ExpandableContent.swift
@@ -98,9 +98,10 @@ extension ExpandableContent: Presentable {
             }
         }
 
-        bag += scrollView.contentSizeSignal.atOnce().animated(style: .mediumBounce()) { contentSize in
-            rerenderHeight(size: contentSize)
-        }
+        bag += scrollView.contentSizeSignal.atOnce()
+            .animated(style: .mediumBounce()) { contentSize in
+                rerenderHeight(size: contentSize)
+            }
 
         bag += isExpanded.animated(style: .mediumBounce()) { _ in
             rerenderHeight(size: scrollView.contentSize)
