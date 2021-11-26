ce8437fa68f570366457210936da866254b20a8d
Sam Pettersson <sam@hedvig.com>
2021-11-26 18:26:45 +0100

    Revert "Trigger hCoreUI build"
    This reverts commit 712cb4d32c001e0c7d85fe395d477a22843c4d7b.

--- Projects/hCoreUI/Sources/ActivityView.swift
+++ Projects/hCoreUI/Sources/ActivityView.swift
@@ -8,7 +8,6 @@ extension PresentationStyle {
         let future = Future<Void> { completion in
             from.present(viewController, animated: true) { completion(.success) }
 
-            
             return NilDisposer()
         }
 
