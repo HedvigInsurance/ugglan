89291606ca9dd6a87ec317674e01825729b8be5e
Sam Pettersson <sam@sampettersson.com>
2021-12-09 09:51:23 +0100

    Formatting

--- Projects/Contracts/Sources/ContractsStore.swift
+++ Projects/Contracts/Sources/ContractsStore.swift
@@ -16,9 +16,10 @@ public struct ContractState: StateProtocol {
 
     func contractForId(_ id: String) -> Contract? {
         if let inBundleContract = contractBundles.flatMap({ $0.contracts })
-        .first(where: { contract in
-            contract.id == id
-        }) {
+            .first(where: { contract in
+                contract.id == id
+            })
+        {
             return inBundleContract
         }
 
