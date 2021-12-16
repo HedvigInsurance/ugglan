c92a0d1cff9e714879d2ad22c8934753a294deb9
Sam Pettersson <sam@sampettersson.com>
2021-12-16 09:11:15 +0100

    Formatting

--- Projects/hCore/Sources/Future+Sink.swift
+++ Projects/hCore/Sources/Future+Sink.swift
@@ -1,13 +1,5 @@
-//
-//  Future+Sink.swift
-//  hCore
-//
-//  Created by Sam Pettersson on 2021-12-16.
-//  Copyright © 2021 Hedvig AB. All rights reserved.
-//
-
-import Foundation
 import Flow
+import Foundation
 
 extension Future {
     /// calls on value on the future, and never notifies you, effectively swallowing its value
