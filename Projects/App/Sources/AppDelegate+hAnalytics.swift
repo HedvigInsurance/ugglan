d51f4b78ee956ca15f9c851b16610c4cb1a3bede
Sam Pettersson <sam@sampettersson.com>
2022-02-03 14:31:34 +0100

    Add increasing timeout to retry fetching experiments

--- Projects/App/Sources/AppDelegate+hAnalytics.swift
+++ Projects/App/Sources/AppDelegate+hAnalytics.swift
@@ -21,15 +21,15 @@ extension AppDelegate {
         hAnalyticsNetworking.trackingId = { ApolloClient.getDeviceIdentifier() }
     }
 
-    func setupHAnalyticsExperiments() {
+    func setupHAnalyticsExperiments(numberOfTries: Int = 0) {
         log.info("Started loading hAnlyticsExperiments")
         hAnalyticsExperiment.load { success in
             if success {
                 log.info("Successfully loaded hAnlyticsExperiments")
                 ApplicationContext.shared.hasLoadedExperiments = true
-            } else {
-                log.info("Failed loading hAnlyticsExperiments, retries in 100 ms")
-                DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            } else {                
+                log.info("Failed loading hAnlyticsExperiments, retries in \(numberOfTries * 100) ms")
+                DispatchQueue.main.asyncAfter(deadline: .now() + (Double(numberOfTries) * 0.1)) {
                     self.setupHAnalyticsExperiments()
                 }
             }
