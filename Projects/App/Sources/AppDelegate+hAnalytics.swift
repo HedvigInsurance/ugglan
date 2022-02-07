35552d7a5ec485f6cede03579b5fe0eab784a044
Sam Pettersson <sam@sampettersson.com>
2022-02-07 15:51:47 +0100

    Fix retry

--- Projects/App/Sources/AppDelegate+hAnalytics.swift
+++ Projects/App/Sources/AppDelegate+hAnalytics.swift
@@ -29,7 +29,7 @@ extension AppDelegate {
             } else {
                 log.info("Failed loading hAnlyticsExperiments, retries in \(numberOfTries * 100) ms")
                 DispatchQueue.main.asyncAfter(deadline: .now() + (Double(numberOfTries) * 0.1)) {
-                    self.setupHAnalyticsExperiments()
+                    self.setupHAnalyticsExperiments(numberOfTries: numberOfTries + 1)
                 }
             }
         }
