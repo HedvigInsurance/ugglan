4bc8fc53fb32d754d67d09aaa103bc170cb18893
Sam Pettersson <sam@sampettersson.com>
2022-02-08 15:57:58 +0100

    Formatting

--- Projects/hCoreUI/Sources/hForm/hFormBottomAttachedBackground.swift
+++ Projects/hCoreUI/Sources/hForm/hFormBottomAttachedBackground.swift
@@ -1,21 +1,15 @@
-//
-//  hFormBottomAttachedBackground.swift
-//  hCoreUI
-//
-//  Created by Sam Pettersson on 2022-02-08.
-//  Copyright © 2022 Hedvig AB. All rights reserved.
-//
-
 import Foundation
 import SwiftUI
 
 public struct hFormBottomAttachedBackground<Content: View>: View {
     var content: () -> Content
-    
-    public init(content: @escaping () -> Content) {
+
+    public init(
+        content: @escaping () -> Content
+    ) {
         self.content = content
     }
-    
+
     public var body: some View {
         VStack {
             hSeparatorColor.separator.frame(height: .hairlineWidth)
