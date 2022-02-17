8032f764d6856e6d0110fe03a399919795f5b033
Satish Bandaru <satish.bandaru1234@gmail.com>
2022-02-17 09:28:34 +0100

    Remove dupe code

--- Projects/Claims/Example/Sources/SceneDelegate.swift
+++ Projects/Claims/Example/Sources/SceneDelegate.swift
@@ -7,36 +7,6 @@ import hCoreUI
 
 @available(iOS 13, *)
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-    let bag = DisposeBag()
-    var window: UIWindow?
-
-    func run() {
-        DefaultStyling.installCustom()
-
-        Localization.Locale.currentLocale = .en_SE
-
-        let navigationController = hNavigationController()
-        navigationController.navigationBar.prefersLargeTitles = true
-
-        let tapGestureRecognizer = UITapGestureRecognizer()
-        tapGestureRecognizer.numberOfTouchesRequired = 3
-
-        bag += tapGestureRecognizer.signal(forState: .recognized)
-            .onValue { _ in self.bag.dispose()
-                self.run()
-            }
-
-        window?.addGestureRecognizer(tapGestureRecognizer)
-
-        bag += { self.window?.removeGestureRecognizer(tapGestureRecognizer) }
-
-        navigationController.present(Debug(), options: [.defaults, .largeTitleDisplayMode(.always)])
-
-        window?.rootViewController = navigationController
-        window?.makeKeyAndVisible()
-
-        ApplicationContext.shared.hasFinishedBootstrapping = true
-    }
 
     func scene(
         _ scene: UIScene,
@@ -46,7 +16,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
             self.window = window
-            run()
         }
     }
 
