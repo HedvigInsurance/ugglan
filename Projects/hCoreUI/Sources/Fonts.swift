7404dea62bdf98b76d0b62727e2a3b8c9290e162
Sam Pettersson <sam@sampettersson.com>
2022-03-03 11:42:42 +0100

    Formatting

--- Projects/hCoreUI/Sources/Fonts.swift
+++ Projects/hCoreUI/Sources/Fonts.swift
@@ -8,18 +8,18 @@ public enum Fonts {
     private static let favoritStdBookFontName = "FavoritStd-Book"
     private static let hedvigLettersStandardFontName = "HedvigLetters-Standard"
     private static let hedvigLettersSmallFontName = "HedvigLetters-Small"
-    
+
     private static func loadFont(resourceName: String) -> UIFont {
         let fontPath = Bundle(for: FontBundleToken.self).path(forResource: resourceName, ofType: "otf")
         let inData = NSData(contentsOfFile: fontPath!)
         let provider = CGDataProvider(data: inData!)
-        
+
         let font = CGFont(provider!)
         var error: Unmanaged<CFError>?
 
         CTFontManagerRegisterGraphicsFont(font!, &error)
         error?.release()
-        
+
         let fontName = font?.postScriptName as String?
         return UIFont(name: fontName!, size: UIFont.labelFontSize)!
     }
@@ -27,15 +27,15 @@ public enum Fonts {
     public static var hedvigLettersStandard: UIFont = {
         loadFont(resourceName: hedvigLettersStandardFontName)
     }()
-    
+
     public static var hedvigLettersSmall: UIFont = {
         loadFont(resourceName: hedvigLettersSmallFontName)
     }()
-    
+
     public static var favoritStdBook: UIFont = {
         loadFont(resourceName: favoritStdBookFontName)
     }()
-    
+
     public static var forceTraitCollection: UITraitCollection? = nil
 
     public static func fontFor(style: UIFont.TextStyle) -> UIFont {
@@ -53,11 +53,11 @@ public enum Fonts {
 
             return UIFont(descriptor: fontDescriptor, size: size)
         }
-        
-        if (hAnalyticsExperiment.useHedvigLettersFont) {
+
+        if hAnalyticsExperiment.useHedvigLettersFont {
             return getFont(favoritStdBook)
         }
-        
+
         switch style {
         case .largeTitle, .title1:
             return getFont(hedvigLettersSmall)
