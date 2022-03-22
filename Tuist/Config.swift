02c9c32492c218b4d57be9d1b8cab9a00aec9a20
Sam Pettersson <sam@sampettersson.com>
2022-03-22 10:19:53 +0100

    Fix compile error on 13.3

--- Tuist/Config.swift
+++ Tuist/Config.swift
@@ -1,6 +1,6 @@
 import ProjectDescription
 
 let config = Config(
-    compatibleXcodeVersions: ["13.1", "13.2.1"],
+    compatibleXcodeVersions: ["13.1", "13.2.1", "13.3"],
     generationOptions: .options()
 )
