8bb86716706a273fd4b4d4a1c456a4a962472d62
Sam Pettersson <sam@sampettersson.com>
2022-04-14 09:08:56 +0200

    Use copy instead of move

--- scripts/post-build-action.sh
+++ scripts/post-build-action.sh
@@ -1,7 +1,7 @@
 rm -rf "${TARGET_BUILD_DIR}/${TARGET_NAME}.app/Frameworks/"*".framework"/Frameworks/FirebaseAnalytics.framework
 rm -rf "${TARGET_BUILD_DIR}/${TARGET_NAME}.app/Frameworks/"*".framework"/Frameworks/GoogleAppMeasurement.framework
-mv -iv -- "${CONFIGURATION_BUILD_DIR}/"Adyen3DS2.framework "${TARGET_BUILD_DIR}/${TARGET_NAME}.app/Frameworks/"Adyen3DS2.framework
-mv -iv -- "${CONFIGURATION_BUILD_DIR}/"Shake.framework "${TARGET_BUILD_DIR}/${TARGET_NAME}.app/Frameworks/"Shake.framework
+cp -rf "${CONFIGURATION_BUILD_DIR}/"Adyen3DS2.framework "${TARGET_BUILD_DIR}/${TARGET_NAME}.app/Frameworks/"Adyen3DS2.framework
+cp -rf "${CONFIGURATION_BUILD_DIR}/"Shake.framework "${TARGET_BUILD_DIR}/${TARGET_NAME}.app/Frameworks/"Shake.framework
 rm -rf "${TARGET_BUILD_DIR}/${TARGET_NAME}.app/Frameworks/"*".framework"/Frameworks
 
 find "${TARGET_BUILD_DIR}" -name '*.framework' -print0 | while read -d $'\0' framework
