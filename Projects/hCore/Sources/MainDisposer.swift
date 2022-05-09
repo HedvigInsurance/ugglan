1be5e56f88f06b03800e2caa6bdff6cfc553a6d4
sampettersson <sampettersson@users.noreply.github.com>
2022-05-09 08:22:51 +0000

    Formatting

--- Projects/hCore/Sources/MainDisposer.swift
+++ Projects/hCore/Sources/MainDisposer.swift
@@ -1,13 +1,15 @@
-import Foundation
 import Flow
+import Foundation
 
 public struct DisposeOnMain: Disposable {
     var disposable: Disposable
-    
-    public init(_ disposable: Disposable) {
+
+    public init(
+        _ disposable: Disposable
+    ) {
         self.disposable = disposable
     }
-    
+
     public func dispose() {
         DispatchQueue.main.async {
             disposable.dispose()
