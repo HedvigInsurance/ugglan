193610a89958dbe1d089691b32fc56a779b07ca7
Satish <satish.bandaru1234@gmail.com>
2022-05-30 11:16:58 +0200

    connect claims and contracts polling data to profile

--- Projects/App/Sources/Journeys/DeleteAccountJourney.swift
+++ Projects/App/Sources/Journeys/DeleteAccountJourney.swift
@@ -4,12 +4,22 @@ import Presentation
 import UIKit
 import hCore
 import hCoreUI
+import Claims
+import Contracts
 
 extension AppJourney {
     static var deleteAccountJourney: some JourneyPresentation {
-        HostingJourney(
+        let claimsStore: ClaimsStore = globalPresentableStoreContainer.get()
+        let contractsStore: ContractStore = globalPresentableStoreContainer.get()
+        
+        return HostingJourney(
             UgglanStore.self,
-            rootView: DeleteAccountView()
+            rootView: DeleteAccountView(
+                viewModel: DeleteAccountViewModel(
+                    claimsStore: claimsStore,
+                    contractsStore: contractsStore
+                )
+            )
         ) { action in
             if action == .openChat {
                 AppJourney.freeTextChat()
--- Projects/App/Sources/Screens/ProfileScreen/Screens/DeleteAccount/DeleteAccountView.swift
+++ Projects/App/Sources/Screens/ProfileScreen/Screens/DeleteAccount/DeleteAccountView.swift
@@ -1,11 +1,58 @@
 import Foundation
+import Flow
 import SwiftUI
 import hCore
+import Combine
+import Claims
+import Contracts
 
 struct DeleteAccountView: View {
-    @PresentableStore var store: UgglanStore
+    @ObservedObject var viewModel: DeleteAccountViewModel
     
     var body: some View {
-        Text("Placeholder text")
+        if viewModel.hasActiveClaims {
+            // Show error state for having claims
+            Text("has active claims")
+        } else if viewModel.hasActiveContracts {
+            // Show error state for having active contracts
+            Text("has active contracts")
+        } else {
+            // Show the screen for deleting claims
+            Text("Placeholder text")
+        }
+    }
+}
+
+class DeleteAccountViewModel: ObservableObject {
+    let claimsStore: ClaimsStore
+    let contractsStore: ContractStore
+    
+    let bag = DisposeBag()
+    
+    var activeClaimsSignal: ReadSignal<Bool> {
+        self.claimsStore.stateSignal.map { $0.hasActiveClaims }
+    }
+    
+    var activeContractsSignal: ReadSignal<Bool> {
+        self.contractsStore.stateSignal.map { $0.hasActiveContracts }
+    }
+    
+    @Published var hasActiveClaims: Bool = false
+    @Published var hasActiveContracts: Bool = false
+    
+    internal init(
+        claimsStore: ClaimsStore,
+        contractsStore: ContractStore
+    ) {
+        self.claimsStore = claimsStore
+        self.contractsStore = contractsStore
+        
+        self.hasActiveClaims = activeClaimsSignal.value
+        self.hasActiveContracts = activeContractsSignal.value
+        
+        bag += activeClaimsSignal.onValue {
+            self.hasActiveClaims = $0
+        }
+        bag += activeContractsSignal.plain().onValue { self.hasActiveContracts = $0 }
     }
 }
