c505295e25d1386140c3283c7ddb06162c8c5019
Sam Pettersson <sam@sampettersson.com>
2022-06-23 12:51:03 +0200

    LS the path

--- scripts/codegen.sh
+++ scripts/codegen.sh
@@ -9,4 +9,6 @@ tuist generate --path Projects/Codegen --no-open
 
 x=$( xcodebuild -showBuildSettings -project Projects/Codegen/Codegen.xcodeproj | grep ' BUILD_DIR =' | sed -e 's/.*= *//' )
 
+ls -R $x
+
 DYLD_FRAMEWORK_PATH=$x/Debug DYLD_LIBRARY_PATH=$x/Debug $x/Debug/Codegen.app/Contents/MacOS/Codegen
