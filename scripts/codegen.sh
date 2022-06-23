13209ca3a519d9baa4f616989b3f5db3ef02ada4
Sam Pettersson <sam@sampettersson.com>
2022-06-23 12:21:32 +0200

    Move resolve

--- scripts/codegen.sh
+++ scripts/codegen.sh
@@ -4,6 +4,6 @@ set -x
 
 tuist generate --path Projects/Codegen --no-open
 
-x=$( xcodebuild -showBuildSettings -resolvePackageDependencies -project Projects/Codegen/Codegen.xcodeproj | grep ' BUILD_DIR =' | sed -e 's/.*= *//' )
+x=$(xcodebuild -resolvePackageDependencies -showBuildSettings -project Projects/Codegen/Codegen.xcodeproj | grep ' BUILD_DIR =' | sed -e 's/.*= *//' )
 
 DYLD_FRAMEWORK_PATH=$x/Debug DYLD_LIBRARY_PATH=$x/Debug $x/Debug/Codegen.app/Contents/MacOS/Codegen
