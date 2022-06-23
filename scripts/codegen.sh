48d962583c9c04fe2465b63aca3c6ec0ea581483
Sam Pettersson <sam@sampettersson.com>
2022-06-23 12:57:43 +0200

    Command fix

--- scripts/codegen.sh
+++ scripts/codegen.sh
@@ -7,8 +7,12 @@ defaults write com.apple.dt.Xcode IDEDisableAutomaticPackageResolution -bool NO
 
 tuist generate --path Projects/Codegen --no-open
 
-x=$( xcodebuild -showBuildSettings -derivedDataPath $CI_DERIVED_DATA_PATH -project Projects/Codegen/Codegen.xcodeproj | grep ' BUILD_DIR =' | sed -e 's/.*= *//' )
+if [[ -z "${CI_DERIVED_DATA_PATH}" ]]; then
+  x=$( xcodebuild -showBuildSettings -project Projects/Codegen/Codegen.xcodeproj | grep ' BUILD_DIR =' | sed -e 's/.*= *//' )
 
-ls -R $x
+  DYLD_FRAMEWORK_PATH=$x/Debug DYLD_LIBRARY_PATH=$x/Debug $x/Debug/Codegen.app/Contents/MacOS/Codegen
+else
+  x=$( xcodebuild -showBuildSettings -derivedDataPath $CI_DERIVED_DATA_PATH -scheme Codegen -project Projects/Codegen/Codegen.xcodeproj | grep ' BUILD_DIR =' | sed -e 's/.*= *//' )
 
-DYLD_FRAMEWORK_PATH=$x/Debug DYLD_LIBRARY_PATH=$x/Debug $x/Debug/Codegen.app/Contents/MacOS/Codegen
+  DYLD_FRAMEWORK_PATH=$x/Debug DYLD_LIBRARY_PATH=$x/Debug $x/Debug/Codegen.app/Contents/MacOS/Codegen
+fi
