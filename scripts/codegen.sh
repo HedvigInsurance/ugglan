7fbfad4661fcc8853f3ef4358492b3aae58fe533
Sam Pettersson <sam@sampettersson.com>
2022-06-23 12:45:17 +0200

    Switch back to project

--- scripts/codegen.sh
+++ scripts/codegen.sh
@@ -7,6 +7,6 @@ defaults write com.apple.dt.Xcode IDEDisableAutomaticPackageResolution -bool NO
 
 tuist generate --path Projects/Codegen --no-open
 
-x=$(xcodebuild -showBuildSettings -workspace Projects/Codegen/Codegen.workspace | grep ' BUILD_DIR =' | sed -e 's/.*= *//' )
+x=$( xcodebuild -showBuildSettings -project Projects/Codegen/Codegen.project | grep ' BUILD_DIR =' | sed -e 's/.*= *//' )
 
 DYLD_FRAMEWORK_PATH=$x/Debug DYLD_LIBRARY_PATH=$x/Debug $x/Debug/Codegen.app/Contents/MacOS/Codegen
