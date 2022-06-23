69ddababe45aac6d75842d60106d1a820592e927
Sam Pettersson <sam@sampettersson.com>
2022-06-23 12:36:07 +0200

    Fix package resolution

--- scripts/codegen.sh
+++ scripts/codegen.sh
@@ -2,8 +2,11 @@
 set -e
 set -x
 
+Set default com.apple.dt.Xcode IDEPackageOnlyUseVersionsFromResolvedFile to false
+Set default com.apple.dt.Xcode IDEDisableAutomaticPackageResolution to false
+
 tuist generate --path Projects/Codegen --no-open
 
-x=$(xcodebuild -resolvePackageDependencies -showBuildSettings -workspace Projects/Codegen/Codegen.workspace | grep ' BUILD_DIR =' | sed -e 's/.*= *//' )
+x=$(xcodebuild -showBuildSettings -workspace Projects/Codegen/Codegen.workspace | grep ' BUILD_DIR =' | sed -e 's/.*= *//' )
 
 DYLD_FRAMEWORK_PATH=$x/Debug DYLD_LIBRARY_PATH=$x/Debug $x/Debug/Codegen.app/Contents/MacOS/Codegen
