c1ce9b063583385c9e73ff02fe68ca6e49503fb3
sampettersson <sampettersson@users.noreply.github.com>
2022-06-28 07:01:05 +0000

    Formatting

--- Projects/App/Sources/Journeys/OnboardingJourney.swift
+++ Projects/App/Sources/Journeys/OnboardingJourney.swift
@@ -3,26 +3,28 @@ import Flow
 import Foundation
 import Presentation
 import UIKit
+import hAnalytics
 import hCore
 import hCoreUI
 import hGraphQL
-import hAnalytics
 
 extension AppJourney {
     @JourneyBuilder
     static func onboarding() -> some JourneyPresentation {
-        ContinueJourney().onPresent {
-            var webUrl = Environment.current.webBaseURL
-            webUrl.appendPathComponent(Localization.Locale.currentLocale.webPath)
-            webUrl.appendPathComponent("new-member")
-            webUrl = webUrl
-                .appending("utm_source", value: "ios")
-                .appending("utm_medium", value: "hedvig-app")
-                .appending("utm_campaign", value: Localization.Locale.currentLocale.market.rawValue.lowercased())
-            
-            UIApplication.shared.open(webUrl)
-            
-            hAnalyticsEvent.redirectedToWebOnboarding().send()
-        }
+        ContinueJourney()
+            .onPresent {
+                var webUrl = Environment.current.webBaseURL
+                webUrl.appendPathComponent(Localization.Locale.currentLocale.webPath)
+                webUrl.appendPathComponent("new-member")
+                webUrl =
+                    webUrl
+                    .appending("utm_source", value: "ios")
+                    .appending("utm_medium", value: "hedvig-app")
+                    .appending("utm_campaign", value: Localization.Locale.currentLocale.market.rawValue.lowercased())
+
+                UIApplication.shared.open(webUrl)
+
+                hAnalyticsEvent.redirectedToWebOnboarding().send()
+            }
     }
 }
