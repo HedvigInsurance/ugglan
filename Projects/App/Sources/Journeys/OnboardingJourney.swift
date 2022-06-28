fe27e1775dc159122233e7d878a54b922a8c8538
sampettersson <sampettersson@users.noreply.github.com>
2022-06-28 06:44:34 +0000

    Formatting

--- Projects/App/Sources/Journeys/OnboardingJourney.swift
+++ Projects/App/Sources/Journeys/OnboardingJourney.swift
@@ -3,23 +3,24 @@ import Flow
 import Foundation
 import Presentation
 import UIKit
+import hAnalytics
 import hCore
 import hCoreUI
 import hGraphQL
-import hAnalytics
 
 extension AppJourney {
     @JourneyBuilder
     static func onboarding() -> some JourneyPresentation {
-        ContinueJourney().onPresent {
-            var webUrl = Environment.current.webBaseURL
-            webUrl.appendPathComponent(Localization.Locale.currentLocale.webPath)
-            webUrl.appendPathComponent("new-member")
-            webUrl = webUrl.appending("utm_source", value: "hedvigIOSApp")
-            
-            UIApplication.shared.open(webUrl)
-            
-            hAnalyticsEvent.redirectedToWebOnboarding().send()
-        }
+        ContinueJourney()
+            .onPresent {
+                var webUrl = Environment.current.webBaseURL
+                webUrl.appendPathComponent(Localization.Locale.currentLocale.webPath)
+                webUrl.appendPathComponent("new-member")
+                webUrl = webUrl.appending("utm_source", value: "hedvigIOSApp")
+
+                UIApplication.shared.open(webUrl)
+
+                hAnalyticsEvent.redirectedToWebOnboarding().send()
+            }
     }
 }
