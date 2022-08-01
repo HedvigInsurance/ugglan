026b49971540dc5d55884dc15f891c1d17e8cf7c
Satish <satish.bandaru1234@gmail.com>
2022-08-01 11:26:46 +0200

    Resolve conflict

--- Projects/hCoreUI/Sources/ImageWithBlurHash.swift
+++ Projects/hCoreUI/Sources/ImageWithBlurHash.swift
@@ -39,41 +39,22 @@ public struct ImageWithHashFallBack: ViewModifier {
     }
 
     public func body(content: Content) -> some View {
-        if #available(iOS 14, *) {
-            content
-                .background(
-                    KFImage(imageURL)
-                        .fade(duration: 0.25)
+        content
+            .background(
+                KFImage(imageURL)
+                    .fade(duration: 0.25)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .edgesIgnoringSafeArea(.all)
+                    .background(
+                        Image(
+                            uiImage: UIImage(
+                                blurHash: blurHash,
+                                size: .init(width: 32, height: 32)
+                            ) ?? UIImage()
+                        )
                         .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .edgesIgnoringSafeArea(.all)
-                        .background(
-                            Image(
-                                uiImage: UIImage(
-                                    blurHash: blurHash,
-                                    size: .init(width: 32, height: 32)
-                                ) ?? UIImage()
-                            )
-                            .resizable()
-                        )
-                )
-
-        } else {
-            content
-                .background(
-                    RemoteImage(url: imageURL)
-                        .aspectRatio(contentMode: .fill)
-                        .background(
-                            Image(
-                                uiImage: UIImage(
-                                    blurHash: blurHash,
-                                    size: .init(width: 32, height: 32)
-                                ) ?? UIImage()
-                            )
-                            .resizable()
-                        )
-                )
-
-        }
+                    )
+            )
     }
 }
