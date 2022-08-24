fe4ac187f886427db1a02c2247c7244569226281
Axel Backlund <19402158+axelbacklund@users.noreply.github.com>
2022-08-24 17:04:52 +0200

    Remove version checks

--- Projects/hCoreUI/Sources/hForm/Gradient/hGradient.swift
+++ Projects/hCoreUI/Sources/hForm/Gradient/hGradient.swift
@@ -16,30 +16,26 @@ struct hGradient: View {
     @State private var colors: [Color] = []
 
     var body: some View {
-        if #available(iOS 14.0, *) {
-            Rectangle()
-                .animatableGradient(
-                    fromGradient: Gradient(colors: oldGradientType.colors(for: colorScheme)),
-                    toGradient: Gradient(colors: newGradientType.colors(for: colorScheme)),
-                    progress: animate ? progress : 1
-                )
-                .edgesIgnoringSafeArea(.all)
-                .onAppear {
-                    if !hasAnimatedCurrentTypes {
-                        self.progress = 0
-                        withAnimation(.easeOut(duration: 1.0)) {
-                            self.progress = 1
-                        }
-                        hasAnimatedCurrentTypes = true
-                    } else {
+        Rectangle()
+            .animatableGradient(
+                fromGradient: Gradient(colors: oldGradientType.colors(for: colorScheme)),
+                toGradient: Gradient(colors: newGradientType.colors(for: colorScheme)),
+                progress: animate ? progress : 1
+            )
+            .edgesIgnoringSafeArea(.all)
+            .onAppear {
+                if !hasAnimatedCurrentTypes {
+                    self.progress = 0
+                    withAnimation(.easeOut(duration: 1.0)) {
                         self.progress = 1
                     }
+                    hasAnimatedCurrentTypes = true
+                } else {
+                    self.progress = 1
                 }
-                .onChange(of: newGradientType) { _ in
-                    hasAnimatedCurrentTypes = false
-                }
-        } else {
-            EmptyView()
-        }
+            }
+            .onChange(of: newGradientType) { _ in
+                hasAnimatedCurrentTypes = false
+            }
     }
 }
