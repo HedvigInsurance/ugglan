d57a6827b046f910d4f93428f4d7860a9b003240
Axel Backlund <19402158+axelbacklund@users.noreply.github.com>
2022-08-24 09:20:29 +0200

    SwiftUI previews and adjust row padding

--- Projects/Forever/Sources/Components/InivtationTable.swift
+++ Projects/Forever/Sources/Components/InivtationTable.swift
@@ -2,6 +2,7 @@ import Foundation
 import SwiftUI
 import hCore
 import hCoreUI
+import hGraphQL
 
 extension ForeverInvitation {
     var imageAsset: ImageAsset {
@@ -58,7 +59,6 @@ struct InvitationTable: View {
     @PresentableStore var store: ForeverStore
 
     var body: some View {
-
         PresentableStoreLens(
             ForeverStore.self,
             getter: { state in
@@ -67,22 +67,7 @@ struct InvitationTable: View {
         ) { invitations in
             if !invitations.isEmpty {
                 hSection(invitations, id: \.name) { row in
-                    hRow {
-                        HStack {
-                            Image(uiImage: row.imageAsset.image).resizable().frame(width: 18, height: 18)
-                            VStack(alignment: .leading) {
-                                hText(row.name).foregroundColor(row.nameLabelColor)
-                                if row.invitedByOther {
-                                    hText(L10n.ReferallsInviteeStates.invitedYou, style: .subheadline)
-                                        .foregroundColor(row.invitedByOtherLabelColor)
-                                }
-                            }
-                        }
-                    }
-                    .withCustomAccessory({
-                        Spacer()
-                        hText(row.discountLabelText).foregroundColor(row.discountLabelColor)
-                    })
+                    InvitationRow(row: row)
                 }
                 .withHeader {
                     hText(L10n.ReferralsActive.Invited.title, style: .title3)
@@ -91,5 +76,55 @@ struct InvitationTable: View {
             }
         }
 
+    }
+}
+
+struct InvitationRow: View {
+    let row: ForeverInvitation
+
+    var body: some View {
+        hRow {
+            HStack {
+                Image(uiImage: row.imageAsset.image).resizable().frame(width: 18, height: 18)
+                VStack(alignment: .leading) {
+                    hText(row.name).foregroundColor(row.nameLabelColor)
+                    if row.invitedByOther {
+                        hText(L10n.ReferallsInviteeStates.invitedYou, style: .subheadline)
+                            .foregroundColor(row.invitedByOtherLabelColor)
+                    }
+                }
+            }
+        }
+        .withCustomAccessory({
+            Spacer()
+            hText(row.discountLabelText).foregroundColor(row.discountLabelColor)
+        })
+        .verticalPadding(row.invitedByOther ? 14 : 21)
+    }
+}
+
+struct InvitationRow_Previews: PreviewProvider {
+    static var mockRow: ForeverInvitation = .init(
+        name: "Axel",
+        state: .active,
+        discount: MonetaryAmount(amount: "10.0", currency: "SEK"),
+        invitedByOther: true
+    )
+    static var mockRow2: ForeverInvitation = .init(
+        name: "Mock",
+        state: .active,
+        discount: MonetaryAmount(amount: "10.0", currency: "SEK"),
+        invitedByOther: false
+    )
+
+    static var previews: some View {
+        hSection {
+            InvitationRow(row: mockRow2)
+            InvitationRow(row: mockRow)
+            InvitationRow(row: mockRow2)
+            InvitationRow(row: mockRow2)
+        }
+        .sectionContainerStyle(.transparent)
+        .previewLayout(PreviewLayout.sizeThatFits)
     }
 }
