d4785bc1a1cc38178ca28aa257c30c118c369502
Sam Pettersson <sam@sampettersson.com>
2022-09-22 19:38:43 +0200

    Fix codegen

--- scripts/codegen.sh
+++ scripts/codegen.sh
@@ -8,13 +8,13 @@ defaults write com.apple.dt.Xcode IDEDisableAutomaticPackageResolution -bool NO
 tuist generate --path Projects/Codegen --no-open
 
 if [[ -z "${CI_DERIVED_DATA_PATH}" ]]; then
-  xcodebuild build -destination 'platform=OS X,arch=arm64' -scheme Codegen -project Projects/Codegen/Codegen.xcodeproj
+  xcodebuild build -destination 'platform=macOS,arch=x86_64' -scheme Codegen -project Projects/Codegen/Codegen.xcodeproj
 
   x=$(xcodebuild -showBuildSettings -scheme Codegen -project Projects/Codegen/Codegen.xcodeproj | grep ' BUILD_DIR =' | sed -e 's/.*= *//' )
 
   DYLD_FRAMEWORK_PATH=$x/Debug DYLD_LIBRARY_PATH=$x/Debug $x/Debug/Codegen.app/Contents/MacOS/Codegen
 else
-  xcodebuild build -destination 'platform=OS X,arch=arm64' -derivedDataPath $CI_DERIVED_DATA_PATH -scheme Codegen -project Projects/Codegen/Codegen.xcodeproj
+  xcodebuild build -destination 'platform=macOS,arch=arm64' -derivedDataPath $CI_DERIVED_DATA_PATH -scheme Codegen -project Projects/Codegen/Codegen.xcodeproj
 
   x=$(xcodebuild -showBuildSettings -scheme Codegen -derivedDataPath $CI_DERIVED_DATA_PATH  -project Projects/Codegen/Codegen.xcodeproj | grep ' BUILD_DIR =' | sed -e 's/.*= *//' )
 
