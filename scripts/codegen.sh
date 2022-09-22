b864556dd7783ddc6bd791ce8d31f2d125d371f6
Sam Pettersson <sam@sampettersson.com>
2022-09-22 19:25:09 +0200

    Xcode cloud fix

--- scripts/codegen.sh
+++ scripts/codegen.sh
@@ -8,11 +8,13 @@ defaults write com.apple.dt.Xcode IDEDisableAutomaticPackageResolution -bool NO
 tuist generate --path Projects/Codegen --no-open
 
 if [[ -z "${CI_DERIVED_DATA_PATH}" ]]; then
-  x=$( xcodebuild -showBuildSettings -project Projects/Codegen/Codegen.xcodeproj | grep ' BUILD_DIR =' | sed -e 's/.*= *//' )
+  xcodebuild build -destination 'platform=OS X,arch=arm64' -scheme Codegen -project Projects/Codegen/Codegen.xcodeproj
+  
+  x=$(xcodebuild -showBuildSettings -scheme Codegen -project Projects/Codegen/Codegen.xcodeproj | grep ' BUILD_DIR =' | sed -e 's/.*= *//' )
 
   DYLD_FRAMEWORK_PATH=$x/Debug DYLD_LIBRARY_PATH=$x/Debug $x/Debug/Codegen.app/Contents/MacOS/Codegen
 else
-  x=$( xcodebuild -showBuildSettings -derivedDataPath $CI_DERIVED_DATA_PATH -scheme Codegen -project Projects/Codegen/Codegen.xcodeproj | grep ' BUILD_DIR =' | sed -e 's/.*= *//' )
+  x=$(xcodebuild -showBuildSettings -scheme Codegen -derivedDataPath $CI_DERIVED_DATA_PATH  -project Projects/Codegen/Codegen.xcodeproj | grep ' BUILD_DIR =' | sed -e 's/.*= *//' )
 
   DYLD_FRAMEWORK_PATH=$x/Debug DYLD_LIBRARY_PATH=$x/Debug $x/Debug/Codegen.app/Contents/MacOS/Codegen
 fi
