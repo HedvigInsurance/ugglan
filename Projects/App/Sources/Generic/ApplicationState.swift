b1bc01f5d071dd60f72b11a0f01379b1aac7d0f3
Sam Pettersson <sam@sampettersson.com>
2022-09-28 16:14:34 +0200

    Remove some firebase stuff

--- Projects/App/Sources/Generic/ApplicationState.swift
+++ Projects/App/Sources/Generic/ApplicationState.swift
@@ -8,16 +8,6 @@ import hCoreUI
 import hGraphQL
 
 extension ApplicationState {
-    private static let firebaseMessagingTokenKey = "firebaseMessagingToken"
-
-    static func setFirebaseMessagingToken(_ token: String) {
-        UserDefaults.standard.set(token, forKey: ApplicationState.firebaseMessagingTokenKey)
-    }
-
-    static func getFirebaseMessagingToken() -> String? {
-        UserDefaults.standard.value(forKey: firebaseMessagingTokenKey) as? String
-    }
-
     public static let lastNewsSeenKey = "lastNewsSeen"
 
     static func getLastNewsSeen() -> String {
