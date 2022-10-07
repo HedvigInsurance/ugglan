46b2ad234b044553d6d8d0998609141673148ef9
Satish Bandaru <satish@hedvig.com>
2022-10-07 08:52:09 +0200

    Delete String+URLScheme.swift

--- Projects/hCore/Sources/String+URLScheme.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-extension String {
-    public var isValidURL: Bool {
-        let urlRegEx =
-            "((?:http|https)://)?(?:[\\w\\d\\-_]+\\.)?[\\w\\d\\-_]+\\.\\w{2,3}(\\.\\w{2})?(/(?<=/)(?:[\\w\\d\\-./_]+)?)?"
-        return NSPredicate(format: "SELF MATCHES %@", urlRegEx).evaluate(with: self)
-    }
-}
