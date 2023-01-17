f568795363680a95690ba18f5c213e977262e020
Sam Pettersson <sam@sampettersson.com>
2023-01-17 09:21:27 +0100

    Bump Odyssey

--- Tuist/ProjectDescriptionHelpers/Project+DependenciesTemplate.swift
+++ Tuist/ProjectDescriptionHelpers/Project+DependenciesTemplate.swift
@@ -99,7 +99,7 @@ public enum ExternalDependencies: CaseIterable {
             ]
         case .odysseyKit:
             return [
-                .package(url: "https://github.com/HedvigInsurance/OdysseyKit.git", .exact("1.169.0"))
+                .package(url: "https://github.com/HedvigInsurance/OdysseyKit.git", .exact("1.184.0"))
             ]
         case .authlib:
             return [
