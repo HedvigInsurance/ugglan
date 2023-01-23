7d07967385620d26159f7ff82f42b87ac3021f27
Sam Pettersson <sam@sampettersson.com>
2023-01-23 10:41:49 +0100

    Add WKNavigationDelegate to allow

--- Projects/Authentication/Sources/Zignsec/ZignsecWebview.swift
+++ Projects/Authentication/Sources/Zignsec/ZignsecWebview.swift
@@ -5,17 +5,36 @@ import hCore
 import hCoreUI
 
 struct WebView: UIViewRepresentable {
-
     var url: URL
+    
+    class Coordinator: NSObject, WKNavigationDelegate {
+        func webView(
+            _ webView: WKWebView,
+            decidePolicyFor navigationAction: WKNavigationAction
+        ) async -> WKNavigationActionPolicy {
+            if (navigationAction.request.url?.host == "hedvig.com") {
+                return .cancel
+            }
+            
+            return .allow
+        }
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator()
+    }
 
     func makeUIView(context: Context) -> WKWebView {
-        return WKWebView()
+        let webview = WKWebView()
+        webview.navigationDelegate = context.coordinator
+        
+        let request = URLRequest(url: url)
+        webview.load(request)
+        
+        return webview
     }
 
-    func updateUIView(_ webView: WKWebView, context: Context) {
-        let request = URLRequest(url: url)
-        webView.load(request)
-    }
+    func updateUIView(_ webView: WKWebView, context: Context) {}
 }
 
 public struct ZignsecWebview: View {
@@ -24,7 +43,10 @@ public struct ZignsecWebview: View {
     @PresentableStore var store: AuthenticationStore
 
     public var body: some View {
-        PresentableStoreLens(AuthenticationStore.self, getter: { state in state.zignsecState.webviewUrl }) { url in
+        PresentableStoreLens(
+            AuthenticationStore.self,
+            getter: { state in state.zignsecState.webviewUrl }
+        ) { url in
             if let url = url {
                 WebView(url: url)
             }
