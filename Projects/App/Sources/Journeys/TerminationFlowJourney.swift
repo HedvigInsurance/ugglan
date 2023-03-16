fd355b4ac236e722a131d8812a0e7e775417206e
julia-andersson <julia.andersson@hedvig.com>
2023-03-16 13:45:41 +0100

    changed to dismiss instead of pop

--- Projects/App/Sources/Journeys/TerminationFlowJourney.swift
+++ Projects/App/Sources/Journeys/TerminationFlowJourney.swift
@@ -30,7 +30,7 @@ extension AppJourney {
         ) {
             action in
             if case .dismissTerminationFlow = action {
-                PopJourney()
+                DismissJourney()
             }
         }
         .setScrollEdgeNavigationBarAppearanceToStandard
