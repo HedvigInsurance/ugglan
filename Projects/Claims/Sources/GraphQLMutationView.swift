ce1abe42781d4f898207d7546b4ba42308f4eb20
sladan-hedvig <sladan-hedvig@users.noreply.github.com>
2023-04-18 11:25:26 +0000

    Formatting

--- Projects/Claims/Sources/GraphQLMutationView.swift
+++ Projects/Claims/Sources/GraphQLMutationView.swift
@@ -1,15 +1,15 @@
+import Apollo
 import Foundation
 import SwiftUI
-import Apollo
 
 struct GraphQLMutationView<Mutation: GraphQLMutation>: View {
     var client: ApolloClient
     var mutation: Mutation
-    
+
     var body: some View {
-        EmptyView().onAppear {
-            
-        }
+        EmptyView()
+            .onAppear {
+
+            }
     }
 }
-
