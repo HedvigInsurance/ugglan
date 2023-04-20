250333e2f78ac8c7bc9c403d89707962d6f96f84
julia-andersson <julia.andersson@hedvig.com>
2023-04-20 15:16:26 +0200

    Updated audio recording schema and model

--- Projects/Claims/Sources/Models/FlowClaimAudioRecordingStepModel.swift
+++ Projects/Claims/Sources/Models/FlowClaimAudioRecordingStepModel.swift
@@ -4,13 +4,28 @@ import hGraphQL
 public struct FlowClaimAudioRecordingStepModel: FlowClaimStepModel {
     let id: String
     let questions: [String]
-    let signedUrl: String?
+    let audioContent: AudioContentModel?
     var url: URL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
     init(
         with data: OctopusGraphQL.FlowClaimAudioRecordingStepFragment
     ) {
         self.id = data.id
         self.questions = data.questions
+        self.audioContent = .init(with: (data.audioContent?.fragments.flowClaimAudioContentFragment))
+    }
+}
+
+struct AudioContentModel: Codable, Equatable, Hashable {
+    let audioUrl: String
+    let signedUrl: String
+
+    init?(
+        with data: OctopusGraphQL.FlowClaimAudioContentFragment?
+    ) {
+        guard let data else {
+            return nil
+        }
+        self.audioUrl = data.audioUrl
         self.signedUrl = data.signedUrl
     }
 }
