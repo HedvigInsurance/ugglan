b331a1b643170ee6963c9b5f492cb303847f5776
Sladan Nimcevic <sladan.nimcevic@hedvig.com>
2023-04-24 18:34:51 +0200

    Returned filter for  singled cross sells

--- Projects/Contracts/Sources/View/CrossSellingStack.swift
+++ Projects/Contracts/Sources/View/CrossSellingStack.swift
@@ -9,10 +9,10 @@ struct CrossSellingStack: View {
             ContractStore.self,
             getter: { state in
                 state.crossSells
-//                state.contractBundles.flatMap { $0.crossSells }
-//                    .filter { crossSell in
-//                        !state.signedCrossSells.contains(crossSell)
-//                    }
+                state.contractBundles.flatMap { $0.crossSells }
+                    .filter { crossSell in
+                        !state.signedCrossSells.contains(crossSell)
+                    }
             }
         ) { crossSells in
             if !crossSells.isEmpty {
