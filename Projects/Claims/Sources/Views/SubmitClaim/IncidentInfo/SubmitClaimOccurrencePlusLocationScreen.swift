c0d783d547e24fdf739f0b9e6529ab8801e110b3
julia-andersson <julia.andersson@hedvig.com>
2023-06-02 14:11:51 +0200

    code refactoring

--- Projects/Claims/Sources/Views/SubmitClaim/IncidentInfo/SubmitClaimOccurrencePlusLocationScreen.swift
+++ Projects/Claims/Sources/Views/SubmitClaim/IncidentInfo/SubmitClaimOccurrencePlusLocationScreen.swift
@@ -12,143 +12,26 @@ struct SubmitClaimOccurrencePlusLocationScreen: View {
 
     var body: some View {
         LoadingViewWithContent(.postDateOfOccurrenceAndLocation) {
-
             if orientation.isLandscape || UIWindow.isLandscape {
-
                 hForm {
-                    ProgressBar()
-                    hTextNew(L10n.Claims.Incident.Screen.Date.Of.incident, style: .title2)
-                        .foregroundColor(hLabelColorNew.primary)
-                        .fixedSize(horizontal: false, vertical: true)
-                        .padding([.top, .leading, .trailing], 16)
-                        .multilineTextAlignment(.center)
-
-                    hSection {
-                        PresentableStoreLens(
-                            SubmitClaimStore.self,
-                            getter: { state in
-                                state.locationStep
-                            }
-                        ) { locationStep in
-
-                            hFloatingField(
-                                value: locationStep?.getSelectedOption()?.displayName ?? "",
-                                placeholder: L10n.Claims.Location.Screen.title,
-                                onTap: {
-                                    store.send(.navigationAction(action: .openLocationPicker(type: .setLocation)))
-                                }
-                            )
-                            .frame(height: 72)
-                        }
-                    }
-                    .withoutBottomPadding
-                    .sectionContainerStyle(.opaque(useNewDesign: true))
-
-                    hSection {
-                        PresentableStoreLens(
-                            SubmitClaimStore.self,
-                            getter: { state in
-                                state.dateOfOccurenceStep
-                            }
-                        ) { dateOfOccurenceStep in
-
-                            hFloatingField(
-                                value: dateOfOccurenceStep?.dateOfOccurence ?? "",
-                                placeholder: L10n.Claims.Item.Screen.Date.Of.Incident.button,
-                                onTap: {
-                                    store.send(.navigationAction(action: .openDatePicker(type: .setDateOfOccurrence)))
-                                }
-                            )
-                        }
-                    }
-                    .withoutBottomPadding
-                    .sectionContainerStyle(.opaque(useNewDesign: true))
-
-                    NoticeComponent(text: L10n.claimsDateNotSureNoticeLabel)
-                        .padding([.bottom, .top], 8)
+                    displayTitleAndProgressBar()
+                    displayFieldsAndNotice()
                 }
                 .hUseNewStyle
                 .hFormAttachToBottom {
-                    hButton.LargeButtonFilled {
-                        store.send(.dateOfOccurrenceAndLocationRequest)
-                    } content: {
-                        hText(L10n.generalContinueButton, style: .body)
-                            .foregroundColor(hLabelColor.primary.inverted)
-                    }
-                    .frame(maxWidth: .infinity, alignment: .bottom)
-                    .padding([.leading, .trailing], 16)
-                    .padding(.bottom, 20)
-
+                    displayButton()
                 }
             } else {
 
                 hForm {
-                    ProgressBar()
-                    hTextNew(L10n.Claims.Incident.Screen.Date.Of.incident, style: .title2)
-                        .foregroundColor(hLabelColorNew.primary)
-                        .fixedSize(horizontal: false, vertical: true)
-                        .padding([.top, .leading, .trailing], 16)
-                        .multilineTextAlignment(.center)
+                    displayTitleAndProgressBar()
                 }
                 .hDisableScroll
                 .hUseNewStyle
                 .hFormAttachToBottom {
                     VStack(spacing: 0) {
-                        hSection {
-                            PresentableStoreLens(
-                                SubmitClaimStore.self,
-                                getter: { state in
-                                    state.locationStep
-                                }
-                            ) { locationStep in
-
-                                hFloatingField(
-                                    value: locationStep?.getSelectedOption()?.displayName ?? "",
-                                    placeholder: L10n.Claims.Location.Screen.title,
-                                    onTap: {
-                                        store.send(.navigationAction(action: .openLocationPicker(type: .setLocation)))
-                                    }
-                                )
-                                .frame(height: 72)
-                            }
-                        }
-                        .withoutBottomPadding
-                        .sectionContainerStyle(.opaque(useNewDesign: true))
-
-                        hSection {
-                            PresentableStoreLens(
-                                SubmitClaimStore.self,
-                                getter: { state in
-                                    state.dateOfOccurenceStep
-                                }
-                            ) { dateOfOccurenceStep in
-
-                                hFloatingField(
-                                    value: dateOfOccurenceStep?.dateOfOccurence ?? "",
-                                    placeholder: L10n.Claims.Item.Screen.Date.Of.Incident.button,
-                                    onTap: {
-                                        store.send(
-                                            .navigationAction(action: .openDatePicker(type: .setDateOfOccurrence))
-                                        )
-                                    }
-                                )
-                            }
-                        }
-                        .withoutBottomPadding
-                        .sectionContainerStyle(.opaque(useNewDesign: true))
-
-                        NoticeComponent(text: L10n.claimsDateNotSureNoticeLabel)
-                            .padding([.bottom, .top], 8)
-
-                        hButton.LargeButtonFilled {
-                            store.send(.dateOfOccurrenceAndLocationRequest)
-                        } content: {
-                            hText(L10n.generalContinueButton, style: .body)
-                                .foregroundColor(hLabelColor.primary.inverted)
-                        }
-                        .frame(maxWidth: .infinity, alignment: .bottom)
-                        .padding([.leading, .trailing], 16)
-                        .padding(.bottom, 20)
+                        displayFieldsAndNotice()
+                        displayButton()
                     }
                 }
             }
@@ -164,6 +47,78 @@ struct SubmitClaimOccurrencePlusLocationScreen: View {
         .onRotate { newOrientation in
             orientation = newOrientation
         }
+    }
+
+    @ViewBuilder
+    private func displayTitleAndProgressBar() -> some View {
+        ProgressBar()
+        hTextNew(L10n.Claims.Incident.Screen.Date.Of.incident, style: .title2)
+            .foregroundColor(hLabelColorNew.primary)
+            .fixedSize(horizontal: false, vertical: true)
+            .padding([.top, .leading, .trailing], 16)
+            .multilineTextAlignment(.center)
+    }
+
+    @ViewBuilder
+    private func displayFieldsAndNotice() -> some View {
+        hSection {
+            PresentableStoreLens(
+                SubmitClaimStore.self,
+                getter: { state in
+                    state.locationStep
+                }
+            ) { locationStep in
+
+                hFloatingField(
+                    value: locationStep?.getSelectedOption()?.displayName ?? "",
+                    placeholder: L10n.Claims.Location.Screen.title,
+                    onTap: {
+                        store.send(.navigationAction(action: .openLocationPicker(type: .setLocation)))
+                    }
+                )
+                .frame(height: 72)
+            }
+        }
+        .withoutBottomPadding
+        .sectionContainerStyle(.opaque(useNewDesign: true))
+
+        hSection {
+            PresentableStoreLens(
+                SubmitClaimStore.self,
+                getter: { state in
+                    state.dateOfOccurenceStep
+                }
+            ) { dateOfOccurenceStep in
+
+                hFloatingField(
+                    value: dateOfOccurenceStep?.dateOfOccurence ?? "",
+                    placeholder: L10n.Claims.Item.Screen.Date.Of.Incident.button,
+                    onTap: {
+                        store.send(
+                            .navigationAction(action: .openDatePicker(type: .setDateOfOccurrence))
+                        )
+                    }
+                )
+            }
+        }
+        .withoutBottomPadding
+        .sectionContainerStyle(.opaque(useNewDesign: true))
+
+        NoticeComponent(text: L10n.claimsDateNotSureNoticeLabel)
+            .padding([.bottom, .top], 8)
+    }
+
+    @ViewBuilder
+    private func displayButton() -> some View {
+        hButton.LargeButtonFilled {
+            store.send(.dateOfOccurrenceAndLocationRequest)
+        } content: {
+            hText(L10n.generalContinueButton, style: .body)
+                .foregroundColor(hLabelColor.primary.inverted)
+        }
+        .frame(maxWidth: .infinity, alignment: .bottom)
+        .padding([.leading, .trailing], 16)
+        .padding(.bottom, 20)
     }
 }
 
