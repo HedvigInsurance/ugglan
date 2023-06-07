3849cbe976423f5c95ceda685e45b4e7327bd234
julia-andersson <julia.andersson@hedvig.com>
2023-06-07 11:25:39 +0200

    fix validation

--- Projects/Claims/Sources/Views/SubmitClaim/Entrypoints/SelectClaimEntrypoint.swift
+++ Projects/Claims/Sources/Views/SubmitClaim/Entrypoints/SelectClaimEntrypoint.swift
@@ -4,83 +4,8 @@ import TagKit
 import hCore
 import hCoreUI
 
-struct ShowTagList: View {
-    var tagsToShow: [String]
-    var onTap: (String) -> Void
-    var onButtonClick: () -> Void
-    @State var addPadding = true
-    @State var notValid = false
-    @State var selection: String? = nil
-
-    var body: some View {
-        VStack(spacing: 0) {
-            showNotValid
-            TagList(tags: tagsToShow) { tag in
-                HStack {
-                    hTextNew(tag, style: .body)
-                        .foregroundColor(hLabelColorNew.secondary)
-                        .lineLimit(1)
-                }
-                .onTapGesture {
-                    onTap(tag)
-                    selection = tag
-                    addPadding = true
-                    let generator = UIImpactFeedbackGenerator(style: .soft)
-                    generator.impactOccurred()
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                        addPadding = false
-                    }
-                }
-                .padding(.horizontal, ((selection == tag) && addPadding) ? 20 : 16)
-                .padding(.vertical, 8)
-                .background(getColorAndShadow(claimId: tag))
-            }
-            .padding([.leading, .trailing], 16)
-            .padding(.bottom, 24)
-
-            hButton.LargeButtonFilled {
-                if selection != nil {
-                    notValid = false
-                    onButtonClick()
-                } else {
-                    notValid = true
-                }
-                selection = ""
-            } content: {
-                hTextNew(L10n.saveAndContinueButtonLabel, style: .body)
-            }
-            .padding([.trailing, .leading], 16)
-        }
-    }
-
-    @ViewBuilder
-    var showNotValid: some View {
-        if notValid {
-            HStack {
-                Image(uiImage: hCoreUIAssets.infoSmall.image)
-                    .foregroundColor(hAmberColorNew.amber600)
-                hTextNew(L10n.claimsSelectCategory, style: .body)
-            }
-        }
-    }
-
-    @ViewBuilder
-    func getColorAndShadow(claimId: String) -> some View {
-        if selection == claimId {
-            Squircle.default()
-                .foregroundColor(hGreenColorNew.green50)
-                .hShadow()
-
-        } else {
-            Squircle.default()
-                .foregroundColor(hGrayscaleTranslucentColorNew.greyScaleTranslucentField)
-        }
-    }
-}
-
 struct SelectClaimEntrypointGroup: View {
     @PresentableStore var store: SubmitClaimStore
-
     @State var selectedClaimGroup: String? = nil
     @State var claimEntrypoints: [ClaimEntryPointResponseModel] = []
     var selectedEntrypoints: ([ClaimEntryPointResponseModel]) -> Void
@@ -297,5 +222,80 @@ struct SelectClaimEntrypointOption: View {
             }
         }
         return arr
+    }
+}
+
+struct ShowTagList: View {
+    var tagsToShow: [String]
+    var onTap: (String) -> Void
+    var onButtonClick: () -> Void
+    @State var addPadding = true
+    @State var notValid = false
+    @State var selection: String? = nil
+
+    var body: some View {
+        VStack(spacing: 0) {
+            showNotValid
+            TagList(tags: tagsToShow) { tag in
+                HStack {
+                    hTextNew(tag, style: .body)
+                        .foregroundColor(hLabelColorNew.secondary)
+                        .lineLimit(1)
+                }
+                .onTapGesture {
+                    onTap(tag)
+                    selection = tag
+                    addPadding = true
+                    notValid = false
+                    let generator = UIImpactFeedbackGenerator(style: .soft)
+                    generator.impactOccurred()
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                        addPadding = false
+                    }
+                }
+                .padding(.horizontal, ((selection == tag) && addPadding) ? 20 : 16)
+                .padding(.vertical, 8)
+                .background(getColorAndShadow(claimId: tag))
+            }
+            .padding([.leading, .trailing], 16)
+            .padding(.bottom, 24)
+
+            hButton.LargeButtonFilled {
+                if selection != nil && selection != "" {
+                    notValid = false
+                    onButtonClick()
+                } else {
+                    notValid = true
+                }
+                selection = ""
+            } content: {
+                hTextNew(L10n.saveAndContinueButtonLabel, style: .body)
+            }
+            .padding([.trailing, .leading], 16)
+        }
+    }
+
+    @ViewBuilder
+    var showNotValid: some View {
+        if notValid {
+            HStack {
+                Image(uiImage: hCoreUIAssets.infoSmall.image)
+                    .foregroundColor(hAmberColorNew.amber600)
+                hTextNew(L10n.claimsSelectCategory, style: .body)
+            }
+        }
+    }
+
+    @ViewBuilder
+    func getColorAndShadow(claimId: String) -> some View {
+        if selection == claimId {
+            Squircle.default()
+                .foregroundColor(hGreenColorNew.green50)
+                .hShadow()
+
+        } else {
+            Squircle.default()
+                .foregroundColor(hGrayscaleTranslucentColorNew.greyScaleTranslucentField)
+        }
     }
 }
