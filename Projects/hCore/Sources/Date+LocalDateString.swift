59379fc1b10ab1bc64cc612333d092ce4743941e
Sladan Nimcevic <sladan.nimcevic@hedvig.com>
2023-07-04 11:48:07 +0200

    Moved formatter to static let - better for performance

--- Projects/hCore/Sources/Date+LocalDateString.swift
+++ Projects/hCore/Sources/Date+LocalDateString.swift
@@ -2,26 +2,38 @@ import Foundation
 
 extension Date {
     public var localDateString: String {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd"
-        return formatter.string(from: self)
+        return DateFormatters.localDateStringFormatter.string(from: self)
     }
 
     public var localDateStringDayFirst: String? {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "dd.MM.yyyy"
-        return formatter.string(from: self)
+        return DateFormatters.localDateStringDayFirstFormatter.string(from: self)
     }
 
     public var displayDateDotFormat: String? {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy.MM.dd"
-        return formatter.string(from: self)
+        return DateFormatters.displayDateDotFormatFormatter.string(from: self)
     }
 
     /// A localDate string where a date of today is replaced with `today`
     public var localDateStringWithToday: String? {
         if Calendar.current.isDateInToday(self) { return L10n.startDateToday } else { return localDateString }
     }
+}
+private struct DateFormatters {
+    static let localDateStringFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        return formatter
+    }()
 
+    static let localDateStringDayFirstFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd.MM.yyyy"
+        return formatter
+    }()
+
+    static let displayDateDotFormatFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy.MM.dd"
+        return formatter
+    }()
 }
--- Projects/hCore/Sources/String+toDate.swift
+++ Projects/hCore/Sources/String+toDate.swift
@@ -2,12 +2,14 @@ import Foundation
 
 extension String {
     // converts a YYYY-MM-DD date-string to a Date
-    public var localDateToDate: Date? {
+    private static let localDateToDateFormatter: DateFormatter = {
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd"
-        return formatter.date(from: self)
+        return formatter
+    }()
+    public var localDateToDate: Date? {
+        return String.localDateToDateFormatter.date(from: self)
     }
-
     public func localDateToIso8601Date(format: String) -> Date? {
         let formatter = DateFormatter()
         formatter.calendar = Calendar(identifier: .iso8601)
