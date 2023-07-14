1fc07e9235bec56b41fbca82e60c79e1ce37e4e6
julia-andersson <julia.andersson@hedvig.com>
2023-07-14 08:26:31 +0200

    updated text label and cleaned up code

--- Projects/Forever/Sources/Components/PriceSectionView.swift
+++ Projects/Forever/Sources/Components/PriceSectionView.swift
@@ -24,17 +24,15 @@ extension View {
 }
 
 struct PriceSectionView: View {
-    @State var grossAmount: MonetaryAmount
     @State var netAmount: MonetaryAmount
 
     @State private var netAmountAnimate: MonetaryAmount = .init(amount: 0, currency: "")
-    @State private var discountAmountAnimate: MonetaryAmount = .init(amount: 0, currency: "")
 
     var body: some View {
         VStack(spacing: 0) {
-            hText("Your monthly cost")
+            hText(L10n.foreverTabMontlyCostLabel)
             HStack(spacing: 4) {
-                hText(grossAmount.formattedAmount)
+                hText(netAmountAnimate.formattedAmount + "/" + L10n.monthAbbreviationLabel)
                 Image(uiImage: hCoreUIAssets.infoIconFilled.image)
                     .onTapGesture {
                         // add scroll to bottom here
@@ -43,14 +41,9 @@ struct PriceSectionView: View {
             .foregroundColor(hTextColorNew.secondary)
         }
         .onAppear {
-            netAmountAnimate = .init(amount: grossAmount.amount, currency: netAmount.currency)
-            discountAmountAnimate = .init(amount: 0, currency: netAmount.currency)
+            netAmount = .init(amount: netAmount.amount, currency: netAmount.currency)
             withAnimation(Animation.easeIn(duration: 0.8).delay(0.7)) {
                 netAmountAnimate = netAmount
-                discountAmountAnimate = MonetaryAmount(
-                    amount: netAmount.value - grossAmount.value,
-                    currency: netAmount.currency
-                )
             }
         }
     }
--- Projects/Forever/Sources/Header.swift
+++ Projects/Forever/Sources/Header.swift
@@ -11,69 +11,66 @@ struct HeaderView: View {
 
     var body: some View {
         hSection {
-            VStack {
-                TemporaryCampaignBanner { /* TODO - can we remove this? */
-                    store.send(.showTemporaryCampaignDetail)
-                }
-                VStack(spacing: 24) {
-                    PresentableStoreLens(
-                        ForeverStore.self,
-                        getter: { state in
-                            state.foreverData
-                        }
-                    ) { data in
-                        if let netAmount = data?.netAmount, let grossAmount = data?.grossAmount {
-                            let discountValue = MonetaryAmount(
-                                amount: netAmount.value - grossAmount.value,
-                                currency: netAmount.currency
-                            )
-                            hText(discountValue.negative.formattedAmount)
-                                .foregroundColor(hTextColorNew.secondary)
-                        }
+            VStack(spacing: 16) {
+                PresentableStoreLens(
+                    ForeverStore.self,
+                    getter: { state in
+                        state.foreverData
                     }
-                    PresentableStoreLens(
-                        ForeverStore.self,
-                        getter: { state in
-                            state.foreverData
-                                ?? ForeverData.init(
-                                    grossAmount: .init(amount: 0, currency: ""),
-                                    netAmount: .init(amount: 0, currency: ""),
-                                    potentialDiscountAmount: .init(amount: 0, currency: ""),
-                                    discountCode: "",
-                                    invitations: []
-                                )
-                        }
-                    ) { data in
-                        if let grossAmount = data?.grossAmount,
-                            let netAmount = data?.netAmount,
-                            let potentialDiscountAmount = data?.potentialDiscountAmount
-                        {
-                            PieChartView(
-                                state: .init(
-                                    grossAmount: grossAmount,
-                                    netAmount: netAmount,
-                                    potentialDiscountAmount: potentialDiscountAmount
-                                ),
-                                newPrice: netAmount.formattedAmount
+                ) { data in
+                    if let netAmount = data?.netAmount, let grossAmount = data?.grossAmount {
+                        let discountValue = MonetaryAmount(
+                            amount: grossAmount.value - netAmount.value,
+                            currency: netAmount.currency
+                        )
+                        hText(discountValue.negative.formattedAmount)
+                            .foregroundColor(hTextColorNew.secondary)
+                    }
+                }
+                PresentableStoreLens(
+                    ForeverStore.self,
+                    getter: { state in
+                        state.foreverData
+                            ?? ForeverData.init(
+                                grossAmount: .init(amount: 0, currency: ""),
+                                netAmount: .init(amount: 0, currency: ""),
+                                potentialDiscountAmount: .init(amount: 0, currency: ""),
+                                discountCode: "",
+                                invitations: []
                             )
-                            .frame(width: 215, height: 215, alignment: .center)
+                    }
+                ) { data in
+                    if let grossAmount = data?.grossAmount,
+                        let netAmount = data?.netAmount,
+                        let potentialDiscountAmount = data?.potentialDiscountAmount
+                    {
+                        PieChartView(
+                            state: .init(
+                                grossAmount: grossAmount,
+                                netAmount: netAmount,
+                                potentialDiscountAmount: potentialDiscountAmount
+                            ),
+                            newPrice: netAmount.formattedAmount
+                        )
+                        .frame(width: 215, height: 215, alignment: .center)
 
-                            if grossAmount.amount != netAmount.amount {
-                                // Discount present
-                                PriceSectionView(grossAmount: grossAmount, netAmount: netAmount)
-                            } else {
-                                // No discount present
-                                hText(
-                                    L10n.ReferralsEmpty.body(
-                                        potentialDiscountAmount.formattedAmount,
-                                        MonetaryAmount(amount: 0, currency: potentialDiscountAmount.currency)
-                                            .formattedAmount
-                                    )
+                        if grossAmount.amount != netAmount.amount {
+                            // Discount present
+                            PriceSectionView(netAmount: netAmount)
+                                .padding(.bottom, 65)
+                                .padding(.top, 8)
+                        } else {
+                            //                            // No discount present
+                            hText(
+                                L10n.ReferralsEmpty.body(
+                                    potentialDiscountAmount.formattedAmount,
+                                    MonetaryAmount(amount: 0, currency: potentialDiscountAmount.currency)
+                                        .formattedAmount
                                 )
-                                .foregroundColor(hTextColorNew.secondary)
-                                .multilineTextAlignment(.center)
-                                .padding(.top, 42)
-                            }
+                            )
+                            .foregroundColor(hTextColorNew.secondary)
+                            .multilineTextAlignment(.center)
+                            .padding(.top, 49)
                         }
                     }
                 }
