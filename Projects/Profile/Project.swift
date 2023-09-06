7c7614af7ff5fa230c8160f25deab4056e6477f0
julia-andersson <julia.andersson@hedvig.com>
2023-09-06 17:05:13 +0200

    add home dependecy

--- Projects/Profile/Project.swift
+++ Projects/Profile/Project.swift
@@ -4,7 +4,7 @@ import ProjectDescriptionHelpers
 let project = Project.framework(
     name: "Profile",
     targets: Set([.framework, .frameworkResources, .tests, .example]),
-    projects: ["hCore", "hCoreUI"],
+    projects: ["hCore", "hCoreUI", "Home"],
     sdks: [],
     includesGraphQL: false
 )
