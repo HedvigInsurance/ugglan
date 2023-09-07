2b9a139ec1caa0a8dd9fe52e87cca884ff5f5b6a
julia-andersson <julia.andersson@hedvig.com>
2023-09-07 08:20:23 +0200

    add missing dependency claims

--- Projects/Profile/Project.swift
+++ Projects/Profile/Project.swift
@@ -4,7 +4,7 @@ import ProjectDescriptionHelpers
 let project = Project.framework(
     name: "Profile",
     targets: Set([.framework, .frameworkResources, .tests, .example]),
-    projects: ["hCore", "hCoreUI", "Home"],
+    projects: ["hCore", "hCoreUI", "Home", "Claims"],
     sdks: [],
     includesGraphQL: false
 )
