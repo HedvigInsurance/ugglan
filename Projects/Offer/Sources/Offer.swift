54bb326202c9800672307871ce3d9d11c5a9ba09
julia-andersson <julia.andersson@hedvig.com>
2023-10-02 16:37:03 +0200

    remove unused code

--- Projects/Offer/Sources/Offer.swift
+++ Projects/Offer/Sources/Offer.swift
@@ -15,39 +15,6 @@ public enum OfferOption {
     case shouldPreserveState
 }
 
-public struct Offer {
-    let menu: hCore.Menu?
-    let options: Set<OfferOption>
-
-    public init(
-        menu: hCore.Menu?,
-        options: Set<OfferOption> = []
-    ) {
-        self.menu = menu
-        self.options = options
-    }
-}
-
-extension Offer {
-    public func setIds(_ ids: [String]) -> Self {
-        let store: OfferStore = globalPresentableStoreContainer.get()
-        store.send(.setIds(ids: ids, selectedIds: ids))
-        return self
-    }
-
-    public func setIds(_ ids: [String], selectedIds: [String]) -> Self {
-        let store: OfferStore = globalPresentableStoreContainer.get()
-        store.send(.setIds(ids: ids, selectedIds: selectedIds))
-        return self
-    }
-
-    public func setQuoteCart(_ id: String, selectedInsuranceTypes: [String]) -> Self {
-        let store: OfferStore = globalPresentableStoreContainer.get()
-        store.send(.setQuoteCartId(id: id, insuranceTypes: selectedInsuranceTypes))
-        return self
-    }
-}
-
 public enum OfferResult {
     case signed(ids: [String], startDates: [String: Date?])
     case signedQuoteCart(accessToken: String, startDates: [String: Date?])
