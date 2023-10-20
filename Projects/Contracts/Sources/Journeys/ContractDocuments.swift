ec5655b85bf68a3a44d75298302c985013dc40ab
Sladan Nimcevic <sladan.nimcevic@hedvig.com>
2023-10-20 12:44:28 +0200

    Updated contract details layout

--- Projects/Contracts/Sources/Journeys/ContractDocuments.swift
+++ Projects/Contracts/Sources/Journeys/ContractDocuments.swift
@@ -23,36 +23,66 @@ struct ContractDocumentsView: View {
             }
         ) { contract in
             if let contract = contract {
-                ForEach(getDocumentsToDisplay(contract: contract), id: \.displayName) { document in
-                    hSection {
-                        if let url = URL(string: document.url) {
-                            hRow {
-                                VStack(alignment: .leading, spacing: 0) {
-                                    HStack(spacing: 1) {
-                                        hText(document.displayName)
-                                        if #available(iOS 16.0, *) {
-                                            hText(L10n.documentPdfLabel, style: .footnote)
-                                                .baselineOffset(6.0)
+                VStack(spacing: 4) {
+                    ForEach(getDocumentsToDisplay(contract: contract), id: \.displayName) { document in
+                        hSection {
+                            if let url = URL(string: document.url) {
+                                hRow {
+                                    VStack(alignment: .leading, spacing: 0) {
+                                        if #available(iOS 15.0, *) {
+                                            Text(attributedPDFString(for: document.displayName))
+                                        } else {
+                                            HStack(spacing: 1) {
+                                                hText(document.displayName)
+                                                if #available(iOS 16.0, *) {
+                                                    hText(L10n.documentPdfLabel, style: .footnote)
+                                                        .baselineOffset(6.0)
+                                                }
+                                            }
                                         }
                                     }
                                 }
-                            }
-                            .withCustomAccessory {
-                                Spacer()
-                                Image(uiImage: hCoreUIAssets.neArrowSmall.image)
-                            }
-                            .onTap {
-                                contractStore.send(
-                                    .contractDetailNavigationAction(
-                                        action: .document(url: url, title: document.displayName)
+                                .withCustomAccessory {
+                                    Spacer()
+                                    Image(uiImage: hCoreUIAssets.neArrowSmall.image)
+                                }
+                                .onTap {
+                                    contractStore.send(
+                                        .contractDetailNavigationAction(
+                                            action: .document(url: url, title: document.displayName)
+                                        )
                                     )
-                                )
+                                }
                             }
                         }
                     }
                 }
             }
         }
+    }
+
+    @available(iOS 15, *)
+    private func attributedPDFString(for title: String) -> AttributedString {
+        let schema = ColorScheme(UITraitCollection.current.userInterfaceStyle) ?? .light
+        let attributes = AttributeContainer(
+            [
+                NSAttributedString.Key.font: Fonts.fontFor(style: .standard),
+                NSAttributedString.Key.foregroundColor: hTextColor.primary.colorFor(schema, .base).color.uiColor(),
+            ]
+        )
+
+        var result = AttributedString(title, attributes: attributes)
+
+        let pdfAddonAttribures = AttributeContainer(
+            [
+                NSAttributedString.Key.font: Fonts.fontFor(style: .standardExtraSmall),
+                NSAttributedString.Key.foregroundColor: hTextColor.primary.colorFor(schema, .base).color.uiColor(),
+                NSAttributedString.Key.baselineOffset: 6,
+            ]
+        )
+        var pdfAddon = AttributedString(" \(L10n.documentPdfLabel)", attributes: pdfAddonAttribures)
+        result.append(pdfAddon)
+        return result
     }
 
     func getDocumentsToDisplay(contract: Contract) -> [InsuranceTerm] {
