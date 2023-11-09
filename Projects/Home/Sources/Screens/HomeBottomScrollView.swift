f3c1d7c64701e942a071e8dc8e2e4d87abbef220
Sladan Nimcevic <sladan.nimcevic@hedvig.com>
2023-11-09 10:42:50 +0100

    Fixed home screen home view jumping

--- Projects/Home/Sources/Screens/HomeBottomScrollView.swift
+++ Projects/Home/Sources/Screens/HomeBottomScrollView.swift
@@ -10,15 +10,17 @@ import hGraphQL
 
 struct HomeBottomScrollView: View {
     @ObservedObject private var vm: HomeButtonScrollViewModel
-
+    @State var height: CGFloat = 0
     init(memberId: String) {
         self.vm = HomeButtonScrollViewModel(memberId: memberId)
     }
 
     var body: some View {
-        return InfoCardScrollView(
+        //        if !vm.items.isEmpty {
+        InfoCardScrollView(
             spacing: 16,
             items: vm.items.sorted(by: { $0.id < $1.id }),
+            previousHeight: height,
             content: { content in
                 switch content.type {
                 case .payment:
@@ -35,6 +37,14 @@ struct HomeBottomScrollView: View {
                 }
             }
         )
+        .background(
+            GeometryReader(content: { geo in
+                Color.clear.onReceive(Just(geo.size.height)) { height in
+                    self.height = height
+                }
+            })
+        )
+        //        }
     }
 }
 
