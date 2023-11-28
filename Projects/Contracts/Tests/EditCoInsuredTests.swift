5e2f99dcc20549bebb65fbe9d87b25152ff4b7b3
julia-andersson <julia.andersson@hedvig.com>
2023-11-28 11:26:16 +0100

    add test cases

--- Projects/Contracts/Tests/EditCoInsuredTests.swift
+++ Projects/Contracts/Tests/EditCoInsuredTests.swift
@@ -129,6 +129,70 @@ final class ContractsEditInsuredCompleteListTests: XCTestCase {
         let list = viewModel.completeList()
         assert(list.count == 2)
     }
+    
+    func testInitialSSNBirthday() {
+        let viewModel = InsuredPeopleNewScreenModel()
+        
+        viewModel.currentAgreementCoInsured = [
+            CoInsuredModel.mockMissingData(),
+            CoInsuredModel.mockMissingData(),
+        ]
+
+        viewModel.upcomingAgreementCoInsured = [
+            CoInsuredModel.testMemberWithSSN1,
+            CoInsuredModel.testMemberWithBirthdate1,
+        ]
+        
+        viewModel.coInsuredAdded = []
+            
+        viewModel.coInsuredDeleted = []
+        let list = viewModel.completeList()
+        assert(list.count == 2)
+    }
+    
+    func testRemovedWithoutDataAddingSSNBirthdate() {
+        let viewModel = InsuredPeopleNewScreenModel()
+        
+        viewModel.currentAgreementCoInsured = [
+            CoInsuredModel.mockMissingData(),
+            CoInsuredModel.mockMissingData(),
+        ]
+
+        viewModel.upcomingAgreementCoInsured = [
+            CoInsuredModel.mockMissingData(),
+        ]
+        
+        viewModel.coInsuredAdded = [
+            CoInsuredModel.testMemberWithSSN1,
+            CoInsuredModel.testMemberWithBirthdate1
+        ]
+            
+        viewModel.coInsuredDeleted = []
+        let list = viewModel.completeList()
+        assert(list.count == 2)
+    }
+    
+    func testAddCoInsuredToEmptyCurrentUpcomingValues() {
+        let viewModel = InsuredPeopleNewScreenModel()
+        
+        viewModel.currentAgreementCoInsured = [
+            CoInsuredModel.mockMissingData(),
+            CoInsuredModel.mockMissingData(),
+        ]
+
+        viewModel.upcomingAgreementCoInsured = [
+            CoInsuredModel.testMemberWithSSN1,
+        ]
+        
+        viewModel.coInsuredAdded = [
+            CoInsuredModel.testMemberWithBirthdate1
+        ]
+            
+        viewModel.coInsuredDeleted = []
+        let list = viewModel.completeList()
+        assert(list.count == 2)
+    }
+    
 }
 
 extension CoInsuredModel {
@@ -137,6 +201,10 @@ extension CoInsuredModel {
     static let testMemberWithSSN3 = CoInsuredModel(firstName: "A", lastName: "B", SSN: "199209016830", birthDate: "1992-09-01")
     static let testMemberWithSSN4 = CoInsuredModel(firstName: "B", lastName: "A", SSN: "199309016830", birthDate: "1993-09-01")
     static let testMemberWithSSN5 = CoInsuredModel(firstName: "C", lastName: "D", SSN: "199409016830", birthDate: "1994-09-01")
+    
+    static let testMemberWithBirthdate1 = CoInsuredModel(firstName: "Test", lastName: "Testsson", SSN: nil, birthDate: "1990-09-01")
+    static let testMemberWithBirthdate2 = CoInsuredModel(firstName: "Test", lastName: "Testsson", SSN: nil, birthDate: "1991-09-01")
+    static let testMemberWithBirthdate3 = CoInsuredModel(firstName: "Test", lastName: "Testsson", SSN: nil, birthDate: "1992-09-01")
     
     static func mock(withSSN ssn: String) -> CoInsuredModel {
         CoInsuredModel(
