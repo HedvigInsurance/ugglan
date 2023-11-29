9c260b112f7bf862969142945a7affef823b4216
Sladan Nimcevic <sladan.nimcevic@hedvig.com>
2023-11-29 13:29:16 +0100

    Removed tests from contracts

--- Projects/Contracts/Project.swift
+++ Projects/Contracts/Project.swift
@@ -3,7 +3,7 @@ import ProjectDescriptionHelpers
 
 let project = Project.framework(
     name: "Contracts",
-    targets: Set([.framework, .frameworkResources, .tests, .example]),
+    targets: Set([.framework, .frameworkResources, .example]),
     projects: ["hCore", "hCoreUI", "TerminateContracts", "EditCoInsured"],
     sdks: [],
     includesGraphQL: false
