163d90a508d9ee720eed1e00e494c41e27bd2b4c
Sladan Nimcevic <sladan.nimcevic@hedvig.com>
2023-11-30 14:12:04 +0100

    Auto stash before merge of "feature/edit-coinsured/add-new-schema" and "origin/feature/edit-coinsured/add-new-schema"

--- Projects/Claims/Sources/ClaimStore.swift
+++ Projects/Claims/Sources/ClaimStore.swift
@@ -8,7 +8,7 @@ import hCoreUI
 import hGraphQL
 
 public final class ClaimsStore: StateStore<ClaimsState, ClaimsAction> {
-    @Inject var octopus: hOctopus
+    @Inject var fetchClaimService: FetchClaimService
 
     public override func effects(
         _ getState: @escaping () -> ClaimsState,
@@ -18,26 +18,43 @@ public final class ClaimsStore: StateStore<ClaimsState, ClaimsAction> {
         case .openFreeTextChat:
             return nil
         case .fetchClaims:
-            return FiniteSignal { callback in
+            return FiniteSignal { [weak self] callback in guard let self = self else { return DisposeBag() }
                 let disposeBag = DisposeBag()
-                disposeBag += self.octopus.client
-                    .fetch(
-                        query: OctopusGraphQL.ClaimsQuery(),
-                        cachePolicy: .fetchIgnoringCacheData
-                    )
-                    .onValue { data in
-                        let claimData = data.currentMember.claims.map { ClaimModel(claim: $0) }
+                Task {
+                    do {
+                        let claimData = try await self.fetchClaimService.get()
                         callback(.value(ClaimsAction.setClaims(claims: claimData)))
-                    }
-                    .onError { error in
+                    } catch {
                         callback(
                             .value(
                                 .setLoadingState(action: action, state: .error(error: L10n.General.errorBody))
                             )
                         )
                     }
+                }
                 return disposeBag
             }
+        //            return FiniteSignal { callback in
+
+        //                let disposeBag = DisposeBag()
+        //                disposeBag += self.octopus.client
+        //                    .fetch(
+        //                        query: OctopusGraphQL.ClaimsQuery(),
+        //                        cachePolicy: .fetchIgnoringCacheData
+        //                    )
+        //                    .onValue { data in
+        //                        let claimData = data.currentMember.claims.map { ClaimModel(claim: $0) }
+        //                        callback(.value(ClaimsAction.setClaims(claims: claimData)))
+        //                    }
+        //                    .onError { error in
+        //                        callback(
+        //                            .value(
+        //                                .setLoadingState(action: action, state: .error(error: L10n.General.errorBody))
+        //                            )
+        //                        )
+        //                    }
+        //                return disposeBag
+        //            }
         default:
             return nil
         }
