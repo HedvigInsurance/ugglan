bfa15554e60ff5af45a122b3d0de126b0382b40b
julia-andersson <julia.andersson@hedvig.com>
2023-12-18 10:58:39 +0100

    .

--- Projects/Home/Sources/Screens/HelpCenter/HelpCenterTopicView.swift
+++ Projects/Home/Sources/Screens/HelpCenter/HelpCenterTopicView.swift
@@ -50,7 +50,8 @@ extension HelpCenterTopicView {
     let questions: [Question] = [
         .init(
             question: "When do you charge for my insurance?",
-            answer: "The total amount of your insurance cost is deducted retrospectively on the 27th of each month, for the current month.\n\nYour insurance starts on 1 June. The first dawn takes place on June 27, for the entire month of June. This means that you pay 27 days in arrears and 3 days in advance.\n\nThe insurance is valid even if the first payment has not been received.\n\nGo to Payments to view your full history.",
+            answer:
+                "The total amount of your insurance cost is deducted retrospectively on the 27th of each month, for the current month.\n\nYour insurance starts on 1 June. The first dawn takes place on June 27, for the entire month of June. This means that you pay 27 days in arrears and 3 days in advance.\n\nThe insurance is valid even if the first payment has not been received.\n\nGo to Payments to view your full history.",
             relatedQuestions: []
         ),
         .init(
@@ -74,7 +75,7 @@ extension HelpCenterTopicView {
             relatedQuestions: []
         ),
     ]
-    
+
     return HelpCenterTopicView(
         commonTopic: .init(
             title: "Payments",
