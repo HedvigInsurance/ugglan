3f2a9ab8b30f36b597fc0843bb0d5e013f8fba0a
Sladan Nimcevic <sladan.nimcevic@hedvig.com>
2024-02-14 11:42:15 +0100

    try npm

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -8,21 +8,7 @@ echo "machine maven.pkg.github.com" > ~/.netrc
 echo "login ${MAVEN_LOGIN}" >> ~/.netrc
 echo "password ${MAVEN_PASSWORD}" >> ~/.netrc
 
-if [ -z "$CI" ]; then
-    TMPDIR=/tmp/datadog-2.30.0
-else
-    mkdir build
-    TMPDIR=build/datadog-2.30.0
-fi
-
-mkdir $TMPDIR
-
-curl -o $TMPDIR/datadog.zip -L https://github.com/DataDog/datadog-ci/releases/download/v2.30.0/datadog-ci_darwin-x64
-
-unzip $TMPDIR/datadog.zip -d $TMPDIR
-
-$TMPDIR/bin/datadog
-
+npm install --save-dev @datadog/datadog-ci
 
 
 export PATH=$PATH":$CI_PRIMARY_REPOSITORY_PATH/.tuist-bin"
