3c39da23b64ee0fb87b6b50d0f153354e6b056f8
Sladan Nimcevic <sladan.nimcevic@hedvig.com>
2024-02-14 13:08:04 +0100

    Updated script

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -14,7 +14,7 @@ TMPDIR=build
 
 curl -L --fail "https://github.com/DataDog/datadog-ci/releases/latest/download/datadog-ci_darwin-x64" --output "$TMPDIR/datadog-ci" && chmod +x $TMPDIR/datadog-ci
 
-datadog-ci dsyms upload $TMPDIR
+$TMPDIR/datadog-ci dsyms upload $TMPDIR
 
 export PATH=$PATH":$CI_PRIMARY_REPOSITORY_PATH/.tuist-bin"
 
