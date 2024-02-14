8c75656d977f301f1334eeadd7c3e3a0e539121f
Sladan Nimcevic <sladan.nimcevic@hedvig.com>
2024-02-14 11:09:25 +0100

    Updated script

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -7,7 +7,7 @@ touch ~/.netrc
 echo "machine maven.pkg.github.com" > ~/.netrc
 echo "login ${MAVEN_LOGIN}" >> ~/.netrc
 echo "password ${MAVEN_PASSWORD}" >> ~/.netrc
-
+echo "value {$CI_WORKFLOW}"
 export PATH=$PATH":$CI_PRIMARY_REPOSITORY_PATH/.tuist-bin"
 
 cd $CI_PRIMARY_REPOSITORY_PATH;
