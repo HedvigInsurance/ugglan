896ba8214f7ae5827102cba6039d004a58d5ebe4
Sladan Nimcevic <sladan.nimcevic@hedvig.com>
2024-02-14 11:35:22 +0100

    Updated scripts

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -8,9 +8,23 @@ echo "machine maven.pkg.github.com" > ~/.netrc
 echo "login ${MAVEN_LOGIN}" >> ~/.netrc
 echo "password ${MAVEN_PASSWORD}" >> ~/.netrc
 
-if [ "$CI_WORKFLOW" = "Ugglan" ]; then
-    echo "Right place."
+if [ -z "$CI" ]; then
+    TMPDIR=/tmp/datadog-2.30.0
+else
+    mkdir build
+    TMPDIR=build/datadog-2.30.0
 fi
+
+mkdir $TMPDIR
+
+curl -o $TMPDIR/datadog.zip -L https://github.com/DataDog/datadog-ci/releases/download/v2.30.0/datadog-ci_darwin-x64
+
+unzip $TMPDIR/datadog.zip -d $TMPDIR
+
+$TMPDIR/bin/datadog
+
+
+
 export PATH=$PATH":$CI_PRIMARY_REPOSITORY_PATH/.tuist-bin"
 
 cd $CI_PRIMARY_REPOSITORY_PATH;
--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -1,1 +1,5 @@
 #!/usr/bin/env bash
+
+if [ "$CI_WORKFLOW" = "Ugglan" ]; then
+    echo "Right place."
+fi
