28e7603dde3916d657ae59b57bf13b19a3ae6f99
julia-andersson <julia.andersson@hedvig.com>
2024-02-27 10:36:26 +0100

    +

--- Projects/MoveFlow/Sources/Service/Protocols/MoveFlowService.swift
+++ Projects/MoveFlow/Sources/Service/Protocols/MoveFlowService.swift
@@ -1,8 +1,12 @@
-import hGraphQL
 import hCore
+import hGraphQL
 
 public protocol MoveFlowService {
     func sendMoveIntent() async throws -> MovingFlowModel?
-    func requestMoveIntent(intentId: String, addressInputModel: AddressInputModel, houseInformationInputModel: HouseInformationInputModel) async throws -> MovingFlowModel?
+    func requestMoveIntent(
+        intentId: String,
+        addressInputModel: AddressInputModel,
+        houseInformationInputModel: HouseInformationInputModel
+    ) async throws -> MovingFlowModel?
     func confirmMoveIntent(intentId: String) async throws
 }
