9b1222e489479aab5b612a998115a15894a22f33
Sladan Nimcevic <sladan.nimcevic@hedvig.com>
2024-03-06 16:44:12 +0100

    Fixed "Save to Files"

--- Projects/hCoreUI/Sources/Document.swift
+++ Projects/hCoreUI/Sources/Document.swift
@@ -33,48 +33,51 @@ extension Document: Presentable {
 
         let activityButton = UIBarButtonItem(system: .action)
 
-        func getPathForFile() -> URL {
-            let temporaryFolder = FileManager.default.temporaryDirectory
-            let fileName = "\(title) \(Date().localDateString).pdf"
-            let url = temporaryFolder.appendingPathComponent(fileName)
-            return url
-        }
-        func transformDataToPresentation(data: Data) -> Presentation<ActivityView> {
-            var thingToShare: Any = data
-            let temporaryFileURL = getPathForFile()
-            do {
-                try? FileManager.default.removeItem(at: temporaryFileURL)
-                try data.write(to: temporaryFileURL)
-                thingToShare = temporaryFileURL
-            } catch let error {
-                print("\(#function): *** Error while writing to temporary file. \(error.localizedDescription)")
-            }
-
-            let activityView = ActivityView(
-                activityItems: [thingToShare],
-                applicationActivities: nil,
-                sourceView: activityButton.view,
-                sourceRect: activityButton.bounds
-            )
-
-            let activityViewPresentation = Presentation(
-                activityView,
-                style: .activityView,
-                options: .defaults
-            )
-            return activityViewPresentation
-        }
-
         bag += viewController.navigationItem.addItem(activityButton, position: .right)
             .withLatestFrom(pdfViewer.data)
-            .onValueDisposePrevious { _, value -> Disposable? in
-                guard let value = value else { return NilDisposer() }
-                let activityViewPresentation = transformDataToPresentation(data: value)
-                    .onDismiss {
-                        try? FileManager.default.removeItem(at: getPathForFile())
-                    }
-                return viewController.present(activityViewPresentation).disposable
+            .onValue {
+                [weak activityButton, weak viewController] _, value
+                in guard let activityButton = activityButton, let viewController = viewController else { return }
+                guard let value = value else { return }
+                let activityViewPresentation = transformDataToPresentationn(source: activityButton, data: value)
+                viewController.present(activityViewPresentation)
             }
+        bag += viewController.deallocSignal.onValue({ _ in
+            try? FileManager.default.removeItem(at: getPathForFile())
+        })
         return (viewController, bag)
+    }
+
+    private func transformDataToPresentationn(source: UIBarButtonItem, data: Data) -> ActivityView {
+        var thingToShare: Any = data
+        let temporaryFileURL = getPathForFile()
+        do {
+            try? FileManager.default.removeItem(at: temporaryFileURL)
+            try data.write(to: temporaryFileURL)
+            thingToShare = temporaryFileURL
+        } catch let error {
+            print("\(#function): *** Error while writing to temporary file. \(error.localizedDescription)")
+        }
+
+        let activityView = ActivityView(
+            activityItems: [thingToShare],
+            applicationActivities: nil,
+            sourceView: source.view,
+            sourceRect: source.bounds
+        )
+
+        let activityViewPresentation = Presentation(
+            activityView,
+            style: .activityView,
+            options: .defaults
+        )
+        return activityView
+    }
+
+    private func getPathForFile() -> URL {
+        let temporaryFolder = FileManager.default.temporaryDirectory
+        let fileName = "\(title).pdf"
+        let url = temporaryFolder.appendingPathComponent(fileName)
+        return url
     }
 }
