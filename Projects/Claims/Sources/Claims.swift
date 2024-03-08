a73318f7bc6e2ef38c44fd4d9ba6e642a3cc7483
Sladan Nimcevic <sladan.nimcevic@hedvig.com>
2024-03-08 09:34:24 +0100

    Merged claims and claim loading section

--- Projects/Claims/Sources/Claims.swift
+++ Projects/Claims/Sources/Claims.swift
@@ -7,6 +7,58 @@ import hCoreUI
 import hGraphQL
 
 public struct Claims {
+    @StateObject var vm = ClaimsViewModel()
+
+    public init() {}
+}
+
+extension Claims: View {
+    func fetch() {
+        vm.store.send(.fetchClaims)
+    }
+
+    @ViewBuilder
+    func claimsSection(_ claims: [ClaimModel]) -> some View {
+        VStack {
+            if claims.isEmpty {
+                Spacer().frame(height: 40)
+            } else if claims.count == 1, let claim = claims.first {
+                ClaimStatus(claim: claim, enableTap: true)
+                    .padding([.bottom, .top])
+            } else {
+                ClaimSection(claims: claims)
+                    .padding([.bottom, .top])
+            }
+        }
+    }
+
+    @ViewBuilder
+    public var body: some View {
+        PresentableStoreLens(
+            ClaimsStore.self,
+            getter: { state in
+                state.claims ?? []
+            },
+            setter: { _ in
+                .fetchClaims
+            }
+        ) { claims, _ in
+            claimsSection(claims)
+        }
+        .onReceive(vm.pollTimer) { _ in
+            if ApplicationContext.shared.isLoggedIn {
+                fetch()
+            } else {
+                vm.pollTimer.upstream.connect().cancel()
+            }
+        }
+        .onAppear {
+            fetch()
+        }
+    }
+}
+
+class ClaimsViewModel: ObservableObject {
     @PresentableStore var store: ClaimsStore
     let pollTimer: Publishers.Autoconnect<Timer.TimerPublisher>
 
@@ -21,25 +73,5 @@ public struct Claims {
             }
         }()
         pollTimer = Timer.publish(every: TimeInterval(refreshOn), on: .main, in: .common).autoconnect()
-    }
-}
-
-extension Claims: View {
-    func fetch() {
-        store.send(.fetchClaims)
-    }
-
-    public var body: some View {
-        ClaimSectionLoading()
-            .onReceive(pollTimer) { _ in
-                if ApplicationContext.shared.isLoggedIn {
-                    fetch()
-                } else {
-                    pollTimer.upstream.connect().cancel()
-                }
-            }
-            .onAppear {
-                fetch()
-            }
     }
 }
