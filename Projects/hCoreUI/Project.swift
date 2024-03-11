4bcc837e0c9dc14ff492d6552d41b88909c55924
Sladan Nimcevic <sladan.nimcevic@hedvig.com>
2024-03-11 12:03:02 +0100

    Updated options

--- Projects/hCoreUI/Project.swift
+++ Projects/hCoreUI/Project.swift
@@ -3,7 +3,7 @@ import ProjectDescriptionHelpers
 
 let project = Project.framework(
     name: "hCoreUI",
-    targets: Set([.framework, .frameworkResources, .tests, .example, .testing]),
+    targets: Set([.framework, .frameworkResources, .tests, .example, .testing, .generateAssets]),
     projects: ["hCore"],
     sdks: ["UIKit"],
     includesGraphQL: true
