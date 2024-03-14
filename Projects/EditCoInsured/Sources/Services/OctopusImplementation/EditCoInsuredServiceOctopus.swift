fa118ffa28451c731180b721be8718d485d0d414
Sladan Nimcevic <sladan.nimcevic@hedvig.com>
2024-03-14 16:32:52 +0100

    Updated errors handling for co insured

--- Projects/EditCoInsured/Sources/Services/OctopusImplementation/EditCoInsuredServiceOctopus.swift
+++ Projects/EditCoInsured/Sources/Services/OctopusImplementation/EditCoInsuredServiceOctopus.swift
@@ -10,24 +10,37 @@ public class EditCoInsuredServiceOctopus: EditCoInsuredService {
         let mutation = OctopusGraphQL.MidtermChangeIntentCommitMutation(intentId: commitId)
         let data = try await octopus.client.perform(mutation: mutation)
         if let error = data.midtermChangeIntentCommit.userError {
-            throw EditCoInsuredError.error(message: error.message ?? L10n.General.errorBody)
+            throw EditCoInsuredError.graphQLError(message: error.message ?? L10n.General.errorBody)
         }
     }
 
     public func getPersonalInformation(SSN: String) async throws -> PersonalData? {
         let SSNInput = OctopusGraphQL.PersonalInformationInput(personalNumber: SSN)
         let query = OctopusGraphQL.PersonalInformationQuery(input: SSNInput)
-        let data = try await octopus.client.fetch(
-            query: query,
-            cachePolicy: .fetchIgnoringCacheCompletely
-        )
+        do {
+            let data = try await octopus.client.fetch(
+                query: query,
+                cachePolicy: .fetchIgnoringCacheCompletely
+            )
+            guard let data = data.personalInformation else {
+                throw EditCoInsuredError.missingSSN
+            }
 
-        guard let data = data.personalInformation else {
-            throw EditCoInsuredError.error(message: L10n.General.errorBody)
+            let personalData = PersonalData(firstName: data.firstName, lastName: data.lastName)
+            return personalData
+        } catch let exception {
+            if let exception = exception as? GraphQLError {
+                switch exception {
+                case .graphQLError:
+                    throw EditCoInsuredError.graphQLError(message: exception.localizedDescription)
+                case .otherError:
+                    throw EditCoInsuredError.otherError
+                }
+            } else {
+                throw EditCoInsuredError.otherError
+
+            }
         }
-
-        let personalData = PersonalData(firstName: data.firstName, lastName: data.lastName)
-        return personalData
     }
 
     public func sendIntent(contractId: String, coInsured: [CoInsuredModel]) async throws -> Intent {
@@ -49,10 +62,10 @@ public class EditCoInsuredServiceOctopus: EditCoInsuredService {
         let data = try await octopus.client.perform(mutation: mutation).midtermChangeIntentCreate
 
         if let userError = data.userError {
-            throw EditCoInsuredError.error(message: userError.message ?? L10n.General.errorBody)
+            throw EditCoInsuredError.graphQLError(message: userError.message ?? L10n.General.errorBody)
         }
         guard let intent = data.intent else {
-            throw EditCoInsuredError.error(message: L10n.General.errorBody)
+            throw EditCoInsuredError.graphQLError(message: L10n.General.errorBody)
         }
         return Intent(
             activationDate: intent.activationDate,
