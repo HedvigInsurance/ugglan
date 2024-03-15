882472ff0ff2a89afd49bca1781b66c1d777a56d
Sladan Nimcevic <sladan.nimcevic@hedvig.com>
2024-03-15 13:23:37 +0100

    Moved Forever to projects in tuist

--- Projects/Payment/Project.swift
+++ Projects/Payment/Project.swift
@@ -4,8 +4,8 @@ import ProjectDescriptionHelpers
 let project = Project.framework(
     name: "Payment",
     targets: Set([.framework]),
-    projects: ["hCore", "hCoreUI", "Contracts"],
-    dependencies: ["CoreDependencies", "ResourceBundledDependencies", "Forever"],
+    projects: ["hCore", "hCoreUI", "Contracts", "Forever"],
+    dependencies: ["CoreDependencies", "ResourceBundledDependencies"],
     sdks: [],
     includesGraphQL: true
 )
