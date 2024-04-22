ad5a8e080ae175b983ce09b6b3711bfa6e153440
julia-andersson <julia.andersson@hedvig.com>
2024-04-22 09:08:57 +0200

    add MoveFlow to Home

--- Projects/Home/Project.swift
+++ Projects/Home/Project.swift
@@ -4,7 +4,7 @@ import ProjectDescriptionHelpers
 let project = Project.framework(
     name: "Home",
     targets: Set([.framework]),
-    projects: ["hCore", "hCoreUI", "TravelCertificate", "TerminateContracts", "Payment", "Chat"],
+    projects: ["hCore", "hCoreUI", "TravelCertificate", "TerminateContracts", "Payment", "Chat", "MoveFlow"],
     sdks: [],
     includesGraphQL: true
 )
