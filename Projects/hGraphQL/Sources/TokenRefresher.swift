837ac0316c06c5e5bbb370977a077c2e4953d8a8
Sladan Nimcevic <sladan.nimcevic@hedvig.com>
2024-04-25 12:00:00 +0200

    Fixed issue with refreshing and observing changes

--- Projects/hGraphQL/Sources/TokenRefresher.swift
+++ Projects/hGraphQL/Sources/TokenRefresher.swift
@@ -5,11 +5,11 @@ import Foundation
 public class TokenRefresher {
     public static let shared = TokenRefresher()
     var isRefreshing: ReadWriteSignal<Bool> = ReadWriteSignal(false)
+    let bag = DisposeBag()
     var needRefresh: Bool {
         guard let token = try? ApolloClient.retreiveToken() else {
             return false
         }
-
         return Date().addingTimeInterval(60) > token.accessTokenExpirationDate
     }
 
@@ -29,8 +29,8 @@ public class TokenRefresher {
 
         if self.isRefreshing.value {
             log.debug("Already refreshing waiting until that is complete")
-            try await withCheckedThrowingContinuation { (inCont: CheckedContinuation<Void, Error>) -> Void in
-                let bag = DisposeBag()
+            try await withCheckedThrowingContinuation {
+                [weak self] (inCont: CheckedContinuation<Void, Error>) -> Void in guard let self = self else { return }
                 bag += self.isRefreshing
                     .filter(predicate: { isRefreshing in
                         !isRefreshing
@@ -52,9 +52,15 @@ public class TokenRefresher {
             do {
                 try await onRefresh?(token.refreshToken)
                 self.isRefreshing.value = false
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) { [weak self] in
+                    self?.bag.dispose()
+                }
             } catch let error {
                 log.error("Refreshing failed \(error.localizedDescription), forcing logout")
                 forceLogoutHook()
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) { [weak self] in
+                    self?.bag.dispose()
+                }
                 throw error
             }
         }
