f8a340d1713f686575c646085af5e8861780eb98
julia-andersson <julia.andersson@hedvig.com>
2024-05-08 16:16:47 +0200

    remove unused

--- Projects/Authentication/Sources/OTP/OTPAuthJourney.swift
+++ Projects/Authentication/Sources/OTP/OTPAuthJourney.swift
@@ -6,30 +6,3 @@ import hCoreUI
 public enum OTPAuthJourneyNext {
     case success
 }
-
-public struct OTPAuthJourney {
-    public static func login<Next: JourneyPresentation>(
-        @JourneyBuilder _ next: @escaping (_ next: OTPAuthJourneyNext) -> Next
-    ) -> some JourneyPresentation {
-        let store: AuthenticationStore = globalPresentableStoreContainer.get()
-        return HostingJourney(
-            AuthenticationStore.self,
-            rootView: OTPEntryView(otpVM: store.otpState)
-        ) { action in
-            if case .navigationAction(action: .otpCode) = action {
-                HostingJourney(
-                    AuthenticationStore.self,
-                    rootView: OTPCodeEntryView(otpVM: store.otpState)
-                ) { action in
-                    if case .navigationAction(action: .authSuccess) = action {
-                        next(.success).hidesBackButton
-                    }
-                }
-            }
-        }
-        .onPresent {
-            let store: AuthenticationStore = globalPresentableStoreContainer.get()
-            store.otpState.reset()
-        }
-    }
-}
