86f087aeaf6541b55cdb0029dde7b4c60cc9e5cf
Sladan Nimcevic <sladan.nimcevic@hedvig.com>
2024-05-21 14:08:22 +0200

    Added scroll height update on push

--- Projects/App/Sources/Screens/ClaimsScreen/Screens/ClaimFlowAskForPushnotifications.swift
+++ Projects/App/Sources/Screens/ClaimsScreen/Screens/ClaimFlowAskForPushnotifications.swift
@@ -20,51 +20,53 @@ struct AskForPushnotifications: View {
     }
 
     var body: some View {
-        hSection {
-            VStack(spacing: 24) {
-                Spacer()
-                hCoreUIAssets.infoIconFilled.view
-                    .resizable()
-                    .frame(width: 24, height: 24)
-                    .foregroundColor(hSignalColor.blueElement)
-                VStack(spacing: 0) {
-                    hText(L10n.activateNotificationsTitle)
-                    hText(text)
-                        .multilineTextAlignment(.center)
-                        .padding(.horizontal, 32)
-                        .foregroundColor(hTextColor.secondary)
-                }
-                hButton.MediumButton(type: .primary) {
-                    let current = UNUserNotificationCenter.current()
-                    current.getNotificationSettings(completionHandler: { settings in
-                        DispatchQueue.main.async {
-                            UIApplication.shared.appDelegate
-                                .registerForPushNotifications {
-                                    onActionExecuted()
-                                }
-                        }
-                    })
-                } content: {
-                    hText(L10n.claimsActivateNotificationsCta, style: .body)
-                }
-                .fixedSize()
+        hForm {
+            hSection {
+                VStack(spacing: 24) {
+                    Spacer()
+                    hCoreUIAssets.infoIconFilled.view
+                        .resizable()
+                        .frame(width: 24, height: 24)
+                        .foregroundColor(hSignalColor.blueElement)
+                    VStack(spacing: 0) {
+                        hText(L10n.activateNotificationsTitle)
+                        hText(text)
+                            .multilineTextAlignment(.center)
+                            .padding(.horizontal, 32)
+                            .foregroundColor(hTextColor.secondary)
+                    }
+                    hButton.MediumButton(type: .primary) {
+                        let current = UNUserNotificationCenter.current()
+                        current.getNotificationSettings(completionHandler: { settings in
+                            DispatchQueue.main.async {
+                                UIApplication.shared.appDelegate
+                                    .registerForPushNotifications {
+                                        onActionExecuted()
+                                    }
+                            }
+                        })
+                    } content: {
+                        hText(L10n.claimsActivateNotificationsCta, style: .body)
+                    }
+                    .fixedSize()
 
-                Spacer()
-                hButton.LargeButton(type: .ghost) {
-                    onActionExecuted()
-                    let store: ProfileStore = globalPresentableStoreContainer.get()
-                    store.send(.setPushNotificationStatus(status: nil))
-                } content: {
-                    hText(L10n.claimsActivateNotificationsDismiss, style: .footnote)
-                        .foregroundColor(hTextColor.primary)
+                    Spacer()
+                    hButton.LargeButton(type: .ghost) {
+                        onActionExecuted()
+                        let store: ProfileStore = globalPresentableStoreContainer.get()
+                        store.send(.setPushNotificationStatus(status: nil))
+                    } content: {
+                        hText(L10n.claimsActivateNotificationsDismiss, style: .footnote)
+                            .foregroundColor(hTextColor.primary)
+                    }
+                    .padding(.bottom, 16)
                 }
-                .padding(.bottom, 16)
             }
+            .sectionContainerStyle(.transparent)
+            .background(
+                BackgroundView().ignoresSafeArea()
+            )
         }
-        .sectionContainerStyle(.transparent)
-        .background(
-            BackgroundView().ignoresSafeArea()
-        )
     }
 }
 
