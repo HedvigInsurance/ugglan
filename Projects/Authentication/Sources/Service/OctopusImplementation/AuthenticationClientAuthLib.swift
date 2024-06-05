4295bbab4ab1f37ef16fdc7fab6b6ff137de1d6a
Sladan Nimcevic <sladan.nimcevic@hedvig.com>
2024-06-05 13:24:10 +0200

    Added missing clients call

--- Projects/Authentication/Sources/Service/OctopusImplementation/AuthenticationClientAuthLib.swift
+++ Projects/Authentication/Sources/Service/OctopusImplementation/AuthenticationClientAuthLib.swift
@@ -5,39 +5,43 @@ import hCore
 import hGraphQL
 
 public class AuthenticationService {
-    @Inject var service: AuthenticationClient
+    @Inject var client: AuthenticationClient
 
     public init() {}
 
     func submit(otpState: OTPState) async throws -> String {
         log.info("AuthenticationService: submit", error: nil, attributes: nil)
-        return try await service.submit(otpState: otpState)
+        return try await client.submit(otpState: otpState)
     }
 
     func start(with otpState: OTPState) async throws -> (verifyUrl: URL, resendUrl: URL, maskedEmail: String?) {
         log.info("AuthenticationService: start", error: nil, attributes: nil)
-        return try await service.start(with: otpState)
+        return try await client.start(with: otpState)
     }
 
     func resend(otp otpState: OTPState) async throws {
         log.info("AuthenticationService: resend", error: nil, attributes: nil)
+        try await client.resend(otp: otpState)
     }
 
     func startSeBankId(updateStatusTo: @escaping (_: ObserveStatusResponseType) -> Void) async throws {
         log.info("AuthenticationService: startSeBankId", error: nil, attributes: nil)
-        return try await service.startSeBankId(updateStatusTo: updateStatusTo)
+        return try await client.startSeBankId(updateStatusTo: updateStatusTo)
     }
 
     func logout() async throws {
         log.info("AuthenticationService: logout", error: nil, attributes: nil)
+        try await client.logout()
     }
 
     public func exchange(code: String) async throws {
-        log.info("AuthenticationService: exchange", error: nil, attributes: nil)
+        log.info("AuthenticationService: exchange code", error: nil, attributes: nil)
+        try await client.exchange(code: code)
     }
 
     func exchange(refreshToken: String) async throws {
-        log.info("AuthenticationService: exchange", error: nil, attributes: nil)
+        log.info("AuthenticationService: exchange refresh token", error: nil, attributes: nil)
+        try await client.exchange(refreshToken: refreshToken)
     }
 }
 
