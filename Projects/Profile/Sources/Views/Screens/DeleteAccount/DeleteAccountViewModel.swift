37a1ef027c34bad63681ed1a241aff1dbaeea78c
julia-andersson <julia.andersson@hedvig.com>
2024-07-22 12:51:29 +0200

    remove flow from DeleteAccountViewModel

--- Projects/Profile/Sources/Views/Screens/DeleteAccount/DeleteAccountViewModel.swift
+++ Projects/Profile/Sources/Views/Screens/DeleteAccount/DeleteAccountViewModel.swift
@@ -2,7 +2,6 @@ import Apollo
 import Claims
 import Combine
 import Contracts
-import Flow
 import Foundation
 import Presentation
 import hCore
@@ -12,18 +11,18 @@ public class DeleteAccountViewModel: ObservableObject {
     var memberDetails: MemberDetails
     let claimsStore: ClaimsStore
     let contractsStore: ContractStore
-    let bag = DisposeBag()
 
-    private var activeClaimsSignal: ReadSignal<Bool> {
-        self.claimsStore.stateSignal.map { $0.hasActiveClaims }
+    private var activeClaimsSignal: AnyPublisher<Bool, Never> {
+        self.claimsStore.stateSignal.plain().map({ $0.hasActiveClaims }).publisher.eraseToAnyPublisher()
     }
 
-    private var activeContractsSignal: ReadSignal<Bool> {
-        self.contractsStore.stateSignal.map { $0.hasActiveContracts }
+    private var activeContractsSignal: AnyPublisher<Bool, Never> {
+        self.contractsStore.stateSignal.plain().map({ $0.hasActiveContracts }).publisher.eraseToAnyPublisher()
     }
 
     @Published var hasActiveClaims: Bool = false
     @Published var hasActiveContracts: Bool = false
+    private var cancellables = Set<AnyCancellable>()
 
     public init(
         memberDetails: MemberDetails,
@@ -34,9 +33,21 @@ public class DeleteAccountViewModel: ObservableObject {
         self.claimsStore = claimsStore
         self.contractsStore = contractsStore
 
-        self.hasActiveClaims = activeClaimsSignal.value
-        self.hasActiveContracts = activeContractsSignal.value
-        bag += activeClaimsSignal.distinct(on: .main).onValue { self.hasActiveClaims = $0 }
-        bag += activeContractsSignal.distinct(on: .main).onValue { self.hasActiveContracts = $0 }
+        self.hasActiveClaims = self.claimsStore.stateSignal.map({ $0.hasActiveClaims }).value
+        self.hasActiveContracts = self.contractsStore.stateSignal.map({ $0.hasActiveContracts }).value
+
+        activeClaimsSignal
+            .receive(on: RunLoop.main)
+            .sink { value in
+                self.hasActiveClaims = value
+            }
+            .store(in: &cancellables)
+
+        activeContractsSignal
+            .receive(on: RunLoop.main)
+            .sink { value in
+                self.hasActiveContracts = value
+            }
+            .store(in: &cancellables)
     }
 }
