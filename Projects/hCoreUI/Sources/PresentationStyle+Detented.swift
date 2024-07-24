d4dc0b21a663b841feb641243855946d49b21469
julia-andersson <julia.andersson@hedvig.com>
2024-07-24 15:06:06 +0200

    update delay

--- Projects/hCoreUI/Sources/PresentationStyle+Detented.swift
+++ Projects/hCoreUI/Sources/PresentationStyle+Detented.swift
@@ -138,7 +138,7 @@ class DetentedTransitioningDelegate: NSObject, UIViewControllerTransitioningDele
             )
 
             Task { @MainActor [weak presentationController, weak self] in
-                try! await Task.sleep(nanoseconds: 1)
+                try! await Task.sleep(nanoseconds: 1_000_000)
 
                 if let self, let presentationController {
                     PresentationStyle.Detent.set(
