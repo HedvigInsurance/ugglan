6c090692bd42b27bd621de4df9921b126dd4ec39
julia-andersson <julia.andersson@hedvig.com>
2024-08-28 11:40:49 +0200

    added view model tests

---
+++ Projects/MoveFlow/Tests/ViewModelTests/MovingFlowAddExtraBuildingViewModelTests.swift
@@ -0,0 +1,44 @@
+import Presentation
+import XCTest
+import hCore
+
+@testable import MoveFlow
+
+final class MovingFlowAddExtraBuildingViewModelTests: XCTestCase {
+    weak var store: MoveFlowStore?
+    let buildingType = "building type"
+
+    override func setUp() {
+        super.setUp()
+        globalPresentableStoreContainer.deletePersistanceContainer()
+    }
+
+    override func tearDown() async throws {
+        await waitUntil(description: "Store deinited successfully") {
+            self.store == nil
+        }
+    }
+
+    func testSetExtraBuildingSuccess() async {
+        let store = MoveFlowStore()
+        self.store = store
+
+        let model = MovingFlowAddExtraBuildingViewModel()
+        model.livingArea = "80"
+
+        await model.store.sendAsync(.setExtraBuildingType(with: buildingType))
+
+        assert(model.buildingType == buildingType)
+        assert(model.isValid() == true)
+    }
+
+    func testIsNotValidSuccess() async {
+        let store = MoveFlowStore()
+        self.store = store
+
+        let model = MovingFlowAddExtraBuildingViewModel()
+        model.buildingType = buildingType
+
+        assert(model.isValid() == false)
+    }
+}
