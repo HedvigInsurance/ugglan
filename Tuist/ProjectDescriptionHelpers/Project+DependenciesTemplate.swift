44313acb8efd7f1f1f509ec184da160636e87081
julia-andersson <julia.andersson@hedvig.com>
2024-09-11 08:32:22 +0200

    upgrade apollo-ios-codegen

--- Tuist/ProjectDescriptionHelpers/Project+DependenciesTemplate.swift
+++ Tuist/ProjectDescriptionHelpers/Project+DependenciesTemplate.swift
@@ -105,7 +105,7 @@ public enum ExternalDependencies: CaseIterable {
             ]
         case .apolloIosCodegen:
             return [
-                .package(url: "https://github.com/apollographql/apollo-ios-codegen", .exact("1.13.0"))
+                .package(url: "https://github.com/apollographql/apollo-ios-codegen", .exact("1.15.1"))
             ]
         case .argumentParser:
             return [
