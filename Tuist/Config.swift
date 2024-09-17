9731b3b2faee504e9c4ad14a0a0455afe7d1b27a
Sladan Nimcevic <sladan.nimcevic@hedvig.com>
2024-09-17 08:49:27 +0200

    Updated Tuist config

--- Tuist/Config.swift
+++ Tuist/Config.swift
@@ -2,7 +2,8 @@ import ProjectDescription
 
 let config = Config(
     compatibleXcodeVersions: .list([
-        .upToNextMajor(Version(15, 0, 0))
+        .upToNextMajor(.init(15, 4, 0)),
+        .upToNextMinor(.init(16, 0, 0)),
     ]),
     cloud: nil,
     cache: nil,
