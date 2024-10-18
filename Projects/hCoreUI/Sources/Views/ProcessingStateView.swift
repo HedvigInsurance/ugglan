1c36c1dd8e6485d82f8ff47373b01afbd419454d
Sladan Nimcevic <sladan.nimcevic@hedvig.com>
2024-10-18 14:37:49 +0200

    Crated dynamic "fake" progress

--- Projects/hCoreUI/Sources/Views/ProcessingStateView.swift
+++ Projects/hCoreUI/Sources/Views/ProcessingStateView.swift
@@ -7,18 +7,21 @@ public enum ProcessingState: Equatable {
     case error(errorMessage: String)
 }
 
+private struct AnimationTiming {
+    let delay: Float
+    let duration: Float
+    let progress: Float
+}
+
 public struct ProcessingStateView: View {
     @StateObject var vm = ProcessingViewModel()
     @Binding var state: ProcessingState
-
+    fileprivate let animationTimings: [AnimationTiming]
     var loadingViewText: String
-
     var successViewTitle: String?
     var successViewBody: String?
     var successViewButtonAction: (() -> Void)?
-
     var errorViewButtons: ErrorViewButtonConfig
-
     @Environment(\.hCustomSuccessView) var customSuccessView
 
     public init(
@@ -27,7 +30,8 @@ public struct ProcessingStateView: View {
         successViewBody: String? = nil,
         successViewButtonAction: (() -> Void)? = nil,
         errorViewButtons: ErrorViewButtonConfig,
-        state: Binding<ProcessingState>
+        state: Binding<ProcessingState>,
+        duration: Float = 1.5
     ) {
         self.loadingViewText = loadingViewText
         self.successViewTitle = successViewTitle
@@ -35,6 +39,24 @@ public struct ProcessingStateView: View {
         self.successViewButtonAction = successViewButtonAction
         self.errorViewButtons = errorViewButtons
         self._state = state
+
+        let baseDurationFactor: Float = duration * (Float(1) / Float(24))
+        animationTimings = [
+            .init(delay: 0.5, duration: baseDurationFactor * 8, progress: 0.3),
+            .init(
+                delay: 0.5 + baseDurationFactor * 8,
+                duration: baseDurationFactor * 4,
+                progress: Float.random(in: 0.3...0.5)
+            ),
+            .init(delay: 0.5 + baseDurationFactor * 12, duration: baseDurationFactor * 3, progress: 0.6),
+            .init(delay: 0.5 + baseDurationFactor * 15, duration: baseDurationFactor, progress: 0.7),
+            .init(
+                delay: 0.5 + baseDurationFactor * 16,
+                duration: baseDurationFactor * 6,
+                progress: Float.random(in: 0.7...0.95)
+            ),
+            .init(delay: 0.5 + baseDurationFactor * 22, duration: baseDurationFactor * 2, progress: 1),
+        ]
     }
 
     public var body: some View {
@@ -84,8 +106,13 @@ public struct ProcessingStateView: View {
                     .frame(width: UIScreen.main.bounds.width * 0.53)
                     .onAppear {
                         DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                            withAnimation(.easeInOut(duration: 1.5).delay(0.5)) {
-                                vm.progress = 1
+                            animationTimings.forEach { item in
+                                print("DELAY FOR \(item.progress) \(item.delay) is \(item.duration)")
+                                withAnimation(
+                                    .easeInOut(duration: TimeInterval(item.duration)).delay(TimeInterval(item.delay))
+                                ) {
+                                    vm.progress = item.progress
+                                }
                             }
                         }
                     }
