84ee3b6ca47e9d8721ec02731fe481b22eebcd64
Sladan Nimcevic <sladan.nimcevic@hedvig.com>
2024-10-30 16:51:09 +0100

    echo build for tests

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -11,6 +11,10 @@ echo "password ${MAVEN_PASSWORD}" >> ~/.netrc
 brew tap tuist/tuist
 brew install --formula tuist
 brew install --formula tuist@4.31.0
+if [[ $CI_WORKFLOW == "Tests" ]]; then
+    export BUILD_FOR_TESTS="1"
+fi
+echo "BUILD FOR TESTS = ${BUILD_FOR_TESTS}"
 
 cd $CI_PRIMARY_REPOSITORY_PATH;
 
