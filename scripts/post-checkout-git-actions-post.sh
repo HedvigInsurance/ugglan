5164ad101af9895d39409a622ff62542c9923a8d
Sladan Nimcevic <sladan.nimcevic@hedvig.com>
2024-10-31 10:06:34 +0100

    Aded actions

---
+++ scripts/post-checkout-git-actions-post.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+scripts/configure-embed-app-extensions.sh
---
+++ scripts/post-checkout-git-actions-pre.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+rm -rf **/Derived/*
+
+scripts/githooks.sh
+arch -x86_64 scripts/translations.sh
+scripts/swiftgen.sh
+scripts/codegen.sh
