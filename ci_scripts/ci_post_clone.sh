879aa456086b02e6205e45906ba76173d5de68c2
Sladan Nimcevic <sladan.nimcevic@hedvig.com>
2024-11-01 16:01:56 +0100

    Use tuist 4.31.0

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -8,8 +8,8 @@ echo "machine maven.pkg.github.com" > ~/.netrc
 echo "login ${MAVEN_LOGIN}" >> ~/.netrc
 echo "password ${MAVEN_PASSWORD}" >> ~/.netrc
 
-brew tap tuist/tuist
-brew install --formula tuist
+brew tap tuist/tuist@4.31.0
+brew install --formula tuist@4.31.0
 brew install --formula tuist@4.31.0
 
 cd $CI_PRIMARY_REPOSITORY_PATH;
