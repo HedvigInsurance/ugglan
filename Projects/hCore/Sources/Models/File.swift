52e736444cf4f0af323256b1793497c3991b3ac0
Sladan Nimcevic <sladan.nimcevic@hedvig.com>
2024-11-06 15:00:56 +0100

    Moved things around

--- Projects/hCore/Sources/Models/File.swift
+++ Projects/hCore/Sources/Models/File.swift
@@ -177,3 +177,62 @@ extension NSItemProvider {
         case invalidData
     }
 }
+
+extension NSItemProvider {
+    public func getFile(onFileResolved: @escaping (File) -> Void) {
+        let name = self.suggestedName ?? ""
+        if self.hasItemConformingToTypeIdentifier(UTType.image.identifier) {
+            self.loadFileRepresentation(forTypeIdentifier: UTType.image.identifier) { url, error in
+                if let url, let data = FileManager.default.contents(atPath: url.relativePath),
+                    let image = UIImage(data: data),
+                    let data = image.jpegData(compressionQuality: 0.9)
+                {
+                    let file = File(
+                        id: UUID().uuidString,
+                        size: Double(data.count),
+                        mimeType: .JPEG,
+                        name: name,
+                        source: .data(data: data)
+                    )
+                    onFileResolved(file)
+                }
+            }
+        } else if self.hasItemConformingToTypeIdentifier(UTType.movie.identifier) {
+            self.loadFileRepresentation(forTypeIdentifier: UTType.movie.identifier) { videoUrl, error in
+                if let videoUrl, let data = FileManager.default.contents(atPath: videoUrl.relativePath),
+                    let mimeType = UTType(filenameExtension: videoUrl.pathExtension)?.preferredMIMEType
+                {
+                    Task {
+                        let mimeType = MimeType.findBy(mimeType: mimeType)
+                        let file = File(
+                            id: UUID().uuidString,
+                            size: Double(data.count),
+                            mimeType: mimeType,
+                            name: name,
+                            source: .data(data: data)
+                        )
+                        onFileResolved(file)
+                    }
+                }
+            }
+        } else if self.hasItemConformingToTypeIdentifier(UTType.item.identifier) {
+            self.loadFileRepresentation(forTypeIdentifier: UTType.item.identifier) { itemUrl, error in
+                if let itemUrl, let data = FileManager.default.contents(atPath: itemUrl.relativePath),
+                    let mimeType = UTType(filenameExtension: itemUrl.pathExtension)?.preferredMIMEType
+                {
+                    Task {
+                        let mimeType = MimeType.findBy(mimeType: mimeType)
+                        let file = File(
+                            id: UUID().uuidString,
+                            size: Double(data.count),
+                            mimeType: mimeType,
+                            name: name,
+                            source: .data(data: data)
+                        )
+                        onFileResolved(file)
+                    }
+                }
+            }
+        }
+    }
+}
