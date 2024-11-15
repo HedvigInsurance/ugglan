765473939daf55bfd961fd40b85e7dd919146bbe
Sladan Nimcevic <sladan.nimcevic@hedvig.com>
2024-11-15 15:58:14 +0100

    Added fixes for assets

--- scripts/swiftgen.sh
+++ scripts/swiftgen.sh
@@ -16,19 +16,6 @@ mkdir Projects/Market/Sources/Derived
 if [[ -f $TMPDIR/bin/swiftgen ]]
 then
     $TMPDIR/bin/swiftgen
-    file="Projects/hCoreUI/Sources/Derived/Assets.swift"         # File where you want to insert the new line
-    search_text="public enum hCoreUIAssets"             # Line number at which to insert the new line
-    replace_text="@MainActor \npublic enum hCoreUIAssets"  # Text to insert
-    sleep 1
-    if grep -q "@MainActor" "$file"; then
-        echo "Already @MainActor"
-    else
-        if [ ! -f "$file" ]; then
-            echo "File not found!"
-            exit 1
-        fi
-    sed -i '' "s/${search_text}/${replace_text}/g" "$file"
-    fi
     exit 0
 fi
 
