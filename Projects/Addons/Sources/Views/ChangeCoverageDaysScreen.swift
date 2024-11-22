e9b57f03b79ae41b6823cdcab91bc6af4e6b08b4
julia-andersson <julia.andersson@hedvig.com>
2024-11-22 16:24:09 +0100

    add buttons

--- Projects/Addons/Sources/Views/ChangeCoverageDaysScreen.swift
+++ Projects/Addons/Sources/Views/ChangeCoverageDaysScreen.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import hCore
 import hCoreUI
 
 public struct ChangeCoverageDaysScreen: View {
@@ -7,52 +8,71 @@ public struct ChangeCoverageDaysScreen: View {
     @State var selectedDays: String?
 
     public var body: some View {
-        hForm {}
-            .hFormTitle(
-                title: .init(
-                    .small,
-                    .heading1,
-                    addon.title,
-                    alignment: .leading
-                ),
-                subTitle: .init(
-                    .small,
-                    .heading1,
-                    "Välj din skyddsnivå"
-                )
-            )
-            .hDisableScroll
-            .hFormAttachToBottom {
-                VStack(spacing: .padding4) {
-                    ForEach(addon.coverageDays ?? [], id: \.title) { coverageDay in
-                        hSection {
-                            hRadioField(
-                                id: coverageDay.title,
-                                itemModel: nil,
-                                leftView: {
-                                    HStack {
-                                        hText(coverageDay.title)
-                                        Spacer()
-                                        hPill(
-                                            text: "+ \(coverageDay.nbOfDays) kr/mo",
-                                            color: .grey(translucent: true),
-                                            colorLevel: .one
-                                        )
-                                        .hFieldSize(.small)
-                                    }
-                                    .asAnyView
-                                },
-                                selected: $selectedDays,
-                                error: .constant(nil),
-                                useAnimation: true
-                            )
-                            .hFieldSize(.medium)
-                            .hFieldLeftAttachedView
-                        }
+        hForm {
+            VStack(spacing: .padding4) {
+                ForEach(addon.coverageDays ?? [], id: \.title) { coverageDay in
+                    hSection {
+                        hRadioField(
+                            id: coverageDay.title,
+                            itemModel: nil,
+                            leftView: {
+                                HStack {
+                                    hText(coverageDay.title)
+                                    Spacer()
+                                    hPill(
+                                        text: "+ \(coverageDay.nbOfDays) kr/mo",
+                                        color: .grey(translucent: true),
+                                        colorLevel: .one
+                                    )
+                                    .hFieldSize(.small)
+                                }
+                                .asAnyView
+                            },
+                            selected: $selectedDays,
+                            error: .constant(nil),
+                            useAnimation: true
+                        )
+                        .hFieldSize(.medium)
+                        .hFieldLeftAttachedView
                     }
                 }
-                .padding(.top, 16)
             }
+            .padding(.top, 32)
+
+        }
+        .hFormTitle(
+            title: .init(
+                .small,
+                .heading1,
+                addon.title,
+                alignment: .leading
+            ),
+            subTitle: .init(
+                .small,
+                .heading1,
+                "Välj din skyddsnivå"
+            )
+        )
+        .hDisableScroll
+        .hFormAttachToBottom {
+            hSection {
+                VStack(spacing: .padding8) {
+                    hButton.LargeButton(type: .primary) {
+                        /* TODO: IMPLEMENT */
+                    } content: {
+                        hText(L10n.generalConfirm)
+                    }
+
+                    hButton.LargeButton(type: .ghost) {
+                        /* TODO: IMPLEMENT */
+                    } content: {
+                        hText(L10n.generalCancelButton)
+                    }
+                }
+            }
+            .sectionContainerStyle(.transparent)
+            .padding(.top, 16)
+        }
     }
 }
 
