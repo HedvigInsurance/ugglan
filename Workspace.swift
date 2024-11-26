0349d219e70179c059ea3f6712ebb6d0f8778569
Sladan Nimcevic <sladan.nimcevic@hedvig.com>
2024-11-26 17:01:35 +0100

    Set lastXcodeUpgradeCheck

--- Workspace.swift
+++ Workspace.swift
@@ -1,4 +1,8 @@
 import ProjectDescription
 import ProjectDescriptionHelpers
 
-let workspace = Workspace(name: "Ugglan", projects: ["Projects/*", "Dependencies/*"])
+let workspace = Workspace(
+    name: "Ugglan",
+    projects: ["Projects/*", "Dependencies/*"],
+    generationOptions: .options(lastXcodeUpgradeCheck: .init(string: "1610"))
+)
