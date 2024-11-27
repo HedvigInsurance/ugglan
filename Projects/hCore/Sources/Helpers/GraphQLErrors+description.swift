7609d958f69e2d08d09e11a8f889f9bd600da7d9
Sladan Nimcevic <sladan.nimcevic@hedvig.com>
2024-11-27 13:04:08 +0100

    Fixed error that we show

--- Projects/hCore/Sources/Helpers/GraphQLErrors+description.swift
+++ Projects/hCore/Sources/Helpers/GraphQLErrors+description.swift
@@ -6,8 +6,7 @@ extension hGraphQL.GraphQLError: @retroactive @preconcurrency LocalizedError {
     public var errorDescription: String? {
         switch self {
         case .graphQLError(let errors):
-            let messages = errors.map { $0.localizedDescription }
-            return messages.joined(separator: " ")
+            return L10n.General.errorBody
         case .otherError:
             return L10n.General.errorBody
         }
