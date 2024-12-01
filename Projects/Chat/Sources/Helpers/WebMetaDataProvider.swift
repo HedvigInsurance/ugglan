f4e46f490f2f5f9523ae565cea32b612e87412c0
Sladan Nimcevic <sladan.nimcevic@hedvig.com>
2024-12-01 10:46:38 +0100

    Fixed crash for LPMetadataProvider

--- Projects/Chat/Sources/Helpers/WebMetaDataProvider.swift
+++ Projects/Chat/Sources/Helpers/WebMetaDataProvider.swift
@@ -1,5 +1,6 @@
-import Foundation
+@preconcurrency import Foundation
 @preconcurrency import LinkPresentation
+import UniformTypeIdentifiers
 
 @MainActor
 class WebMetaDataProvider {
@@ -7,84 +8,34 @@ class WebMetaDataProvider {
     private init() {}
     private var cache: [URL: WebMetaDataProviderData?] = [:]
     private var failedURLs: [URL] = []
+    @MainActor
     func data(for url: URL) async throws -> WebMetaDataProviderData? {
         if let data = cache[url] {
             return data
         } else if failedURLs.contains(url) {
             return nil
         } else {
-            let data = try await withCheckedThrowingContinuation {
-                (inCont: CheckedContinuation<WebMetaDataProviderData, Error>) -> Void in
-                let metadataProvider = LPMetadataProvider()
-                DispatchQueue.main.async {
-                    metadataProvider.startFetchingMetadata(for: url) { [weak self] metadata, error in
-                        if let metadata {
-                            if #available(iOS 16.0, *) {
-                                if let imageProvider = metadata.imageProvider {
-                                    let title = metadata.title ?? ""
-                                    _ =
-                                        imageProvider
-                                        .loadDataRepresentation(
-                                            for: .image,
-                                            completionHandler: { [weak self] data, error in
-                                                Task { @MainActor in
-                                                    if let data, let image = UIImage(data: data) {
-                                                        self?.cache[url] = WebMetaDataProviderData(
-                                                            title: title,
-                                                            image: image
-                                                        )
-                                                    } else {
-                                                        self?.cache[url] = WebMetaDataProviderData(
-                                                            title: title,
-                                                            image: nil
-                                                        )
-                                                    }
-                                                    if let data = self?.cache[url], let data = data {
-                                                        inCont.resume(returning: data)
-                                                    } else {
-                                                        inCont.resume(
-                                                            throwing: WebMetaDataProviderError.somethingWentWrong(
-                                                                url: url
-                                                            )
-                                                        )
-                                                    }
-                                                }
-                                            }
-                                        )
-                                } else if let title = metadata.title {
-                                    self?.cache[url] = WebMetaDataProviderData(
-                                        title: title,
-                                        image: nil
-                                    )
-                                    if let data = self?.cache[url], let data = data {
-                                        inCont.resume(returning: data)
-                                    } else {
-                                        inCont.resume(throwing: WebMetaDataProviderError.somethingWentWrong(url: url))
-                                    }
-                                } else {
-                                    self?.failedURLs.append(url)
-                                    inCont.resume(throwing: WebMetaDataProviderError.somethingWentWrong(url: url))
-                                }
-                            } else {
-                                self?.cache[url] = WebMetaDataProviderData(
-                                    title: metadata.title ?? "",
-                                    image: nil
-                                )
-
-                                if let data = self?.cache[url], let data = data {
-                                    inCont.resume(returning: data)
-                                } else {
-                                    inCont.resume(throwing: WebMetaDataProviderError.somethingWentWrong(url: url))
-                                }
-                            }
-                        } else {
-                            inCont.resume(throwing: WebMetaDataProviderError.somethingWentWrong(url: url))
+            let metadataProvider = LPMetadataProvider()
+            do {
+                let metadata = try await metadataProvider.startFetchingMetadata(for: url)
+                if let imageProvider = metadata.imageProvider {
+                    let title = metadata.title ?? ""
+                    var image: UIImage?
+                    do {
+                        let imageFromUrl = try await imageProvider.loadItem(forTypeIdentifier: UTType.image.identifier)
+                        if let data = imageFromUrl as? Data {
+                            image = UIImage(data: data)
                         }
-
                     }
+                    let returnValue = WebMetaDataProviderData(title: title, image: image)
+                    cache[url] = returnValue
+                    return returnValue
+                } else {
+                    throw WebMetaDataProviderError.somethingWentWrong(url: url)
                 }
+            } catch {
+                throw WebMetaDataProviderError.somethingWentWrong(url: url)
             }
-            return data
         }
     }
 
