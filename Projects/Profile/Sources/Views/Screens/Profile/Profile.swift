721b94f5c751325b24c18746a9ac882960065c70
Sladan Nimcevic <sladan.nimcevic@hedvig.com>
2025-01-14 14:57:25 +0100

    Updated logout button padding

--- Projects/Profile/Sources/Views/Screens/Profile/Profile.swift
+++ Projects/Profile/Sources/Views/Screens/Profile/Profile.swift
@@ -67,6 +67,7 @@ public struct ProfileView: View {
                     .alert(isPresented: $showLogoutAlert) {
                         logoutAlert
                     }
+                    .padding(.bottom, .padding16)
                 }
             }
             .sectionContainerStyle(.transparent)
