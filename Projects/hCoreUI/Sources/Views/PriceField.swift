07aa216228d1c64f5389b61f9a2a8e32d4f947da
julia-andersson <julia.andersson@hedvig.com>
2025-02-04 15:26:41 +0100

    some edit co-insured fixes

--- Projects/hCoreUI/Sources/Views/PriceField.swift
+++ Projects/hCoreUI/Sources/Views/PriceField.swift
@@ -5,66 +5,74 @@ import hGraphQL
 public struct PriceField: View {
     let newPremium: MonetaryAmount?
     let currentPremium: MonetaryAmount?
+    let subTitle: String?
     @SwiftUI.Environment(\.hWithStrikeThroughPrice) var strikeThroughPrice
 
     public init(
         newPremium: MonetaryAmount?,
-        currentPremium: MonetaryAmount?
+        currentPremium: MonetaryAmount?,
+        subTitle: String? = nil
     ) {
         self.newPremium = newPremium
         self.currentPremium = currentPremium
+        self.subTitle = subTitle
     }
 
     public var body: some View {
-        HStack(alignment: .top) {
-            hText(L10n.tierFlowTotal)
-                .foregroundColor(getTotalColor())
-            Spacer()
+        VStack(spacing: .padding2) {
+            HStack(alignment: .top) {
+                hText(L10n.tierFlowTotal)
+                    .foregroundColor(getTotalColor())
+                Spacer()
 
-            if strikeThroughPrice != .none, newPremium != currentPremium {
-                if #available(iOS 16.0, *) {
-                    hText(currentPremium?.formattedAmountPerMonth ?? "")
-                        .strikethrough()
-                        .foregroundColor(hTextColor.Opaque.secondary)
-                        .accessibilityValue(L10n.voiceoverCurrentPrice)
-                } else {
-                    hText(currentPremium?.formattedAmountPerMonth ?? "")
-                        .foregroundColor(hTextColor.Opaque.secondary)
-                }
-            }
-
-            VStack(alignment: .trailing, spacing: 0) {
-                if strikeThroughPrice == .crossNewPrice {
+                if strikeThroughPrice != .none, newPremium != currentPremium {
                     if #available(iOS 16.0, *) {
-                        hText(newPremium?.formattedAmountPerMonth ?? "")
+                        hText(currentPremium?.formattedAmountPerMonth ?? "")
                             .strikethrough()
                             .foregroundColor(hTextColor.Opaque.secondary)
-                            .accessibilityValue(
-                                L10n.voiceoverCurrentPrice
-                            )
-
+                            .accessibilityValue(L10n.voiceoverCurrentPrice)
                     } else {
-                        hText(newPremium?.formattedAmountPerMonth ?? "")
+                        hText(currentPremium?.formattedAmountPerMonth ?? "")
                             .foregroundColor(hTextColor.Opaque.secondary)
-                            .accessibilityValue(
-                                L10n.ReferralsActive.Your.New.Price.title
-                            )
                     }
-                } else {
-                    hText(newPremium?.formattedAmountPerMonth ?? currentPremium?.formattedAmountPerMonth ?? "")
-                        .accessibilityValue(
-                            L10n.ReferralsActive.Your.New.Price.title
+                }
+
+                VStack(alignment: .trailing, spacing: 0) {
+                    if strikeThroughPrice == .crossNewPrice {
+                        if #available(iOS 16.0, *) {
+                            hText(newPremium?.formattedAmountPerMonth ?? "")
+                                .strikethrough()
+                                .foregroundColor(hTextColor.Opaque.secondary)
+                                .accessibilityValue(
+                                    L10n.voiceoverCurrentPrice
+                                )
+
+                        } else {
+                            hText(newPremium?.formattedAmountPerMonth ?? "")
+                                .foregroundColor(hTextColor.Opaque.secondary)
+                                .accessibilityValue(
+                                    L10n.ReferralsActive.Your.New.Price.title
+                                )
+                        }
+                    } else {
+                        hText(newPremium?.formattedAmountPerMonth ?? currentPremium?.formattedAmountPerMonth ?? "")
+                    }
+
+                    if let currentPremium, let newPremium, newPremium != currentPremium,
+                        strikeThroughPrice != .crossOldPrice
+                    {
+                        hText(
+                            L10n.tierFlowPreviousPrice(currentPremium.formattedAmountPerMonth),
+                            style: .label
                         )
+                        .foregroundColor(hTextColor.Opaque.secondary)
+                    }
                 }
-                if let currentPremium, let newPremium, newPremium != currentPremium,
-                    strikeThroughPrice != .crossOldPrice
-                {
-                    hText(
-                        L10n.tierFlowPreviousPrice(currentPremium.formattedAmountPerMonth),
-                        style: .label
-                    )
+            }
+            if let subTitle {
+                hText(subTitle, style: .label)
+                    .frame(maxWidth: .infinity, alignment: .trailing)
                     .foregroundColor(hTextColor.Opaque.secondary)
-                }
             }
         }
         .accessibilityElement(children: .combine)
@@ -100,6 +108,13 @@ public struct PriceField: View {
             currentPremium: nil
         )
         .hWithStrikeThroughPrice(setTo: .crossNewPrice)
+
+        PriceField(
+            newPremium: .init(amount: "99", currency: "SEK"),
+            currentPremium: MonetaryAmount(amount: "49", currency: "SEK"),
+            subTitle: "sub title"
+        )
+        .hWithStrikeThroughPrice(setTo: .crossOldPrice)
     }
     .sectionContainerStyle(.transparent)
 }
