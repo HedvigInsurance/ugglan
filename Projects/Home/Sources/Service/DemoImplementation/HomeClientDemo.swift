b4a16fccf87bbca652d53810bc7d9745785f00db
Sladan Nimcevic <sladan.nimcevic@hedvig.com>
2025-02-11 17:02:58 +0100

    Fixed missing seperator

--- Projects/Home/Sources/Service/DemoImplementation/HomeClientDemo.swift
+++ Projects/Home/Sources/Service/DemoImplementation/HomeClientDemo.swift
@@ -31,32 +31,35 @@ public class HomeClientDemo: HomeClient {
         let question1 = FAQModel(
             id: "id1",
             question: "How do I add more insurances?",
-            answer: "You can easily add more insurance policies at hedvig.com. As soon as you sign a new policy with Hedvig, it will appear here in the app. Read more about our insurances [here](https://www.hedvig.com/se/forsakringar).\n\nDo you want to sign by phone? Call us on +46 10-45 99 200 on weekdays between 9-16."
+            answer:
+                "You can easily add more insurance policies at hedvig.com. As soon as you sign a new policy with Hedvig, it will appear here in the app. Read more about our insurances [here](https://www.hedvig.com/se/forsakringar).\n\nDo you want to sign by phone? Call us on +46 10-45 99 200 on weekdays between 9-16."
         )
         let question2 = FAQModel(
             id: "id2",
             question: "How do I delete my account?",
-            answer: "As an insurance company, Hedvig is obliged to keep the insurance contract and related information for as long as it is possible to make a claim for insurance compensation. This period may vary depending on the type of insurance but is at least 10 years after the expiry of the insurance based on limitation periods.\n\nThese rules affect the possibility to delete an account, as described below.\n\nIf you have an active policy with us, we cannot delete your account. If you want to cancel your insurance, you can do so [here](https://hedvig.page.link/insurances), then select the policy you want to cancel and click on Cancel insurance.\n\nIf you have an open claim, we recommend keeping your account active until your claim is closed. To track of the status of your claim and get messages from our service team, please keep the Hedvig app on your phone.\n\nIf you have cancelled your insurance with Hedvig, we are obliged to save your insurance contract and related information (e.g. registered claims) for at least 10 years after the cancellation of the insurance."
+            answer:
+                "As an insurance company, Hedvig is obliged to keep the insurance contract and related information for as long as it is possible to make a claim for insurance compensation. This period may vary depending on the type of insurance but is at least 10 years after the expiry of the insurance based on limitation periods.\n\nThese rules affect the possibility to delete an account, as described below.\n\nIf you have an active policy with us, we cannot delete your account. If you want to cancel your insurance, you can do so [here](https://hedvig.page.link/insurances), then select the policy you want to cancel and click on Cancel insurance.\n\nIf you have an open claim, we recommend keeping your account active until your claim is closed. To track of the status of your claim and get messages from our service team, please keep the Hedvig app on your phone.\n\nIf you have cancelled your insurance with Hedvig, we are obliged to save your insurance contract and related information (e.g. registered claims) for at least 10 years after the cancellation of the insurance."
         )
-        
+
         let topic1 = FaqTopic(
             id: "topic1",
             title: "Payments",
             commonQuestions: [
                 question1
-                ]
+            ],
             allQuestions: [
                 question2
             ]
         )
         return .init(
             topics: [
-                topic1,
+                topic1
             ],
             commonQuestions: [
                 question1,
-                question2
-            ])
+                question2,
+            ]
+        )
     }
 
 }
