457db23af38fbe4a706b4a8d8c564b72443d309e
Sladan Nimcevic <sladan.nimcevic@hedvig.com>
2025-02-28 16:18:39 +0100

    Moved logger to separated module

---
+++ LocalModules/Logger/Package.swift
@@ -0,0 +1,24 @@
+import PackageDescription
+
+let package = Package(
+    name: "Logger",
+    platforms: [.iOS(.v15)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Logger",
+            targets: ["Logger"]
+        )
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "Logger"
+        ),
+        .testTarget(
+            name: "LoggerTests",
+            dependencies: ["Logger"]
+        ),
+    ]
+)
