d18acbe0b08c79f53649070d67ae91f24ea893d1
Sladan Nimcevic <sladan.nimcevic@hedvig.com>
2025-03-07 12:35:39 +0100

    fixed addon stests

--- Projects/Addons/Tests/ServiceTests/AddonsServiceTests.swift
+++ Projects/Addons/Tests/ServiceTests/AddonsServiceTests.swift
@@ -24,6 +24,7 @@ final class AddonsServiceTests: XCTestCase {
                     displayName: "option title",
                     quoteId: "quoteId1",
                     addonId: "addonId1",
+                    addonSubtype: "addonSubtype1",
                     displayItems: [],
                     price: .init(amount: "49", currency: "SEK"),
                     addonVariant: .init(
@@ -38,6 +39,7 @@ final class AddonsServiceTests: XCTestCase {
                     displayName: "option title",
                     quoteId: "quoteId2",
                     addonId: "addonId2",
+                    addonSubtype: "addonSubtype2",
                     displayItems: [],
                     price: .init(amount: "79", currency: "SEK"),
                     addonVariant: .init(
