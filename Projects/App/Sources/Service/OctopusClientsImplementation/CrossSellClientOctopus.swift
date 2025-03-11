3738d4954f485a5806964fb937b2ff719b64390d
Sladan Nimcevic <sladan.nimcevic@hedvig.com>
2025-03-11 15:30:59 +0100

    Place CrossSell service to proper place

--- Projects/App/Sources/Service/OctopusClientsImplementation/CrossSellClientOctopus.swift
+++ Projects/App/Sources/Service/OctopusClientsImplementation/CrossSellClientOctopus.swift
@@ -3,16 +3,6 @@ import Foundation
 import hCore
 import hGraphQL
 
-@MainActor
-public class CrossSellService {
-    @Inject var service: CrossSellClient
-
-    public func getCrossSell() async throws -> [CrossSell] {
-        log.info("CrossSellService: getCrossSell", error: nil, attributes: nil)
-        return try await service.getCrossSell()
-    }
-}
-
 public class CrossSellClientOctopus: CrossSellClient {
     @Inject private var octopus: hOctopus
     public init() {}
