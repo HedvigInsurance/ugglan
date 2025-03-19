ec89bb012fbb64c75fe82cc08969d5770d8eda0b
julia-andersson <julia.andersson@hedvig.com>
2025-03-19 11:20:30 +0100

    update field size for ContractSelectView

--- Projects/hCoreUI/Sources/Views/ContractSelectView.swift
+++ Projects/hCoreUI/Sources/Views/ContractSelectView.swift
@@ -19,26 +19,27 @@ public struct ContractSelectView<T: Hashable>: View {
         self.modally = modally ?? false
     }
     public var body: some View {
-        if modally {
-            ItemPickerScreen<T>(
-                config: itemPickerConfig
-            )
-            .hFieldSize(.large)
-            .hItemPickerAttributes([.singleSelect])
-            .hFormContentPosition(.compact)
-            .configureTitle(title)
-        } else {
-            ItemPickerScreen<T>(
-                config: itemPickerConfig
-            )
-            .hFormTitle(
-                title: .init(.small, .body2, title, alignment: .leading),
-                subTitle: subtitle != nil ? .init(.small, .body2, subtitle ?? "") : nil
-            )
-            .hFieldSize(.medium)
-            .hItemPickerAttributes([.singleSelect, .attachToBottom, .disableIfNoneSelected])
-            .hFormContentPosition(.bottom)
-            .withDismissButton()
+        Group {
+            if modally {
+                ItemPickerScreen<T>(
+                    config: itemPickerConfig
+                )
+                .hItemPickerAttributes([.singleSelect])
+                .hFormContentPosition(.compact)
+                .configureTitle(title)
+            } else {
+                ItemPickerScreen<T>(
+                    config: itemPickerConfig
+                )
+                .hFormTitle(
+                    title: .init(.small, .body2, title, alignment: .leading),
+                    subTitle: subtitle != nil ? .init(.small, .body2, subtitle ?? "") : nil
+                )
+                .hItemPickerAttributes([.singleSelect, .attachToBottom, .disableIfNoneSelected])
+                .hFormContentPosition(.bottom)
+                .withDismissButton()
+            }
         }
+        .hFieldSize(.medium)
     }
 }
