932774a64ff8b436052bc148bc772df4927447b4
julia-andersson <julia.andersson@hedvig.com>
2025-03-19 17:29:09 +0100

    add addon banner

--- Projects/CrossSell/Sources/View/CrossSellingScreen.swift
+++ Projects/CrossSell/Sources/View/CrossSellingScreen.swift
@@ -1,17 +1,53 @@
+import Addons
 import SwiftUI
 import hCore
 import hCoreUI
 
 public struct CrossSellingScreen: View {
+    @StateObject private var vm = CrossSellingScreenViewModel()
 
     public init() {}
 
     public var body: some View {
         hForm {
-            CrossSellingStack(withHeader: false)
+            VStack(spacing: .padding24) {
+                CrossSellingStack(withHeader: false)
+                if let banner = vm.addonBannerModel {
+                    AddonCardView(
+                        openAddon: {
+                            /* TODO: ADD */
+                        },
+                        addon: banner
+                    )
+                }
+            }
         }
         .hFormContentPosition(.compact)
         .configureTitleView(title: L10n.crossSellTitle, subTitle: L10n.crossSellSubtitle)
+        .onAppear {
+            Task {
+                await vm.getAddonBanner()
+            }
+        }
+    }
+}
+
+@MainActor
+public class CrossSellingScreenViewModel: ObservableObject {
+    @Inject var service: CrossSellClient
+    @Published var addonBannerModel: AddonBannerModel?
+
+    func getAddonBanner() async {
+        do {
+            let data = try await service.getAddonBannerModel(source: .crossSell)
+            withAnimation {
+                self.addonBannerModel = data
+            }
+        } catch {
+            withAnimation {
+                self.addonBannerModel = nil
+            }
+        }
     }
 }
 
