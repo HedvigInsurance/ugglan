0575e56906622f418d8f069cb7c7b014adfb7475
sladan-hedvig <127749787+sladan-hedvig@users.noreply.github.com>
2025-04-24 14:32:06 +0200

    Remove danish and norwegian (#1845)

--- Projects/hCoreUI/Sources/Localization.Locale+Icon.swift
+++ Projects/hCoreUI/Sources/Localization.Locale+Icon.swift
@@ -6,10 +6,6 @@ extension Localization.Locale {
         switch self {
         case .sv_SE:
             return hCoreUIAssets.flagSE.image
-        case .nb_NO:
-            return hCoreUIAssets.flagNO.image
-        case .da_DK:
-            return hCoreUIAssets.flagDK.image
         case .en_SE, .en_NO, .en_DK:
             return hCoreUIAssets.flagUK.image
         }
