16812bf325af1ab5b67459f383ee8787d2ee5bb4
Sladan Nimcevic <sladan.nimcevic@hedvig.com>
2025-04-30 09:57:59 +0200

    Removed tests from insurance evidence

--- Projects/InsuranceEvidence/Project.swift
+++ Projects/InsuranceEvidence/Project.swift
@@ -3,7 +3,7 @@ import ProjectDescriptionHelpers
 
 let project = Project.framework(
     name: "InsuranceEvidence",
-    targets: Set([.framework, .tests, .example]),
+    targets: Set([.framework, .example]),
     projects: ["hCore", "hCoreUI"],
     sdks: [],
     includesGraphQL: true
