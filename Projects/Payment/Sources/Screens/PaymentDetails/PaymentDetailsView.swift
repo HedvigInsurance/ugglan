7cf51a5636f83b81fc6aa40bc0b3da366655fcf8
julia-andersson <julia.andersson@hedvig.com>
2025-05-07 10:31:48 -0400

    set infoButtonDescription to nil

--- Projects/Payment/Sources/Screens/PaymentDetails/PaymentDetailsView.swift
+++ Projects/Payment/Sources/Screens/PaymentDetails/PaymentDetailsView.swift
@@ -43,7 +43,6 @@ struct PaymentDetailsView: View {
             }
             .withHeader(
                 title: L10n.paymentsReferralsInfoTitle,
-                infoButtonDescription: L10n.paymentsDiscountInfoDescription,
                 withoutBottomPadding: false
             )
             .sectionContainerStyle(.transparent)
