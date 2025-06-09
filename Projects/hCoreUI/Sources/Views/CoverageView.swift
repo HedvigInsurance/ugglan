95fd8642728f9fa8f7b30ba40d206e6c3fbc2b74
sladan-hedvig <127749787+sladan-hedvig@users.noreply.github.com>
2025-06-09 13:54:25 +0200

    Fixed perils spacing for insurance details (#1888)

--- Projects/hCoreUI/Sources/Views/CoverageView.swift
+++ Projects/hCoreUI/Sources/Views/CoverageView.swift
@@ -23,6 +23,7 @@ public struct CoverageView: View {
             ) { limit in
                 didTapInsurableLimit(limit)
             }
+            .hWithoutHorizontalPadding([.row, .divider])
             VStack(spacing: .padding32) {
                 ForEach(perils, id: \.title) { perils in
                     VStack(spacing: .padding8) {
@@ -43,6 +44,7 @@ public struct CoverageView: View {
                     }
                 }
             }
+            .hWithoutHorizontalPadding([.section, .divider])
         }
     }
 }
