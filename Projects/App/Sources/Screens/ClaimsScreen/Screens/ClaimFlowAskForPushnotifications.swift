8cb1bcac93933b8bb915742d9a7f49416f9acaf1
julia-andersson <julia.andersson@hedvig.com>
2025-06-24 09:27:14 -0500

    extract buttons view and change padding

--- Projects/App/Sources/Screens/ClaimsScreen/Screens/ClaimFlowAskForPushnotifications.swift
+++ Projects/App/Sources/Screens/ClaimsScreen/Screens/ClaimFlowAskForPushnotifications.swift
@@ -40,7 +40,7 @@ struct AskForPushNotifications: View {
 
     var mainContent: some View {
         hSection {
-            VStack(spacing: .padding24) {
+            VStack(spacing: .padding16) {
                 if !wrapWithForm {
                     Spacer()
                 }
@@ -57,36 +57,58 @@ struct AskForPushNotifications: View {
                         .padding(.horizontal, .padding32)
                         .foregroundColor(hTextColor.Opaque.secondary)
                 }
-                hButton(
-                    .medium,
-                    .primary,
-                    content: .init(title: L10n.claimsActivateNotificationsCta),
-                    {
-                        Task {
-                            await UIApplication.shared.appDelegate.registerForPushNotifications()
-                            onActionExecuted()
-                        }
-                    }
-                )
-                if !wrapWithForm {
-                    Spacer()
-                }
-                hButton(
-                    .large,
-                    .ghost,
-                    content: .init(title: L10n.claimsActivateNotificationsDismiss),
-                    {
-                        onActionExecuted()
-                        let store: ProfileStore = globalPresentableStoreContainer.get()
-                        store.send(.setPushNotificationStatus(status: nil))
-                    }
-                )
-                .padding(.bottom, .padding16)
+                buttonsView
             }
         }
         .sectionContainerStyle(.transparent)
         .background(
             BackgroundView().ignoresSafeArea()
+        )
+    }
+
+    @ViewBuilder
+    var buttonsView: some View {
+        if wrapWithForm {
+            VStack(spacing: .padding4) {
+                activateNotificationsButton
+                if !wrapWithForm {
+                    Spacer()
+                }
+                closeButton
+            }
+        } else {
+            activateNotificationsButton
+            if !wrapWithForm {
+                Spacer()
+            }
+            closeButton
+        }
+    }
+
+    var activateNotificationsButton: some View {
+        hButton(
+            .medium,
+            .primary,
+            content: .init(title: L10n.claimsActivateNotificationsCta),
+            {
+                Task {
+                    await UIApplication.shared.appDelegate.registerForPushNotifications()
+                    onActionExecuted()
+                }
+            }
+        )
+    }
+
+    var closeButton: some View {
+        hButton(
+            .large,
+            .ghost,
+            content: .init(title: L10n.claimsActivateNotificationsDismiss),
+            {
+                onActionExecuted()
+                let store: ProfileStore = globalPresentableStoreContainer.get()
+                store.send(.setPushNotificationStatus(status: nil))
+            }
         )
     }
 }
