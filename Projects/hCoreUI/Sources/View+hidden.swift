76c51629ae7d5a4473e81a64cbeb23f3a3d0f457
julia-andersson <julia.andersson@hedvig.com>
2025-06-26 14:01:01 -0500

    rename

--- Projects/hCoreUI/Sources/View+hidden.swift
+++ Projects/hCoreUI/Sources/View+hidden.swift
@@ -43,7 +43,7 @@ struct HideViewController: ViewModifier {
             if let navigation = vc.navigationController {
                 return findProperVC(from: navigation)
             } else {
-                if vc.presentationController is BlurredSheetPresentationController {
+                if vc.presentationController is BlurredSheetPresenationController {
                     return vc
                 } else if let superviewVc = vc.view.superview?.viewController {
                     return findProperVC(from: superviewVc)
