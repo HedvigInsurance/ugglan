27d086d41048bee8d4943746249b555521012e68
stylianosgakis <stylianos.gakis98@gmail.com>
2025-07-07 16:52:51 +0200

    Update framework name in post-build-action.sh to HedvigShared

--- scripts/post-build-action.sh
+++ scripts/post-build-action.sh
@@ -2,7 +2,7 @@
 function copyFramework() {
     cp -rf "${CONFIGURATION_BUILD_DIR}/"$1.framework "${TARGET_BUILD_DIR}/${TARGET_NAME}.app/Frameworks/"$1.framework
 }
-copyFramework authlib
+copyFramework HedvigShared
 
 rm -rf "${TARGET_BUILD_DIR}/${TARGET_NAME}.app/Frameworks/"*".framework"/Frameworks
 
