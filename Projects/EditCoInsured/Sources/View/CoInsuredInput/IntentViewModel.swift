985028a32567693adb8200eaf56a102c5bafe74f
julia-andersson <julia.andersson@hedvig.com>
2025-07-24 10:40:39 -0500

    Clean up CoInusuredInputScreen

---
+++ Projects/EditCoInsured/Sources/View/CoInsuredInput/IntentViewModel.swift
@@ -0,0 +1,98 @@
+import EditCoInsuredShared
+import Foundation
+import SwiftUI
+import hCore
+import hCoreUI
+
+@MainActor
+public class IntentViewModel: ObservableObject {
+    @Published var intent = Intent(
+        activationDate: "",
+        currentPremium: MonetaryAmount(amount: 0, currency: ""),
+        newPremium: MonetaryAmount(amount: 0, currency: ""),
+        id: "",
+        state: ""
+    )
+    @Published var isLoading: Bool = false
+    @Published var firstName = ""
+    @Published var lastName = ""
+    @Published var nameFetchedFromSSN: Bool = false
+    @Published var enterManually: Bool = false
+    @Published var errorMessageForInput: String?
+    @Published var errorMessageForCoinsuredList: String?
+    @Published var viewState: ProcessingState = .loading
+
+    var fullName: String {
+        return firstName + " " + lastName
+    }
+    var service = EditCoInsuredService()
+
+    var showErrorViewForCoInsuredList: Bool {
+        errorMessageForCoinsuredList != nil
+    }
+
+    var showErrorViewForCoInsuredInput: Bool {
+        errorMessageForInput != nil
+    }
+
+    var contractId: String?
+
+    @MainActor
+    func getIntent(contractId: String, origin: GetIntentOrigin, coInsured: [CoInsuredModel]) async {
+        self.contractId = contractId
+        withAnimation {
+            self.isLoading = true
+            self.errorMessageForInput = nil
+            self.errorMessageForCoinsuredList = nil
+            self.viewState = .loading
+        }
+        do {
+            let data = try await service.sendIntent(contractId: contractId, coInsured: coInsured)
+            withAnimation {
+                self.intent = data
+                self.viewState = .success
+            }
+        } catch let exception {
+            withAnimation {
+                switch origin {
+                case .coinsuredSelectList:
+                    self.errorMessageForCoinsuredList = exception.localizedDescription
+                    self.viewState = .error(errorMessage: errorMessageForCoinsuredList ?? L10n.generalError)
+                case .coinsuredInput:
+                    self.errorMessageForInput = exception.localizedDescription
+                    self.viewState = .error(errorMessage: errorMessageForInput ?? L10n.generalError)
+                }
+            }
+        }
+        withAnimation {
+            self.isLoading = false
+        }
+    }
+
+    enum GetIntentOrigin {
+        case coinsuredSelectList
+        case coinsuredInput
+    }
+
+    @MainActor
+    func performCoInsuredChanges(commitId: String) async {
+        withAnimation {
+            viewState = .loading
+            self.isLoading = true
+        }
+        do {
+            try await service.sendMidtermChangeIntentCommit(commitId: commitId)
+            withAnimation {
+                self.viewState = .success
+            }
+            AskForRating().askForReview()
+        } catch let exception {
+            withAnimation {
+                viewState = .error(errorMessage: exception.localizedDescription)
+            }
+        }
+        withAnimation {
+            self.isLoading = false
+        }
+    }
+}
