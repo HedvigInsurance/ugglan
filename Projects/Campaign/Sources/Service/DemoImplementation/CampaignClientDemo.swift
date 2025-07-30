c3ccd8ff8d398121a04454b4239467c4222baf65
sladan-hedvig <127749787+sladan-hedvig@users.noreply.github.com>
2025-07-30 09:44:49 +0200

    Remove add and remove campaign (#1966)
    * Removed unused code
    
    * Updated tests
    
    * Swift options for PaymentDetailsDiscountOptions

--- Projects/Campaign/Sources/Service/DemoImplementation/CampaignClientDemo.swift
+++ Projects/Campaign/Sources/Service/DemoImplementation/CampaignClientDemo.swift
@@ -1,14 +1,6 @@
 public class hCampaignClientDemo: hCampaignClient {
     public init() {}
 
-    public func remove(codeId: String) async throws {
-        try await Task.sleep(nanoseconds: 1_000_000_000)
-    }
-
-    public func add(code: String) async throws {
-        try await Task.sleep(nanoseconds: 1_000_000_000)
-    }
-
     public func getPaymentDiscountsData() async throws -> PaymentDiscountsData {
         try await Task.sleep(nanoseconds: 1_000_000_000)
         return .init(
--- Projects/Campaign/Sources/Service/hCampaignService.swift
+++ Projects/Campaign/Sources/Service/hCampaignService.swift
@@ -4,16 +4,6 @@ import hCore
 public class hCampaignService {
     @Inject var service: hCampaignClient
 
-    public func remove(codeId: String) async throws {
-        log.info("hCampaignService: remove", error: nil, attributes: nil)
-        return try await service.remove(codeId: codeId)
-    }
-
-    public func add(code: String) async throws {
-        log.info("hCampaignService: add", error: nil, attributes: nil)
-        return try await service.add(code: code)
-    }
-
     public func getPaymentDiscountsData() async throws -> PaymentDiscountsData {
         log.info("hPaymentService: getPaymentDiscountsData", error: nil, attributes: nil)
         return try await service.getPaymentDiscountsData()
