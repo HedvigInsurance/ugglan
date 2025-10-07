98cd845d0d7688eaf7fc643add1d98b7726c8c7c
julia-andersson <julia.andersson@hedvig.com>
2025-10-07 14:12:48 -0500

    add test plan to workspace-ugglan

--- Workspace.swift
+++ Workspace.swift
@@ -4,5 +4,22 @@ import ProjectDescriptionHelpers
 let workspace = Workspace(
     name: "Ugglan",
     projects: ["Projects/*", "Dependencies/*"],
+    schemes: [
+        Scheme.scheme(
+            name: "Ugglan-Workspace",
+            shared: true,
+            buildAction: .buildAction(
+                targets: [.project(path: "Projects/App", target: "Ugglan")]
+            ),
+            testAction: .testPlans(
+                [
+                    .relativeToRoot("TestPlans/Ugglan-Workspace.xctestplan")
+                ]
+            ),
+            runAction: .runAction(
+                executable: .project(path: "Projects/App", target: "Ugglan")
+            )
+        )
+    ],
     generationOptions: .options(lastXcodeUpgradeCheck: .init(string: "1610"))
 )
