5ae372da6bca3d77142ed9cfbd632ba4b873b6e2
Sladan Nimcevic <sladan.nimcevic@hedvig.com>
2025-10-21 16:43:15 +0200

    Check if memberDeviceRegister = true

--- Projects/App/Sources/Service/Notifications/OctopusImplementation/NotificationClientOctopus.swift
+++ Projects/App/Sources/Service/Notifications/OctopusImplementation/NotificationClientOctopus.swift
@@ -17,7 +17,7 @@ class NotificationClientOctopus: NotificationClient {
     func register(for token: String) async throws {
         let data = try await octopus.client
             .mutation(mutation: OctopusGraphQL.MemberDeviceRegisterMutation(token: token))
-        if let data = data?.memberDeviceRegister {
+        if data?.memberDeviceRegister == true {
             log.info("Did register CustomerIO push token for user")
         } else {
             log.info("Failed to register CustomerIO push token for user")
