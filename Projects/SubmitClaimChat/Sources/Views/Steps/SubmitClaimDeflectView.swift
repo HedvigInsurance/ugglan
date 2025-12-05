b38c30629ad0bcca667681dbb2401d4e9dcdd2df
Sladan Nimcevic <sladan.nimcevic@hedvig.com>
2025-12-05 14:53:33 +0100

    WIP on feature/claims-automation/main

--- Projects/SubmitClaimChat/Sources/Views/Steps/SubmitClaimDeflectView.swift
+++ Projects/SubmitClaimChat/Sources/Views/Steps/SubmitClaimDeflectView.swift
@@ -15,74 +15,70 @@ public struct SubmitClaimDeflectScreen: View {
     }
 
     public var body: some View {
-        hForm {
-            VStack(spacing: .padding16) {
-                if let warningText = model.warningText {
-                    hSection {
-                        InfoCard(text: warningText, type: .attention)
-                            .accessibilitySortPriority(2)
-                    }
+        VStack(spacing: .padding16) {
+            if let warningText = model.warningText {
+                hSection {
+                    InfoCard(text: warningText, type: .attention)
+                        .accessibilitySortPriority(2)
                 }
+            }
 
-                VStack(spacing: .padding16) {
-                    if let infoViewTitle = model.title, let infoViewText = model.infoText {
-                        let title =
-                            model.partners.count == 1
-                            ? L10n.submitClaimPartnerSingularTitle : L10n.submitClaimPartnerTitle
-                        hSection {
-                            HStack {
-                                hText(title)
-                                Spacer()
-                                InfoViewHolder(
-                                    title: infoViewTitle,
-                                    description: infoViewText
-                                )
-                            }
-                        }
-                        .sectionContainerStyle(.transparent)
-                    }
-                    VStack(spacing: .padding8) {
-                        ForEach(model.partners, id: \.id) { partner in
-                            ClaimContactCard(
-                                model: partner
+            VStack(spacing: .padding16) {
+                if let infoViewTitle = model.title, let infoViewText = model.infoText {
+                    let title =
+                        model.partners.count == 1
+                        ? L10n.submitClaimPartnerSingularTitle : L10n.submitClaimPartnerTitle
+                    hSection {
+                        HStack {
+                            hText(title)
+                            Spacer()
+                            InfoViewHolder(
+                                title: infoViewTitle,
+                                description: infoViewText
                             )
                         }
                     }
+                    .sectionContainerStyle(.transparent)
                 }
-
-                hSection {
-                    VStack(alignment: .leading, spacing: 8) {
-                        hText(model.content.title)
-                        hText(model.content.description)
-                            .foregroundColor(hTextColor.Opaque.secondary)
-                    }
-                }
-                .padding(.top, .padding8)
-                .sectionContainerStyle(.transparent)
-                .accessibilityElement(children: .combine)
-
-                VStack(spacing: .padding4) {
-                    ForEach(model.questions, id: \.question) { question in
-                        InfoExpandableView(
-                            title: question.question,
-                            text: question.answer,
-                            onMarkDownClick: { url in
-                                NotificationCenter.default.post(name: .openDeepLink, object: url)
-                            }
+                VStack(spacing: .padding8) {
+                    ForEach(model.partners, id: \.id) { partner in
+                        ClaimContactCard(
+                            model: partner
                         )
                     }
                 }
-                .padding(.top, .padding8)
-                ForEach(model.linkOnlyPartners, id: \.id) { partner in
-                    ParnerButtonView(model: partner)
+            }
+
+            hSection {
+                VStack(alignment: .leading, spacing: 8) {
+                    hText(model.content.title)
+                    hText(model.content.description)
+                        .foregroundColor(hTextColor.Opaque.secondary)
                 }
-                SupportView(openChat: openChat)
-                    .padding(.top, .padding56)
             }
             .padding(.top, .padding8)
+            .sectionContainerStyle(.transparent)
+            .accessibilityElement(children: .combine)
+
+            VStack(spacing: .padding4) {
+                ForEach(model.questions, id: \.question) { question in
+                    InfoExpandableView(
+                        title: question.question,
+                        text: question.answer,
+                        onMarkDownClick: { url in
+                            NotificationCenter.default.post(name: .openDeepLink, object: url)
+                        }
+                    )
+                }
+            }
+            .padding(.top, .padding8)
+            ForEach(model.linkOnlyPartners, id: \.id) { partner in
+                ParnerButtonView(model: partner)
+            }
+            SupportView(openChat: openChat)
+                .padding(.top, .padding56)
         }
-        .hFormBottomBackgroundColor(.gradient(from: hBackgroundColor.primary, to: hSurfaceColor.Opaque.primary))
-        .edgesIgnoringSafeArea(.bottom)
+        .padding(.top, .padding8)
     }
 }
 
