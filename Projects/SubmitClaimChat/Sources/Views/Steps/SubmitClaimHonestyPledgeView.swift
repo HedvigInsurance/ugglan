b24faa336c54bcb2dead2d9a1ccb44a963ec6d14
julia-andersson <julia.andersson@hedvig.com>
2025-12-18 15:48:52 -0600

    add honesty pledge design

---
+++ Projects/SubmitClaimChat/Sources/Views/Steps/SubmitClaimHonestyPledgeView.swift
@@ -0,0 +1,38 @@
+import SwiftUI
+import hCore
+import hCoreUI
+
+struct SubmitClaimHonestyPledgeView: View {
+    @ObservedObject var submitClaimChatViewModel: SubmitClaimChatViewModel
+    @ObservedObject var viewModel: SubmitClaimHonestyPledgeStep
+    @EnvironmentObject var router: Router
+
+    var body: some View {
+        hSection {
+            VStack(spacing: .padding8) {
+                hButton(.large, .primary, content: .init(title: L10n.claimsPledgeSlideLabel)) {
+                    viewModel.startFlow()
+                    submitClaimChatViewModel.startClaimIntent()
+                }
+                hCancelButton {
+                    router.dismiss()
+                }
+            }
+        }
+        .sectionContainerStyle(.transparent)
+    }
+}
+
+struct SubmitClaimHonestyPledgeResultView: View {
+    var body: some View {
+        hPill(
+            text: L10n.claimsPledgeSlideLabel,
+            color: .grey
+        )
+        .hFieldSize(.capsuleShape)
+    }
+}
+
+#Preview {
+
+}
