21f98d7b87df8dafa69b055964d96292a289e6c7
Sladan Nimcevic <sladan.nimcevic@hedvig.com>
2026-03-11 15:12:20 +0100

    rive

--- Projects/SubmitClaimChat/Sources/Views/Components/HedvigRiveAnimationView.swift
+++ Projects/SubmitClaimChat/Sources/Views/Components/HedvigRiveAnimationView.swift
@@ -1,45 +1,56 @@
 @_spi(RiveExperimental) import RiveRuntime
 import SwiftUI
+import hCore
 
 struct HedvigRiveAnimationView: View {
     private enum Constants {
         static let darkFileName = "White"
         static let lightFileName = "Black"
-        static let stateMachine = "State Machine 1"
-        static let animatingInput = "Boolean 1"
         static let size: CGFloat = 100
     }
 
     @Binding var isAnimating: Bool
-    @Environment(\.colorScheme) private var colorScheme
-    @State private var riveViewModel: RiveViewModel?
+    @StateObject private var riveViewModel: RiveViewModel
+
+    init(isAnimating: Binding<Bool>) {
+        self._isAnimating = isAnimating
+
+        let model = RiveViewModel(
+            fileName: Constants.lightFileName,
+            animationName: "Idle",
+            autoPlay: false
+        )
+        self._riveViewModel = StateObject(wrappedValue: model)
+    }
 
     var body: some View {
         Group {
-            if let riveViewModel {
-                riveViewModel.view()
-            } else {
-                Color.clear
-            }
+            riveViewModel.view()
         }
         .frame(width: Constants.size, height: Constants.size)
         .task {
-            let vm = makeViewModel()
-            riveViewModel = vm
-            vm.setInput(Constants.animatingInput, value: isAnimating)
+            print("ANIMATION PLAYED TASK")
+            playAnimations(animating: isAnimating)
         }
         .onChange(of: isAnimating) { newValue in
-            riveViewModel?.setInput(Constants.animatingInput, value: newValue)
-        }
-        .onChange(of: colorScheme) { _ in
-            riveViewModel = makeViewModel()
+            print("ANIMATION PLAYED CHANGE")
+            playAnimations(animating: newValue)
         }
     }
-
-    private func makeViewModel() -> RiveViewModel {
-        RiveViewModel(
-            fileName: colorScheme == .dark ? Constants.darkFileName : Constants.lightFileName,
-            stateMachineName: Constants.stateMachine
-        )
+    @State var task: Task<(), Error>? = nil
+    private func playAnimations(animating: Bool) {
+        task?.cancel()
+        task = Task { [weak riveViewModel] in
+            if animating {
+                await delay(0.1)
+                riveViewModel?.play(animationName: "Loading intro")
+                await delay(1)
+                try Task.checkCancellation()
+                riveViewModel?.play(animationName: "Loading")
+            } else {
+                riveViewModel?.stop()
+                riveViewModel?.play(animationName: "Loading outro")
+            }
+        }
     }
 }
