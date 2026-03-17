a5296c41edf8b1c8274a18ecde622e7c897e5d2f
sladan-hedvig <127749787+sladan-hedvig@users.noreply.github.com>
2026-03-17 16:30:07 +0100

    Chore: make insurance list expandable (#2302)
    * Make expendable insurance list
    
    * Make it faster
    
    * Updated card radius
    
    * Scroll to selected card
    
    * Changed scroll position

--- Projects/App/Sources/Navigation/LoggedInNavigation.swift
+++ Projects/App/Sources/Navigation/LoggedInNavigation.swift
@@ -466,6 +466,9 @@ struct LoggedInNavigation: View {
             profileTab
         }
         .tint(hTextColor.Opaque.primary)
+        .onChange(of: vm.selectedTab) { newTab in
+            vm.contractsNavigationVm.isActiveTab = (newTab == 1)
+        }
         .handleLoggedInPresentations(with: vm)
         .introspect(.tabView, on: .iOS(.v13...)) { tabBar in
             vm.tabBar = tabBar
