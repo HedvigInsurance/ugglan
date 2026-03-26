a73b0d0e33f5e3b954187ff8f03d37629af24e39
Sladan Nimcevic <sladan.nimcevic@hedvig.com>
2026-03-26 16:31:13 +0100

    Added log info

--- Projects/App/Sources/AppDelegate+Notifications.swift
+++ Projects/App/Sources/AppDelegate+Notifications.swift
@@ -79,6 +79,7 @@ extension AppDelegate: @preconcurrency UNUserNotificationCenterDelegate {
         withCompletionHandler completionHandler: @escaping () -> Void
     ) {
         let userInfo = response.notification.request.content.userInfo
+        log.info("DEEPLINK INFO HANDLING \(userInfo)")
 
         if response.actionIdentifier == UNNotificationDefaultActionIdentifier {
             if let notificationType = getNotificationType(from: userInfo) {
