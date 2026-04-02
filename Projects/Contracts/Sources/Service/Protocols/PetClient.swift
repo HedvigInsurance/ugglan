c6c171d22325ba1fa3a107cf67155dcd9e844bb0
victor-reyes-by <victor.reyes@hedvig.com>
2026-04-02 12:21:31 +0200

    fix: correct typo in success toast method and remove unused imports

--- Projects/Contracts/Sources/Service/Protocols/PetClient.swift
+++ Projects/Contracts/Sources/Service/Protocols/PetClient.swift
@@ -2,7 +2,7 @@ import Foundation
 
 @MainActor
 public protocol PetClient {
-    func addMissing(petChipId: String, for contractId: String, ) async throws -> PetError?
+    func addMissing(petChipId: String, for contractId: String) async throws -> PetError?
 }
 
 public struct PetError {
