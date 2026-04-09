23f3c0ecbd09c5762597e74e0acbeb8ef267f779
Sladan Nimcevic <sladan.nimcevic@hedvig.com>
2026-04-09 15:57:51 +0200

    Refactor

--- Projects/App/Sources/Service/OctopusClientsImplementation/NetworkClient+AuthorizationCodeClient.swift
+++ Projects/App/Sources/Service/OctopusClientsImplementation/NetworkClient+AuthorizationCodeClient.swift
@@ -8,34 +8,8 @@ import hGraphQL
 extension NetworkClient: @retroactive AuthorizationCodeClient {
     public func getAuthorizationCode() async throws -> AuthorizationCodeCreationOutput {
         let request = try await AuthorizationCodeRequest.createCode.asRequest()
-        let response: AuthorizationCodeCreationOutput = try await withCheckedThrowingContinuation { continuation in
-            let task = self.sessionClient.dataTask(with: request) { [weak self] data, response, error in
-                Task {
-                    do {
-                        guard let self else {
-                            continuation.resume(
-                                throwing: NSError(
-                                    domain: "NetworkClient",
-                                    code: -1,
-                                    userInfo: [NSLocalizedDescriptionKey: "NetworkClient was deallocated"]
-                                )
-                            )
-                            return
-                        }
-                        let result: AuthorizationCodeCreationOutput = try await self.handleResponseForced(
-                            data: data,
-                            response: response,
-                            error: error
-                        )
-                        continuation.resume(returning: result)
-                    } catch {
-                        continuation.resume(throwing: error)
-                    }
-                }
-            }
-            task.resume()
-        }
-        return response
+        let (data, response) = try await sessionClient.data(for: request)
+        return try await handleResponseForced(data: data, response: response, error: nil)
     }
 }
 
@@ -47,9 +21,7 @@ private enum AuthorizationCodeRequest {
     }
 
     func asRequest() async throws -> URLRequest {
-        var urlString = baseUrl.absoluteString
-        urlString.append("/member-authorization-codes")
-        let url = URL(string: urlString)!
+        let url = baseUrl.appending(path: "member-authorization-codes")
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
