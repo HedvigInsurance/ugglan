1209698f48cab5d51241cdb54b643b9e5e490632
Sladan Nimcevic <sladan.nimcevic@hedvig.com>
2026-05-12 14:22:23 +0200

    Removed unused files + upgrade tuist to min xcode 26

--- Projects/TestingUtil/Sources/CombineMultiple.swift
+++
@@ -1,9 +0,0 @@
-import Apollo
-import ApolloAPI
-import Foundation
-import hCore
-
-public func combineMultiple(_ jsonObjects: [JSONObject]) -> JSONObject {
-    jsonObjects.reduce(JSONObject()) { result, jsonObject in result.merging(jsonObject, uniquingKeysWith: takeRight)
-    }
-}
--- Workspace.swift
+++ Workspace.swift
@@ -4,5 +4,5 @@ import ProjectDescriptionHelpers
 let workspace = Workspace(
     name: "Ugglan",
     projects: ["Projects/*", "Dependencies/*"],
-    generationOptions: .options(lastXcodeUpgradeCheck: .init(string: "1610"))
+    generationOptions: .options(lastXcodeUpgradeCheck: .init(string: "2600"))
 )
