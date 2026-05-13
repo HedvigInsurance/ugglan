952f3f21cc29ecffd421458bfb95163e411b9d3b
stylianosgakis <stylianos.gakis98@gmail.com>
2026-05-13 10:16:40 +0200

    Bump umbrella

--- Tuist/ProjectDescriptionHelpers/Project+DependenciesTemplate.swift
+++ Tuist/ProjectDescriptionHelpers/Project+DependenciesTemplate.swift
@@ -82,7 +82,7 @@ public enum ExternalDependencies: CaseIterable {
         case .umbrella:
             if isLocalUmbrellaMode { return [] }
             return [
-                .package(url: "https://github.com/HedvigInsurance/umbrella.git", .exact("0.0.20260507135718"))
+                .package(url: "https://github.com/HedvigInsurance/umbrella.git", .exact("0.0.20260513081147"))
             ]
         case .kmpNativeCoroutines:
             return [
