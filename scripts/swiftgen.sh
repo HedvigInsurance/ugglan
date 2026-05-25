7683892fd362d3f58c94e760c7bccd55ad06fbdc
Sladan Nimcevic <sladan.nimcevic@hedvig.com>
2026-05-25 10:35:59 +0200

    Replaced brew with mise

--- scripts/swiftgen.sh
+++ scripts/swiftgen.sh
@@ -1,10 +1,3 @@
-if [ -z "$CI" ]; then
-    TMPDIR=/tmp/swiftgen-6.6.3
-else
-    mkdir -p build
-    TMPDIR=build/swiftgen-6.6.3
-fi
-
 mkdir -p Projects/hCoreUI/Sources/Derived
 mkdir -p Projects/App/Sources/Derived
 mkdir -p Projects/hCore/Sources/Derived
@@ -13,14 +6,9 @@ mkdir -p Projects/Contracts/Sources/Derived
 mkdir -p Projects/Home/Sources/Derived
 mkdir -p Projects/Market/Sources/Derived
 
-if [[ ! -f $TMPDIR/bin/swiftgen ]]
-then
-    mkdir -p $TMPDIR
-    curl -o $TMPDIR/swiftgen.zip -L https://github.com/SwiftGen/SwiftGen/releases/download/6.6.3/swiftgen-6.6.3.zip
-    unzip $TMPDIR/swiftgen.zip -d $TMPDIR
-fi
-
-$TMPDIR/bin/swiftgen
+SWIFTGEN_VERSION=6.6.3
+mise install swiftgen@$SWIFTGEN_VERSION
+mise exec swiftgen@$SWIFTGEN_VERSION -- swiftgen
 
 # Stock SwiftGen swift5 template emits `public enum hCoreUIAssets` with non-Sendable
 # static lets, which Swift 6 strict concurrency rejects. Annotate with @MainActor.
