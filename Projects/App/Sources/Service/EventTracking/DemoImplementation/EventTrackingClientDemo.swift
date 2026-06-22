11f52451cb81773b4f092b3d9aa3b5dd7bc0ee01
Sladan Nimcevic <sladan.nimcevic@hedvig.com>
2026-06-22 14:19:35 +0200

    Removed demo log

--- Projects/App/Sources/Service/EventTracking/DemoImplementation/EventTrackingClientDemo.swift
+++ Projects/App/Sources/Service/EventTracking/DemoImplementation/EventTrackingClientDemo.swift
@@ -4,19 +4,11 @@ import hCore
 struct EventTrackingClientDemo: EventTrackingClient {
     func setCollectionEnabled(_ enabled: Bool) {}
 
-    func trackEvent(name: String, parameters: [String: Any]?) {
-        log.info("[EventTracking demo] event=\(name) params=\(parameters ?? [:])", error: nil, attributes: nil)
-    }
+    func trackEvent(name: String, parameters: [String: Any]?) {}
 
-    func trackScreen(name: String, parameters: [String: Any]?) {
-        log.info("[EventTracking demo] screen=\(name) params=\(parameters ?? [:])", error: nil, attributes: nil)
-    }
+    func trackScreen(name: String, parameters: [String: Any]?) {}
 
-    func setUserId(_ userId: String?) {
-        log.info("[EventTracking demo] setUserId=\(userId ?? "nil")", error: nil, attributes: nil)
-    }
+    func setUserId(_ userId: String?) {}
 
-    func setUserProperty(name: String, value: String?) {
-        log.info("[EventTracking demo] setUserProperty \(name)=\(value ?? "nil")", error: nil, attributes: nil)
-    }
+    func setUserProperty(name: String, value: String?) {}
 }
