71325e50ce8e3426377608718c7eaee764a8984b
victor-reyes-by <victor.reyes@hedvig.com>
2026-08-14 14:11:24 +0200

    feat: contract status card in the Home banner carousel

--- Projects/Home/Sources/Screens/HomeBottomScrollView.swift
+++ Projects/Home/Sources/Screens/HomeBottomScrollView.swift
@@ -26,7 +26,8 @@ struct HomeBottomScrollView: View {
                     if let importantMessage = store.getImportantMessage(with: id) {
                         ImportantMessageView(importantMessage: importantMessage)
                     }
-                case .terminated: InfoCard(text: L10n.HomeTab.terminatedBody, type: .info)
+                case .terminated, .activeInFuture, .pendingSwitchable, .pendingNonswitchable:
+                    InfoCard(text: content.id.statusMessage, type: .info)
                 }
             }
         )
@@ -59,7 +60,7 @@ class HomeBottomScrollViewModel: ObservableObject {
         handleMissingCoOwners()
         handleImportantMessages()
         handleRenewalCardView()
-        handleTerminatedMessage()
+        handleStatusMessage()
         handleUpdateContactInfo()
         handleMissingPetChipIds()
     }
@@ -169,17 +170,38 @@ class HomeBottomScrollViewModel: ObservableObject {
             .store(in: &cancellables)
     }
 
-    func handleTerminatedMessage() {
+    private func handleStatusMessage() {
         let store: HomeStore = globalAppStateContainer.get()
         store.$memberContractState
+            .combineLatest(store.$futureStatus)
+            .map { memberContractState, futureStatus in
+                Self.statusCard(for: memberContractState, futureStatus: futureStatus)
+            }
+            .removeDuplicates()
             .receive(on: RunLoop.main)
-            .sink(receiveValue: { [weak self] memberContractState in
-                switch memberContractState {
-                case .terminated: self?.handleItem(.terminated, with: true)
-                default: self?.handleItem(.terminated, with: false)
+            .sink(receiveValue: { [weak self] statusCard in
+                guard let self = self else { return }
+                let staleItems = self.localItems.filter { $0.id.isStatusMessage }
+                var newItems = self.localItems.subtracting(staleItems)
+                if let statusCard {
+                    newItems.insert(.init(with: statusCard))
                 }
+                withAnimation { self.localItems = newItems }
             })
             .store(in: &cancellables)
+    }
+
+    private static func statusCard(
+        for memberContractState: MemberContractState,
+        futureStatus: FutureStatus
+    ) -> InfoCardType? {
+        switch (memberContractState, futureStatus) {
+        case (.terminated, _): .terminated
+        case (.future, let .activeInFuture(inceptionDate)): .activeInFuture(inceptionDate: inceptionDate)
+        case (.future, .pendingSwitchable): .pendingSwitchable
+        case (.future, .pendingNonswitchable): .pendingNonswitchable
+        default: nil
+        }
     }
 
     func handleUpdateContactInfo() {
@@ -206,7 +228,13 @@ class HomeBottomScrollViewModel: ObservableObject {
 }
 
 #Preview {
+    Localization.Locale.currentLocale.send(.en_SE)
+    Dependencies.shared.add(module: Module { () -> FeatureFlags in FeatureFlags.shared })
     Dependencies.shared.add(module: Module { () -> FeatureFlagsClient in FeatureFlagsDemo() })
+    Dependencies.shared.add(module: Module { () -> HomeClient in HomeClientDemo() })
+    Dependencies.shared.add(module: Module { () -> FetchContractsClient in FetchContractsClientDemo() })
+    Dependencies.shared.add(module: Module { () -> hPaymentClient in hPaymentClientDemo() })
+    Dependencies.shared.add(module: Module { () -> DateService in DateService() })
     return HomeBottomScrollView()
 }
 
@@ -217,8 +245,29 @@ struct InfoCardView: Identifiable, Hashable {
     }
 }
 
+/// The synthesized `Comparable` follows the declaration order, and that order is the carousel order.
 public enum InfoCardType: Hashable, Comparable {
     case importantMessage(message: String)
     case renewal
     case terminated
+    case activeInFuture(inceptionDate: String)
+    case pendingSwitchable
+    case pendingNonswitchable
+}
+
+extension InfoCardType {
+    /// `nil` for the cards that bring their own view instead of a line of status copy.
+    var statusMessage: String? {
+        switch self {
+        case .terminated: L10n.HomeTab.terminatedBody
+        case let .activeInFuture(inceptionDate): L10n.HomeTab.activeInFutureInfo(inceptionDate)
+        case .pendingSwitchable: L10n.HomeTab.pendingSwitchableInfo
+        case .pendingNonswitchable: L10n.HomeTab.pendingNonswitchableInfo
+        case .importantMessage, .renewal: nil
+        }
+    }
+
+    var isStatusMessage: Bool {
+        statusMessage != nil
+    }
 }
