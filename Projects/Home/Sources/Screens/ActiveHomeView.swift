657e4dab31fb54d25a0af899cde0e34941e5e8a0
Sladan Nimcevic <sladan.nimcevic@hedvig.com>
2026-08-17 16:47:44 +0200

    Conflict resolved

--- Projects/Home/Sources/Screens/ActiveHomeView.swift
+++ Projects/Home/Sources/Screens/ActiveHomeView.swift
@@ -88,6 +88,13 @@ struct ActiveHomeView: View {
         }
         .sectionContainerStyle(.transparent)
     }
+
+    @ViewBuilder private var infoMessagesCarouselSection: some View {
+        if !bottomVm.items.isEmpty {
+            hSection { HomeBottomScrollView(vm: bottomVm) }
+                .sectionContainerStyle(.transparent)
+        }
+    }
 }
 
 /// Clips a view by trimming `topInset` points off its top edge, revealing the rest below.
@@ -100,20 +107,6 @@ private struct TopClipShape: Shape {
         return Path(
             CGRect(x: rect.minX, y: rect.minY + inset, width: rect.width, height: rect.height - inset)
         )
-    }
-
-    @ViewBuilder private var bannerSection: some View {
-        if !bottomVm.items.isEmpty {
-            hSection { HomeBottomScrollView(vm: bottomVm) }
-                .sectionContainerStyle(.transparent)
-        }
-    }
-
-    @ViewBuilder private var infoMessagesCarouselSection: some View {
-        if !bottomVm.items.isEmpty {
-            hSection { HomeBottomScrollView(vm: bottomVm) }
-                .sectionContainerStyle(.transparent)
-        }
     }
 }
 
