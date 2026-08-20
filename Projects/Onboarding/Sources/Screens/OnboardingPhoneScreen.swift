94be85d389c33aa65c3121246e0b6cf2e53642ba
Sladan Nimcevic <sladan.nimcevic@hedvig.com>
2026-08-20 09:50:37 +0200

    Added is button

--- Projects/Onboarding/Sources/Screens/OnboardingPhoneScreen.swift
+++ Projects/Onboarding/Sources/Screens/OnboardingPhoneScreen.swift
@@ -33,6 +33,7 @@ struct OnboardingPhoneScreen: View {
                                             animateDigits.removeFirst()
                                         }
                                     }
+                                    .accessibilityAddTraits(.isButton)
                             }
                         }
                     }
