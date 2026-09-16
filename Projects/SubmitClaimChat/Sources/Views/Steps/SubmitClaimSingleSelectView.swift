28dd43a53ab8b3f8d38c8ebbd4ea5d56935934b2
Sladan Nimcevic <sladan.nimcevic@hedvig.com>
2026-09-16 08:30:23 +0200

    Updated accessibility

--- Projects/SubmitClaimChat/Sources/Views/Steps/SubmitClaimSingleSelectView.swift
+++ Projects/SubmitClaimChat/Sources/Views/Steps/SubmitClaimSingleSelectView.swift
@@ -41,19 +41,16 @@ struct SubmitClaimSingleSelectView: View {
         ) { optionId in
             let option = viewModel.model.options.first(where: { $0.id == optionId })!
             if showOptions {
-                hPill(
-                    text: option.title,
-                    color: viewModel.selectedOptionId == optionId ? .green : .grey,
-                    colorLevel: .two,
-                    withBorder: false,
-                    minWidth: .padding60
-                )
-                .hFieldSize(.large)
-                .capsuleShape(true)
-                .transition(.submitClaimOptionAppear)
-                .onTapGesture { selectOption(id: option.id) }
+                hButton(
+                    .medium,
+                    viewModel.selectedOptionId == optionId ? .primaryAlt : .secondary,
+                    content: .init(title: option.title)
+                ) {
+                    selectOption(id: option.id)
+                }
                 .accessibilityAddTraits(.isButton)
                 .optionAccessibility(label: option.title)
+                .transition(.submitClaimOptionAppear)
             }
         }
         .tagFlow(
@@ -83,7 +80,6 @@ struct SubmitClaimSingleSelectView: View {
                     }
                     .hWrapInPill(color: viewModel.selectedOptionId == option.id ? .green : .grey, colorLevel: .two)
                     .hButtonTakeFullWidth(true)
-                    .optionAccessibility(label: option.title)
                     .transition(.submitClaimOptionAppear)
                     .accessibilityAddTraits(.isButton)
                     .optionAccessibility(label: option.title)
@@ -110,7 +106,7 @@ struct SubmitClaimSingleSelectView: View {
 extension View {
     fileprivate func optionAccessibility(label: String) -> some View {
         self.accessibilityLabel(label)
-            .accessibilityHint(L10n.voiceoverDoubleClickTo + " " + L10n.voiceoverOptionSelected)
+            .accessibilityHint(L10n.voiceoverDoubleClickTo + " " + L10n.generalSelectButton)
             .accessibilityAddTraits(.isButton)
     }
 }
